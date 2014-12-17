22eaa90201f1c6f5cc5a11d8071b5ac46f17c2cd
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2014-12-17 14:33:21 -0700

    banner mentions cannot overlap other protein mentions

--- src/main/scala/edu/arizona/sista/bionlp/reach/ruler/DarpaActions.scala
+++ src/main/scala/edu/arizona/sista/bionlp/reach/ruler/DarpaActions.scala
@@ -12,8 +12,8 @@ class DarpaActions extends Actions {
   }
 
   def mkBannerMention(label: String, mention: Map[String, Seq[Interval]], sent: Int, doc: Document, ruleName: String, state: State): Seq[Mention] = {
-    val allMentions = state.allMentions.filter(_.sentence == sent).map(_.tokenInterval)
-    // make sure each interval doesn't intersect with existing mentions (only TextBound at this point)
+    val allMentions = state.allMentions.filter(m => m.sentence == sent && m.matches("Gene_or_gene_product")).map(_.tokenInterval)
+    // make sure each interval doesn't intersect with existing Gene_or_gene_product mentions previously found
     for (m <- mention("--GLOBAL--") if allMentions.forall(!_.intersects(m))) yield new TextBoundMention(label, m, sent, doc, ruleName)
   }
 
