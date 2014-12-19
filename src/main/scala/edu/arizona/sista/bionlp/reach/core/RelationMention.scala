4c3b0da0b94eff70bf63a0792bb9dba05daf8171
Gus Hahn-Powell <gushahnpowell@gmail.com>
2014-12-19 15:40:27 -0700

    added RelationMention

--- src/main/scala/edu/arizona/sista/bionlp/reach/core/RelationMention.scala
+++ src/main/scala/edu/arizona/sista/bionlp/reach/core/RelationMention.scala
@@ -1,8 +1,23 @@
 package edu.arizona.sista.bionlp.reach.core
 
+import edu.arizona.sista.matcher.Mention
+import edu.arizona.sista.processors.Document
+import edu.arizona.sista.struct.Interval
+
 /**
  * Created by gus on 12/19/14.
  */
-class RelationMention {
 
+class RelationMention
+  (val label: String,
+  val arguments: Map[String, Seq[Mention]],
+  val sentence: Int,
+  val document: Document,
+  val foundBy: String) extends Mention {
+    // token interval that contains trigger and all matched arguments
+    override def tokenInterval: Interval = {
+      val allStarts = arguments.values.flatMap(_.map(_.start)).toSeq
+      val allEnds = arguments.values.flatMap(_.map(_.end)).toSeq
+      Interval(allStarts.min, allEnds.max)
+    }
 }
