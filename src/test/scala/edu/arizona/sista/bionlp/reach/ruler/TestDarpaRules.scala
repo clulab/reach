080bdadf1471c0ec118375478df6de2acdc16474
Mihai Surdeanu <surdeanu@gmail.com>
2014-12-31 17:30:50 -0700

    Added all unit tests.

--- src/test/scala/edu/arizona/sista/bionlp/reach/ruler/TestDarpaRules.scala
+++ src/test/scala/edu/arizona/sista/bionlp/reach/ruler/TestDarpaRules.scala
@@ -78,8 +78,8 @@ class TestDarpaRules extends AssertionsForJUnit {
     val doc = proc.annotate("As expected based on previous studies, wild- type K-Ras bound primarily 32P-GDP, while G12V-Ras bound 32P-GTP (Fig.2, A and B).")
     val mentions = extractor.extractFrom(doc)
     RuleShell.displayMentions(mentions, doc)
-    assertTrue(hasEventWithArguments("Binding", List("K-Ras"), mentions)) // TODO: add 32P-GDP
-    assertTrue(hasEventWithArguments("Binding", List("G12V-Ras"), mentions)) // TODO: add 32P-GTP
+    assertTrue(hasEventWithArguments("Binding", List("K-Ras"), mentions)) // TODO: add 32P-GDP (MARCO)
+    assertTrue(hasEventWithArguments("Binding", List("G12V-Ras"), mentions)) // TODO: add 32P-GTP (MARCO)
   }
 
   @Test def testRules2() {
@@ -93,7 +93,7 @@ class TestDarpaRules extends AssertionsForJUnit {
     val doc = proc.annotate("We hypothesized that MEK inhibition activates AKT by inhibiting ERK activity, which blocks an inhibitory threonine phosphorylation on the JM domains of EGFR and HER2, thereby increasing ERBB3 phosphorylation.")
     val mentions = extractor.extractFrom(doc)
     RuleShell.displayMentions(mentions, doc)
-    // TODO: all these Phosphorylations fail!
+    // TODO: all these Phosphorylations fail! (GUS)
     assertTrue(hasEventWithArguments("Phosphorylation", List("EGFR"), mentions))
     assertTrue(hasEventWithArguments("Phosphorylation", List("HER2"), mentions))
     assertTrue(hasEventWithArguments("Phosphorylation", List("ERBB3"), mentions))
@@ -109,7 +109,7 @@ class TestDarpaRules extends AssertionsForJUnit {
     assertTrue(hasEntity("ERBB3", mentions))
 
     assertTrue(hasEntityWithSite("EGFR", "JM domains", mentions))
-    // TODO: this fails because of not handling the coordination
+    // TODO: this fails because of not handling the coordination (GUS)
     assertTrue(hasEntityWithSite("HER2", "JM domains", mentions))
   }
 
@@ -121,7 +121,7 @@ class TestDarpaRules extends AssertionsForJUnit {
     assertTrue(hasEntity("ERBB3", mentions))
     assertTrue(hasEntity("EGFR", mentions))
 
-    // TODO: this fails: the site is attached to the incorrect entity
+    // TODO: this fails: the site is attached to the incorrect entity (GUS)
     assertTrue(hasEntityWithSite("EGFR", "T669A", mentions))
   }
 
@@ -130,7 +130,7 @@ class TestDarpaRules extends AssertionsForJUnit {
     val mentions = extractor.extractFrom(doc)
     RuleShell.displayMentions(mentions, doc)
 
-    // TODO: this fails: the site is attached to the incorrect entity
+    // TODO: this fails: the site is attached to the incorrect entity (GUS)
     assertTrue(hasEntityWithSite("HER2", "T677A mutant", mentions))
   }
 
@@ -139,7 +139,7 @@ class TestDarpaRules extends AssertionsForJUnit {
     val mentions = extractor.extractFrom(doc)
     RuleShell.displayMentions(mentions, doc)
 
-    // TODO: this fails
+    // TODO: this fails (MARCO)
     assertTrue(hasEventWithArguments("Binding", List("Gab1", "phosphatidylinositol 3-kinase"), mentions))
   }
 
@@ -155,12 +155,65 @@ class TestDarpaRules extends AssertionsForJUnit {
     val mentions = extractor.extractFrom(doc)
     RuleShell.displayMentions(mentions, doc)
 
-    assertTrue(hasEntityWithSite("HER2", "JM domains", mentions)) // TODO: this fails
+    assertTrue(hasEntityWithSite("HER2", "JM domains", mentions)) // TODO: this fails (GUS)
     assertTrue(hasEntityWithSite("EGFR", "JM domains", mentions))
 
     assertTrue(hasEventWithArguments("Phosphorylation", List("EGFR"), mentions))
     assertTrue(hasEventWithArguments("Phosphorylation", List("HER2"), mentions))
 
+  }
+
+  @Test def testRules10() {
+    val doc = proc.annotate("For example, ERK- mediated serine phosphorylation of the GAB1 adaptor has been shown to negatively regulate GAB1-PI3K binding and downstream AKT signaling")
+    val mentions = extractor.extractFrom(doc)
+    RuleShell.displayMentions(mentions, doc)
+
+    assertTrue(hasEventWithArguments("Phosphorylation", List("GAB1 adaptor"), mentions))
+    assertTrue(hasEventWithArguments("Binding", List("GAB1-PI3K"), mentions))
+
+    // TODO: there are 2 regulations here! Whoever works on this, please add asserts for these two (MARCO or GUS)
+  }
+
+  @Test def testRules11() {
+    val doc = proc.annotate("In contrast, the EGFR T669A mutant increased both basal EGFR and ERBB3 tyrosine phosphorylation that was not augmented by MEK inhibition")
+    val mentions = extractor.extractFrom(doc)
+    RuleShell.displayMentions(mentions, doc)
+
+    // TODO: this fails due to coordination (GUS)
+    assertTrue(hasEventWithArguments("Phosphorylation", List("EGFR"), mentions))
+    assertTrue(hasEventWithArguments("Phosphorylation", List("ERBB3"), mentions))
+  }
+
+  @Test def testRules12() {
+    val doc = proc.annotate("To address the effect of K-Ras ubiquitination on its binding to PI3K and Raf family members, either total G12V-K-Ras or the ubiquitinated subfraction of G12V-K-Ras was immunoprecipitated and the immunoprecipitates were probed with antibodies to detect associated Ras effector molecules.")
+    val mentions = extractor.extractFrom(doc)
+    RuleShell.displayMentions(mentions, doc)
+
+    // TODO: this fails due to weird rule (GUS)
+    assertTrue(hasEventWithArguments("Ubiquitination", List("K-Ras"), mentions))
+    assertTrue(hasEventWithArguments("Binding", List("PI3K", "Raf family members"), mentions))
+
+    assertTrue(hasEntity("G12V-K-Ras", mentions))
+  }
+
+  @Test def testRules13() {
+    val doc = proc.annotate("We observed increased ERBB3 binding to PI3K following MEK inhibition (Figure 1D), and accordingly, MEK inhibition substantially increased tyrosine phosphorylated ERBB3 levels (Figure 1A).")
+    val mentions = extractor.extractFrom(doc)
+    RuleShell.displayMentions(mentions, doc)
+
+    // TODO: this fails, missing PI3K (MARCO)
+    assertTrue(hasEventWithArguments("Binding", List("PI3K", "ERBB3"), mentions))
+
+    // TODO: missing positive regulation of Binding, Controller: MEK. Please add assert for this (MARCO or GUS)
+  }
+
+  @Test def testRules14() {
+    val doc = proc.annotate("We propose that once ubiquitination occurs on Ras at Lys147, it enhances GDP/GTP exchange of Ras and increases the fraction of Ras in the GTP-form (Fig. 6B).")
+    val mentions = extractor.extractFrom(doc)
+    RuleShell.displayMentions(mentions, doc)
+
+    // TODO: this fails, missing Ras (DANE)
+    assertTrue(hasEventWithArguments("Exchange", List("GTP", "GDP", "Ras"), mentions))
   }
 }
 
