3f12709c20b1b9d59a2a9818688e26d38124561f
Gus Hahn-Powell <gushahnpowell@gmail.com>
2015-01-02 16:57:17 -0700

    added regulation event files.

--- src/main/scala/edu/arizona/sista/bionlp/reach/ruler/Ruler.scala
+++ src/main/scala/edu/arizona/sista/bionlp/reach/ruler/Ruler.scala
@@ -57,7 +57,15 @@ object Ruler {
 
   def readEventRules: String = {
     val dir = "/edu/arizona/sista/bionlp/extractors"
-    val files = Seq(s"$dir/phospho_events.yml", s"$dir/ubiq_events.yml", s"$dir/hydrox_events.yml", s"$dir/bind_events.yml", s"$dir/exchange_events.yml", s"$dir/degrad_events.yml", s"$dir/transcription_events.yml")
+    val files = Seq(s"$dir/phospho_events.yml",
+                    s"$dir/ubiq_events.yml",
+                    s"$dir/hydrox_events.yml",
+                    s"$dir/bind_events.yml",
+                    s"$dir/exchange_events.yml",
+                    s"$dir/degrad_events.yml",
+                    s"$dir/transcription_events.yml",
+                    s"$dir/down_reg_events.yml",
+                    s"$dir/up_reg_events.yml")
     (files map readFile).mkString("\n\n")
   }
 
