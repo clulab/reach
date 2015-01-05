5f4ccb7eee8d12e9193b07c6b485228ecefab008
Mihai Surdeanu <surdeanu@gmail.com>
2015-01-04 19:32:42 -0700

    Added nicer printouts to unit tests.

--- src/test/scala/edu/arizona/sista/bionlp/reach/ruler/TestDarpaRules.scala
+++ src/test/scala/edu/arizona/sista/bionlp/reach/ruler/TestDarpaRules.scala
@@ -77,6 +77,7 @@ class TestDarpaRules extends AssertionsForJUnit {
   @Test def testRules1() {
     val doc = proc.annotate("As expected based on previous studies, wild- type K-Ras bound primarily 32P-GDP, while G12V-Ras bound 32P-GTP (Fig.2, A and B).")
     val mentions = extractor.extractFrom(doc)
+    println("testRules1 ==>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
     RuleShell.displayMentions(mentions, doc)
     assertTrue(hasEventWithArguments("Binding", List("K-Ras"), mentions)) // TODO: add 32P-GDP (MARCO)
     assertTrue(hasEventWithArguments("Binding", List("G12V-Ras"), mentions)) // TODO: add 32P-GTP (MARCO)
@@ -85,6 +86,7 @@ class TestDarpaRules extends AssertionsForJUnit {
   @Test def testRules2() {
     val doc = proc.annotate("Copyright notice and Disclaimer Abstract GTP loaded Ras induces multiple signaling pathways by binding to its numerous effectors such as Raf and PI3K.")
     val mentions = extractor.extractFrom(doc)
+    println("testRules2 ==>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
     RuleShell.displayMentions(mentions, doc)
     assertTrue(hasEventWithArguments("Binding", List("Raf", "PI3K", "Ras"), mentions))
   }
@@ -92,6 +94,7 @@ class TestDarpaRules extends AssertionsForJUnit {
   @Test def testRules3() {
     val doc = proc.annotate("We hypothesized that MEK inhibition activates AKT by inhibiting ERK activity, which blocks an inhibitory threonine phosphorylation on the JM domains of EGFR and HER2, thereby increasing ERBB3 phosphorylation.")
     val mentions = extractor.extractFrom(doc)
+    println("testRules3 ==>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
     RuleShell.displayMentions(mentions, doc)
 
     assertTrue(hasEventWithArguments("Phosphorylation", List("EGFR"), mentions))
@@ -102,6 +105,7 @@ class TestDarpaRules extends AssertionsForJUnit {
   @Test def testRules4() {
     val doc = proc.annotate("We hypothesized that MEK inhibition activates AKT by inhibiting ERK activity, which blocks an inhibitory threonine phosphorylation on the JM domains of EGFR and HER2, thereby increasing ERBB3 phosphorylation.")
     val mentions = extractor.extractFrom(doc)
+    println("testRules4 ==>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
     RuleShell.displayMentions(mentions, doc)
     assertTrue(hasEntity("ERK", mentions))
     assertTrue(hasEntity("EGFR", mentions))
@@ -115,6 +119,7 @@ class TestDarpaRules extends AssertionsForJUnit {
   @Test def testRules5() {
     val doc = proc.annotate("To test this hypothesis, we transiently transfected CHO-KI cells, which do not express ERBB receptors endogenously, with wildtype ERBB3 with either wild-type EGFR or EGFR T669A.")
     val mentions = extractor.extractFrom(doc)
+    println("testRules5 ==>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
     RuleShell.displayMentions(mentions, doc)
     assertTrue(hasEntity("ERBB receptors", mentions))
     assertTrue(hasEntity("ERBB3", mentions))
@@ -126,6 +131,7 @@ class TestDarpaRules extends AssertionsForJUnit {
   @Test def testRules6() {
     val doc = proc.annotate("We observed analogous results in CHO-KI cells expressing wild-type ERBB3 in combination with wild-type or T677A mutant HER2 (Figure 6B)")
     val mentions = extractor.extractFrom(doc)
+    println("testRules6 ==>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
     RuleShell.displayMentions(mentions, doc)
 
     assertTrue(hasEntityWithSite("HER2", "T677A mutant", mentions))
@@ -134,6 +140,7 @@ class TestDarpaRules extends AssertionsForJUnit {
   @Test def testRules7() {
     val doc = proc.annotate("ERK negatively regulates the epidermal growth factor mediated interaction of Gab1 and the phosphatidylinositol 3-kinase.")
     val mentions = extractor.extractFrom(doc)
+    println("testRules7 ==>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
     RuleShell.displayMentions(mentions, doc)
 
     // TODO: this fails (MARCO)
@@ -143,6 +150,7 @@ class TestDarpaRules extends AssertionsForJUnit {
   @Test def testRules8() {
     val doc = proc.annotate("Figure 3 Raf and PI3K bind more to ubiquitinated Ras than to non- ubiquitinated Ras To examine whether the binding of ubiquitinated K-Ras to Raf and PI3K inhibits or can actually enhance their kinase activity, both total G12V-K-Ras and the ubiquitinated subfraction of G12V-K-Ras were purified from cell lysates and subjected to an in vitro kinase (I.V.K.) assay (Fig. 4A).")
     val mentions = extractor.extractFrom(doc)
+    println("testRules8 ==>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
     RuleShell.displayMentions(mentions, doc)
     assertTrue(hasEventWithArguments("Binding", List("Raf", "PI3K", "K-Ras"), mentions))
   }
@@ -150,6 +158,7 @@ class TestDarpaRules extends AssertionsForJUnit {
   @Test def testRules9() {
     val doc = proc.annotate("Figure 5 MEK inhibition blocks phosphorylation of a direct ERK target site in the conserved JM domains of EGFR and HER2 We hypothesized that the MEK/ERK pathway may suppress trans-phosphorylation of ERBB3 by directly phosphorylating the JM domains of EGFR and HER2, and that this could be a dominant MEK inhibitor induced feedback leading to AKT activation in these cancers.")
     val mentions = extractor.extractFrom(doc)
+    println("testRules9 ==>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
     RuleShell.displayMentions(mentions, doc)
 
     assertTrue(hasEntityWithSite("HER2", "JM domains", mentions))
@@ -163,6 +172,7 @@ class TestDarpaRules extends AssertionsForJUnit {
   @Test def testRules10() {
     val doc = proc.annotate("For example, ERK- mediated serine phosphorylation of the GAB1 adaptor has been shown to negatively regulate GAB1-PI3K binding and downstream AKT signaling")
     val mentions = extractor.extractFrom(doc)
+    println("testRules10 ==>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
     RuleShell.displayMentions(mentions, doc)
 
     assertTrue(hasEventWithArguments("Phosphorylation", List("GAB1 adaptor"), mentions))
@@ -174,6 +184,7 @@ class TestDarpaRules extends AssertionsForJUnit {
   @Test def testRules11() {
     val doc = proc.annotate("In contrast, the EGFR T669A mutant increased both basal EGFR and ERBB3 tyrosine phosphorylation that was not augmented by MEK inhibition")
     val mentions = extractor.extractFrom(doc)
+    println("testRules11 ==>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
     RuleShell.displayMentions(mentions, doc)
 
     assertTrue(hasEventWithArguments("Phosphorylation", List("EGFR"), mentions))
@@ -183,6 +194,7 @@ class TestDarpaRules extends AssertionsForJUnit {
   @Test def testRules12() {
     val doc = proc.annotate("To address the effect of K-Ras ubiquitination on its binding to PI3K and Raf family members, either total G12V-K-Ras or the ubiquitinated subfraction of G12V-K-Ras was immunoprecipitated and the immunoprecipitates were probed with antibodies to detect associated Ras effector molecules.")
     val mentions = extractor.extractFrom(doc)
+    println("testRules12 ==>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
     RuleShell.displayMentions(mentions, doc)
 
     // TODO: this fails due to weird rule (GUS)
@@ -195,6 +207,7 @@ class TestDarpaRules extends AssertionsForJUnit {
   @Test def testRules13() {
     val doc = proc.annotate("We observed increased ERBB3 binding to PI3K following MEK inhibition (Figure 1D), and accordingly, MEK inhibition substantially increased tyrosine phosphorylated ERBB3 levels (Figure 1A).")
     val mentions = extractor.extractFrom(doc)
+    println("testRules13 ==>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
     RuleShell.displayMentions(mentions, doc)
 
     // TODO: this fails, missing PI3K (MARCO)
@@ -206,6 +219,7 @@ class TestDarpaRules extends AssertionsForJUnit {
   @Test def testRules14() {
     val doc = proc.annotate("We propose that once ubiquitination occurs on Ras at Lys147, it enhances GDP/GTP exchange of Ras and increases the fraction of Ras in the GTP-form (Fig. 6B).")
     val mentions = extractor.extractFrom(doc)
+    println("testRules14 ==>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
     RuleShell.displayMentions(mentions, doc)
 
     // TODO: this fails, missing Ras (DANE)
