3c8cb3070e44c855f3ea2767773de5c22aa7c9f8
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2015-01-08 20:00:16 -0700

    updated shell

--- src/main/scala/edu/arizona/sista/bionlp/reach/ruler/RulerShell.scala
+++ src/main/scala/edu/arizona/sista/bionlp/reach/ruler/RulerShell.scala
@@ -2,7 +2,6 @@ package edu.arizona.sista.bionlp.reach.ruler
 
 import jline.console.ConsoleReader
 import jline.console.history.MemoryHistory
-import scala.util.control.Breaks._
 import edu.arizona.sista.processors.bionlp.BioNLPProcessor
 
 object RulerShell extends App {
@@ -10,8 +9,7 @@ object RulerShell extends App {
 
   val proc = new BioNLPProcessor
 
-
-  def createBasicRuler:BasicRuler = {
+  def createBasicRuler: BasicRuler = {
     val entityRules = BasicRuler.readEntityRules(shell=true)
     val ruleArgIndex = args.indexOf("--rules")
     val eventRules = if (ruleArgIndex == -1) BasicRuler.readEventRules(shell=true) else BasicRuler.readFile(args(ruleArgIndex + 1))
@@ -26,34 +24,45 @@ object RulerShell extends App {
   reader.setPrompt(">>> ")
   reader.setHistory(new MemoryHistory)
 
-  val commands = Map("%reload" -> "reload rules",
+  val commands = Map(
+    "%reload" -> "reload rules",
     "%help" -> "show commands",
-    "%exit" -> "exit system")
+    "%exit" -> "exit system"
+  )
 
-  def displayCommands = commands.foreach { case (k, v) => println(s"\t$k\t=>\t$v")}
+  def displayCommands() = commands foreach {
+    case (k, v) => println(s"\t$k\t=>\t$v")
+  }
 
   println("Welcome to RulerShell")
-  displayCommands
+  displayCommands()
 
-  breakable {
-    while (true) {
-      val text = reader.readLine
-      text match {
-        case reload if text == "%reload" => {
-          println("reloading RulerShell...")
+  var running = true
+
+  while (running) {
+    reader.readLine match {
+      case "%reload" =>
+        println("reloading RulerShell...")
+        try {
           basicRuler = createBasicRuler
+        } catch {
+          case e: Throwable => println("Error reloading RulerShell. Please check your rules and try again.")
         }
-        case exit if text == "%exit" => break
-        case help if text == "%help" => {
-          println("\tCOMMANDS:")
-          displayCommands
-        }
-        case _ => {
-          val doc = proc.annotate(text)
-          val mentions = basicRuler.extractFrom(doc)
-          displayMentions(mentions, doc)
-        }
-      }
+
+      case "%exit" =>
+        running = false
+
+      case "%help" =>
+        println("\tCOMMANDS:")
+        displayCommands()
+
+      case null =>
+        println("\nPlease type %exit to quit.")
+
+      case text =>
+        val doc = proc.annotate(text)
+        val mentions = basicRuler.extractFrom(doc)
+        displayMentions(mentions, doc)
     }
   }
 }
