248cf2c254d602c490cc9fb7d507bab013dfb4e1
Tom Hicks <hickst@tohono.com>
2015-05-03 17:35:38 -0700

    FriesDriver: Redo file handling. Rewrite flow more like bionlp.

--- src/main/scala/edu/arizona/sista/odin/extern/export/fries/FriesDriver.scala
+++ src/main/scala/edu/arizona/sista/odin/extern/export/fries/FriesDriver.scala
@@ -5,7 +5,9 @@ import java.io._
 import edu.arizona.sista.processors.{DocumentSerializer, Document}
 import edu.arizona.sista.processors.bionlp.BioNLPProcessor
 import edu.arizona.sista.odin._
-import edu.arizona.sista.odin.domains.bigmechanism.dryrun2015.{DarpaActions,Ruler}
+import edu.arizona.sista.odin.domains.bigmechanism.dryrun2015.Ruler.readRules
+import edu.arizona.sista.odin.domains.bigmechanism.dryrun2015.DarpaActions
+import edu.arizona.sista.odin.domains.bigmechanism.summer2015.{ LocalGrounder, Coref }
 import edu.arizona.sista.odin.extern.inward._
 
 import org.slf4j.LoggerFactory
@@ -13,19 +15,20 @@ import org.slf4j.LoggerFactory
 /**
   * Top-level test driver for Fries output development.
   *   Author: by Tom Hicks. 4/30/2015.
-  *   Last Modified: Fake doc ID in document.
+  *   Last Modified: Redo file handling. Rewrite flow more like bionlp.
   */
 object FriesDriver extends App {
   val logger = LoggerFactory.getLogger(this.getClass.getSimpleName)
 
-  val entityRules = Ruler.readEntityRules()
-  val eventRules = Ruler.readEventRules()
-  val rules = entityRules + "\n\n" + eventRules
-
   val ds = new DocumentSerializer
-  val actions = new DarpaActions
   val processor = new BioNLPProcessor()
-  val extractor = new Ruler(rules, actions)
+
+  val rules = readRules()
+  val actions = new DarpaActions
+  val grounder = new LocalGrounder
+  val coref = new Coref
+  val flow = grounder andThen coref
+  val engine = ExtractorEngine(rules, actions, flow.apply)
 
   val PapersDir = s"${System.getProperty("user.dir")}/src/test/resources/papers/"
   val paperNames = Seq(
@@ -47,7 +50,6 @@ object FriesDriver extends App {
 
   def getText(fileName: String):String = scala.io.Source.fromFile(fileName).mkString
 
-  // val outDir = s"${System.getProperty("java.io.tmpdir")}" + File.separator
   val outDir = s"${System.getProperty("user.dir")}" + File.separator
   def mkOutputName (paper:String, ext:String): String = {
     outDir + {"""^.*?/|.txt.ser""".r.replaceAllIn(paper, "")} + ext
@@ -60,16 +62,17 @@ object FriesDriver extends App {
   def processPaper (paper: String, asStrings:Boolean=false) = {
     val ext = if (asStrings) ".txt" else ".json"
     val outName = mkOutputName(paper, ext)
-    val outFile = new FileOutputStream(new File(outName))
+    val outFile = new File(outName)
     val inFile = s"$PapersDir/$paper"
 
     val doc = paper match {
       case ser if ser.endsWith("ser") => docFromSerializedFile(inFile)
-      case _ => processor.annotate(getText(inFile))
+      case _ => processor.annotate(getText(inFile), keepText=true)
     }
     doc.id = Some(paper)                    // fake document ID for now
 
-    val mentions = extractor.extractFrom(doc)
+    // val mentions = extractor.extractFrom(doc)
+    val mentions = engine.extractFrom(doc)
     val sortedMentions = mentions.sortBy(m => (m.sentence, m.start)) // sort by sentence, start idx
     if (asStrings)
       outputEventMentions(sortedMentions, doc, outFile)
@@ -78,9 +81,9 @@ object FriesDriver extends App {
   }
 
   /** Output string representations for the given sequence of mentions. */
-  def outputAllMentions (mentions:Seq[Mention], doc:Document, fos:FileOutputStream) = {
-    val out:PrintWriter = new PrintWriter(new BufferedWriter(new OutputStreamWriter(fos)))
-    val menMgr:MentionManager = new MentionManager()
+  def outputAllMentions (mentions:Seq[Mention], doc:Document, outFile:File) = {
+    val out = new PrintWriter(new BufferedWriter(new FileWriter(outFile)))
+    val menMgr = new MentionManager()
     mentions.foreach { m =>
       menMgr.mentionToStrings(m).foreach { str => out.println(str) }
     }
@@ -89,17 +92,15 @@ object FriesDriver extends App {
   }
 
   /** Output a FRIES representation for the given sequence of mentions. */
-  def outputFries (mentions:Seq[Mention], doc:Document, out:FileOutputStream) = {
-    val frier:FriesOutput = new FriesOutput()
-    frier.toJSON(mentions, doc, out)
-    out.flush()
-    out.close()
+  def outputFries (mentions:Seq[Mention], doc:Document, outFile:File) = {
+    val frier = new FriesOutput()
+    frier.toJSON(mentions, doc, outFile)
   }
 
   /** Output string representations for event mentions in the given sequence. */
-  def outputEventMentions (mentions:Seq[Mention], doc:Document, fos:FileOutputStream) = {
-    val menMgr:MentionManager = new MentionManager()
-    menMgr.outputSelectedMentions("Event", mentions, fos)
+  def outputEventMentions (mentions:Seq[Mention], doc:Document, outFile:File) = {
+    val menMgr = new MentionManager()
+    menMgr.outputSelectedMentions("Event", mentions, outFile)
   }
 
 
