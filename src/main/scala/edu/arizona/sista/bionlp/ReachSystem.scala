68477378e1cc2eb12307c92b214ffb2e1c951122
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2015-05-08 13:50:04 -0700

    copied dryrun code

--- src/main/scala/edu/arizona/sista/bionlp/ReachSystem.scala
+++ src/main/scala/edu/arizona/sista/bionlp/ReachSystem.scala
@@ -1,13 +1,14 @@
 package edu.arizona.sista.bionlp
 
+import java.io.File
 import edu.arizona.sista.odin._
-import edu.arizona.sista.odin.domains.bigmechanism.dryrun2015.Ruler.readRules
-import edu.arizona.sista.odin.domains.bigmechanism.dryrun2015.DarpaActions
 import edu.arizona.sista.odin.domains.bigmechanism.summer2015.{ LocalGrounder, Coref }
 import edu.arizona.sista.processors.Document
 import edu.arizona.sista.processors.bionlp.BioNLPProcessor
 
 class ReachSystem {
+  import ReachSystem._
+
   // read rule files
   val rules = readRules()
   // initialize actions object
@@ -38,5 +39,45 @@ class ReachSystem {
     require(doc.id.isDefined, "document must have an id")
     require(doc.text.isDefined, "document should keep original text")
     engine.extractFrom(doc)
+  }
+
+
+}
+
+object ReachSystem {
+  val resourcesDir = "/edu/arizona/sista/odin/domains/bigmechanism/dryrun2015"
+
+  def readRules(): String =
+    readEntityRules() + "\n\n" + readEventRules()
+
+  def readEntityRules(): String = {
+    val files = Seq(
+      s"$resourcesDir/custom_entities.yml",
+      s"$resourcesDir/model_entities.yml")
+    files map readResource mkString "\n\n"
+  }
+
+  def readEventRules(): String = {
+    val files = Seq(
+      s"$resourcesDir/phospho_events.yml",
+      s"$resourcesDir/ubiq_events.yml",
+      s"$resourcesDir/hydrox_events.yml",
+      s"$resourcesDir/hydrolysis_events.yml",
+      s"$resourcesDir/bind_events.yml",
+      s"$resourcesDir/exchange_events.yml",
+      s"$resourcesDir/degrad_events.yml",
+      s"$resourcesDir/transcription_events.yml",
+      s"$resourcesDir/regulation_events.yml",
+      s"$resourcesDir/neg_reg_events.yml",
+      s"$resourcesDir/pos_reg_events.yml",
+      s"$resourcesDir/transport_events.yml")
+    files map readResource mkString "\n\n"
+  }
+
+  def readResource(filename: String) = {
+    val source = io.Source.fromURL(getClass.getResource(filename))
+    val data = source.mkString
+    source.close()
+    data
   }
 }
