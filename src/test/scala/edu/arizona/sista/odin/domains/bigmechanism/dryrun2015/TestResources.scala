469bf20ec3786a5bc39ee81e2736b0b713449cd6
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2015-05-12 13:53:59 -0700

    porting tests

--- src/test/scala/edu/arizona/sista/odin/domains/bigmechanism/dryrun2015/TestResources.scala
+++ src/test/scala/edu/arizona/sista/odin/domains/bigmechanism/dryrun2015/TestResources.scala
@@ -1,6 +1,6 @@
 package edu.arizona.sista.odin.domains.bigmechanism.dryrun2015
 
-import edu.arizona.sista.processors.bionlp.BioNLPProcessor
+import edu.arizona.sista.bionlp.ReachSystem
 
 /**
  * Makes sure resource are findable by BioNLPProcessor
@@ -10,19 +10,7 @@ class TestResources {
 }
 
 object TestResources {
-  val bioproc = {
-    val proc = new BioNLPProcessor
-    proc.annotate("Test sentence") // To trick BANNER into not failing...
-    proc
-  }
-
-  val extractor = mkExtractor
-
-  def mkExtractor = {
-    val actions = new DarpaActions
-    val rules = Ruler.readRules()
-    new Ruler(rules, actions)
-  }
+  val reach = new ReachSystem
 
   def summarizeError(sentence: String, label: String, assignedParty: String): String =
     s"Failed ${label} test for sentence:\n\tWe measured transcription activation in the presence of ASPP2, which is phosphorylated by Ras.\n\tResponsible: ${assignedParty}"
