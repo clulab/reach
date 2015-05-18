1ca844971f7c4d8719176c363be2a8bd151bf198
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2015-05-18 16:05:38 -0700

    added Negation modification

--- src/main/scala/edu/arizona/sista/bionlp/mentions/Modifications.scala
+++ src/main/scala/edu/arizona/sista/bionlp/mentions/Modifications.scala
@@ -1,16 +1,15 @@
 package edu.arizona.sista.bionlp.mentions
 
-import scala.collection.mutable
 import edu.arizona.sista.odin.Mention
 
 trait Modifications {
   this: Mention =>
-  val modifications = new mutable.HashSet[Modification]
+  var modifications = Set.empty[Modification]
 
-  def isModified:Boolean = modifications.size > 0
+  def isModified: Boolean = modifications.nonEmpty
 }
 
-trait Modification {
+sealed trait Modification {
   // modifications should at least have a label that explains
   // what kind of modification they are
   def label: String
@@ -21,18 +20,19 @@ case class PTM(
   evidence: Option[Mention] = None,
   site: Option[Mention] = None
 ) extends Modification {
-  override def toString:String = {
+  override def toString: String = {
     val b = new StringBuilder()
     b.append(label)
-    if(site.isDefined)
+    if (site.isDefined)
       b.append(" @ " + site.get.text)
     b.toString()
   }
 }
 
-case class EventSite(
-  label: String = "UnknownEvent",
-  site: Mention) extends Modification {
-  override def toString:String =
-    s"$label@${site.text}"
+case class EventSite(site: Mention) extends Modification {
+  val label = "EventSite"
+}
+
+case class Negation(evidence: Mention) extends Modification {
+  val label = "Negation"
 }
