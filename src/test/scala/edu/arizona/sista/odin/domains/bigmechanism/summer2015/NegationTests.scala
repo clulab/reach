179764fceaa8c4b9d422a8ccbc09fab83e9952a0
enoriega <enrique.noriega@gmail.com>
2015-05-21 17:03:54 -0700

    Improved the Negation tests

--- src/test/scala/edu/arizona/sista/odin/domains/bigmechanism/summer2015/NegationTests.scala
+++ src/test/scala/edu/arizona/sista/odin/domains/bigmechanism/summer2015/NegationTests.scala
@@ -1,39 +1,177 @@
 package edu.arizona.sista.odin.domains.bigmechanism.summer2015
 
 import org.scalatest.{Matchers, FlatSpec}
-import edu.arizona.sista.bionlp.mentions.BioEventMention
-import edu.arizona.sista.bionlp.mentions.Negation
+import edu.arizona.sista.bionlp.mentions._
 
 import TestUtils._
 
 class NegationTests extends FlatSpec with Matchers{
-  val sentences = Seq(
-    "RAS does not phosphorylate MEK",
-    "RAS doesn't phosphorylate MEK",
-    "RAS is not phosphorylating MEK",
-    "RAS isn't phosphorylating MEK",
-    "RAS fails to phosphorylate MEK",
-    "RAS fails phosphorylating MEK",
-    "RAS plays no role in the phosphorylation of MEK",
-    "RAS plays little role in the phosphorylation of MEK")
 
-    for(sentence <- sentences){
-      sentence should "contain an event with a negation mofification" in {
-        val mentions = parseSentence(sentence)
+    def getNegations(mention:BioMention) = mention.modifications filter {
+      case mod:Negation => true
+      case _ => false
+    }
 
-        val events = mentions filter {
-          case event:BioEventMention =>
-            true
-          case _ =>
-            false
-        }
 
-        val modifications = events flatMap (_.modifications) filter {
-          case m:Negation => true
-          case _ => false
-        }
+    val sen1 = "RAS does not phosphorylate MEK"
 
-        modifications should have size (1)
-      }
+    sen1 should "contain a mention with a negation modification" in {
+      val mentions = parseSentence(sen1)
+
+      mentions filter (_ matches "Event") should have size (2)
+
+      val pos_reg = mentions filter (_ matches ("Positive_regulation"))
+      pos_reg should have size (1)
+      getNegations(pos_reg.head) should have size (1)
+
+      val phospho = mentions filter (_ matches ("Phosphorylation"))
+      phospho should have size (1)
+      getNegations(phospho.head) should have size (0)
+
+    }
+
+    val sen2 = "RAS doesn't phosphorylate MEK"
+
+    sen2 should "contain a mention with a negation modification" in {
+      val mentions = parseSentence(sen2)
+
+      mentions filter (_ matches "Event") should have size (2)
+
+      val pos_reg = mentions filter (_ matches ("Positive_regulation"))
+      pos_reg should have size (1)
+      getNegations(pos_reg.head) should have size (1)
+
+      val phospho = mentions filter (_ matches ("Phosphorylation"))
+      phospho should have size (1)
+      getNegations(phospho.head) should have size (0)
+
+    }
+
+    val sen3 = "RAS is not phosphorylating MEK"
+
+    sen3 should "contain a mention with a negation modification" in {
+      val mentions = parseSentence(sen3)
+
+      mentions filter (_ matches "Event") should have size (2)
+
+      val pos_reg = mentions filter (_ matches ("Positive_regulation"))
+      pos_reg should have size (1)
+      getNegations(pos_reg.head) should have size (1)
+
+      val phospho = mentions filter (_ matches ("Phosphorylation"))
+      phospho should have size (1)
+      getNegations(phospho.head) should have size (0)
+
+    }
+
+    val sen4 = "RAS isn't phosphorylating MEK"
+
+    sen4 should "contain a mention with a negation modification" in {
+      val mentions = parseSentence(sen4)
+
+      mentions filter (_ matches "Event") should have size (2)
+
+      val pos_reg = mentions filter (_ matches ("Positive_regulation"))
+      pos_reg should have size (1)
+      getNegations(pos_reg.head) should have size (1)
+
+      val phospho = mentions filter (_ matches ("Phosphorylation"))
+      phospho should have size (1)
+      getNegations(phospho.head) should have size (0)
+
+    }
+
+    val sen5 = "RAS wasn't phosphorylated"
+
+    sen5 should "contain a mention with a negation modification" in {
+      val mentions = parseSentence(sen5)
+
+      mentions filter (_ matches "Event") should have size (1)
+
+      val phospho = mentions filter (_ matches "Phosphorylation")
+      phospho should have size (1)
+      getNegations(phospho.head) should have size (1)
+    }
+
+    val sen6 = "RAS fails to phosphorylate MEK"
+
+    sen6 should "contain a mention with a negation modification" in {
+      val mentions = parseSentence(sen6)
+
+      mentions filter (_ matches "Event") should have size (2)
+
+      val pos_reg = mentions filter (_ matches ("Positive_regulation"))
+      pos_reg should have size (1)
+      getNegations(pos_reg.head) should have size (1)
+
+      val phospho = mentions filter (_ matches "Phosphorylation")
+      phospho should have size (1)
+      getNegations(phospho.head) should have size (0)
+    }
+
+    val sen7 = "RAS fails phosphorylating MEK"
+
+    sen7 should "contain a mention with a negation modification" in {
+      val mentions = parseSentence(sen7)
+
+      mentions filter (_ matches "Event") should have size (2)
+
+      val pos_reg = mentions filter (_ matches ("Positive_regulation"))
+      pos_reg should have size (1)
+      getNegations(pos_reg.head) should have size (1)
+
+      val phospho = mentions filter (_ matches "Phosphorylation")
+      phospho should have size (1)
+      getNegations(phospho.head) should have size (0)
+    }
+
+    val sen8 = "RAS plays no role in the phosphorylation of MEK"
+
+    sen8 should "contain a mention with a negation modification" in {
+      val mentions = parseSentence(sen8)
+
+      mentions filter (_ matches "Event") should have size (2)
+
+      val pos_reg = mentions filter (_ matches ("Positive_regulation"))
+      pos_reg should have size (1)
+      getNegations(pos_reg.head) should have size (1)
+
+      val phospho = mentions filter (_ matches "Phosphorylation")
+      phospho should have size (1)
+      getNegations(phospho.head) should have size (0)
+    }
+
+    val sen9 = "RAS plays little role in the phosphorylation of MEK"
+
+    sen9 should "contain a mention with a negation modification" in {
+      val mentions = parseSentence(sen9)
+
+      mentions filter (_ matches "Event") should have size (2)
+
+      val pos_reg = mentions filter (_ matches ("Positive_regulation"))
+      pos_reg should have size (1)
+      getNegations(pos_reg.head) should have size (1)
+
+      val phospho = mentions filter (_ matches "Phosphorylation")
+      phospho should have size (1)
+      getNegations(phospho.head) should have size (0)
+    }
+
+    // Negative tests
+
+    val sen10 = "RAS phosphorylates MEK"
+
+    sen10 should "not contain a mention with a negation modification" in {
+      val mentions = parseSentence(sen10)
+
+      mentions filter (_ matches "Event") should have size (2)
+
+      val pos_reg = mentions filter (_ matches ("Positive_regulation"))
+      pos_reg should have size (1)
+      getNegations(pos_reg.head) should have size (0)
+
+      val phospho = mentions filter (_ matches "Phosphorylation")
+      phospho should have size (1)
+      getNegations(phospho.head) should have size (0)
     }
 }
