7eed0e26c2ce3da097becbdc1eb0e065bf2fa17f
Mihai Surdeanu <surdeanu@gmail.com>
2015-05-25 18:27:29 -0700

    Commented out export code affected by the bug.

--- src/main/scala/edu/arizona/sista/odin/extern/export/hans/HansOutput.scala
+++ src/main/scala/edu/arizona/sista/odin/extern/export/hans/HansOutput.scala
@@ -175,6 +175,8 @@ class HansOutput extends JsonOutputter {
       }
     }
 
+    // TODO: reenable this block, after fixing the missing argument bug
+    /*
     // now, print all regulation events, which control the above events
     for(mention <- eventMentions) {
       if(REGULATION_EVENTS.contains(mention.label)) {
@@ -184,6 +186,7 @@ class HansOutput extends JsonOutputter {
         frames += mkEventMention(paperId, passageMeta, mention.toBioMention, entityMap, eventMap)
       }
     }
+    */
 
     // write the JSON to the given file
     writeJsonToFile(model, outFile)
