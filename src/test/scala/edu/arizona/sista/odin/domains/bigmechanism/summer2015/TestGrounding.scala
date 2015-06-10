eb423966b7246761bb467409b2b22297f9e81b4e
Gus Hahn-Powell <gushahnpowell@gmail.com>
2015-06-10 12:10:56 -0700

    Typo.

--- src/test/scala/edu/arizona/sista/odin/domains/bigmechanism/summer2015/TestGrounding.scala
+++ src/test/scala/edu/arizona/sista/odin/domains/bigmechanism/summer2015/TestGrounding.scala
@@ -7,7 +7,7 @@ import TestUtils._
 /**
  * Unit tests to ensure grounding is working properly
  */
-class TestModifications extends FlatSpec with Matchers {
+class TestGrounding extends FlatSpec with Matchers {
 
   val grounding1 = "p65 and NF-kappaB p65 are the same entity."
   grounding1 should "contain two entities with the same grounding ID: Xref(uniprot,P21579)" in {
