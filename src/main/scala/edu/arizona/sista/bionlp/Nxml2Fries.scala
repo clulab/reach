b8e872a67463a2614d71305197891864a247aa5b
enoriega <enrique.noriega@gmail.com>
2015-08-19 17:37:09 -0700

    Added a runnable class to output tsv files to the txt dir

--- src/main/scala/edu/arizona/sista/bionlp/Nxml2Fries.scala
+++ src/main/scala/edu/arizona/sista/bionlp/Nxml2Fries.scala
@@ -46,7 +46,11 @@ case class FriesEntry(
   sectionId: String,
   sectionName: String,
   isTitle: Boolean,
-  text: String)
+  text: String) {
+
+  override def toString(): String =  s"$chunkId\t$sectionName\t$sectionId\t${if(isTitle) 1 else 0}\t$text" 
+
+  }
 
 class Nxml2FriesException(msg: String, val entry: String)
     extends Exception(msg)
