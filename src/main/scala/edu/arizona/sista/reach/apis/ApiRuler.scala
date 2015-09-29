5badf27741345d230f67362af22dadd5d54b6188
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2015-09-29 16:28:33 -0700

    modified api to return all sections in a single response

--- src/main/scala/edu/arizona/sista/reach/apis/ApiRuler.scala
+++ src/main/scala/edu/arizona/sista/reach/apis/ApiRuler.scala
@@ -3,6 +3,7 @@ package edu.arizona.sista.reach.apis
 import java.util.{Date, List => JList, Map => JMap}
 
 import com.typesafe.config.ConfigFactory
+import edu.arizona.sista.odin.Mention
 import edu.arizona.sista.reach._
 import edu.arizona.sista.reach.extern.export.fries.FriesOutput
 import edu.arizona.sista.reach.nxml.{FriesEntry, NxmlReader}
@@ -10,6 +11,9 @@ import edu.arizona.sista.reach.nxml.{FriesEntry, NxmlReader}
 import scala.collection.JavaConverters._
 
 class ApiRuler {
+
+  // a response is a heterogeneous java Map from String to either String or Boolean
+  type Response = JMap[String, Any]
 
   // read configuration
   val config = ConfigFactory.load()
@@ -27,29 +31,36 @@ class ApiRuler {
   val prefix = "webapi"
   val suffix = "reach"
 
-  // extracts raw text from given nxml and returns a list of results
-  // (nxml is splitted into sections and each section is handled independently)
-  def annotateNxml(nxml: String): JList[JMap[String, Any]] = {
-    val maps = reader.readNxml(nxml, prefix) map annotateEntry
-    maps.asJava
+  // extracts raw text from given nxml and returns a response with all the mentions
+  def annotateNxml(nxml: String): Response = {
+    val entries = reader.readNxml(nxml, prefix)
+    mkResponse(entries, entries flatMap extractMentions)
   }
 
-  // annotates a single FriesEntry and returns the results in a Java Map
-  def annotateEntry(entry: FriesEntry): JMap[String, Any] = {
+  // annotates a single FriesEntry and returns a response
+  def annotateEntry(entry: FriesEntry): Response =
+    mkResponse(Seq(entry), extractMentions(entry))
+
+  // annotates some text by converting it to a FriesEntry and calling annotateEntry()
+  def annotateText(text: String, docId: String = prefix, chunkId: String = suffix): Response = {
+    annotateEntry(FriesEntry(docId, chunkId, "NoSection", "NoSection", false, text))
+  }
+
+  // extracts the mentions from a FriesEntry
+  def extractMentions(entry: FriesEntry): Seq[Mention] =
+    reach.extractFrom(entry).toList
+
+  // gets a sequence of FriesEntries and their extracted mentions (by reference) and constructs a Response
+  def mkResponse(entries: Seq[FriesEntry], lazyMentions: => Seq[Mention]): Response = {
     try {
       val startTime = new Date()
-      val mentions = reach.extractFrom(entry).toList
+      val mentions = lazyMentions
       val endTime = new Date()
-      val json = outputter.toJSON("FakePaperID", mentions, Seq(entry), startTime, endTime, prefix)
+      val json = outputter.toJSON("FakePaperID", mentions, entries, startTime, endTime, prefix)
       Map("resultJson" -> json, "hasError" -> false).asJava
     } catch {
       case e: Exception => Map("resultJson" -> "", "hasError" -> true, "errorMessage" -> e.getMessage).asJava
     }
-  }
-
-  // annotates some text by converting it to a FriesEntry and calling annotateEntry()
-  def annotateText(text: String, docId: String = prefix, chunkId: String = suffix): JMap[String, Any] = {
-    annotateEntry(FriesEntry(docId, chunkId, "NoSection", "NoSection", false, text))
   }
 
 }
