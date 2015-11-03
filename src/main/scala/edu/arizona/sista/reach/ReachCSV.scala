06835beb1462ca2199aa6b83d0d664470a7739a9
Mihai Surdeanu <surdeanu@gmail.com>
2015-11-03 12:33:56 -0700

    Added ReachCSV, which runs the Reach system on data in CSV format.

--- src/main/scala/edu/arizona/sista/reach/ReachCSV.scala
+++ src/main/scala/edu/arizona/sista/reach/ReachCSV.scala
@@ -1,8 +1,11 @@
 package edu.arizona.sista.reach
 
 import java.io.File
+import java.util.Date
 
 import com.typesafe.config.ConfigFactory
+import edu.arizona.sista.reach.extern.export.indexcards.IndexCardOutput
+import edu.arizona.sista.reach.nxml.FriesEntry
 import edu.arizona.sista.reach.utils.CSVParser
 
 /**
@@ -14,35 +17,69 @@ object ReachCSV extends App {
   //
   // Customize these for various CSV files
   //
-  val EXPECTED_COLUMN_COUNT = 4
-  val DOCID_COLUMN = 0
-  val CHUNKID_COLUMN = 1
-  val TEXT_COLUMN = 2
-  val HAS_HEADER = true
 
   // use specified config file or the default one if one is not provided
   val config =
     if (args.isEmpty) ConfigFactory.load()
     else ConfigFactory.parseFile(new File(args(0))).resolve()
 
+  // arguments
   val csvFile = new File(config.getString("csv"))
+  val outputDir = config.getString("output")
+  val docIdColumn = config.getInt("docIdColumn")
+  val chunkIdColumn = config.getInt("chunkIdColumn")
+  val textColumn = config.getInt("textColumn")
+  val hasHeader = config.getBoolean("hasHeader")
 
   val reach = new ReachSystem
   val csvParser = new CSVParser
 
   var count = 0
-  for(line <- csvParser.split(csvFile, removeHeader = HAS_HEADER)) {
+  for(line <- csvParser.split(csvFile, removeHeader = hasHeader)) {
     //println(s"""Parsing line: ${line.mkString("; ")}""")
-    assert(line.length == EXPECTED_COLUMN_COUNT)
-    val text = line(TEXT_COLUMN)
-    val docId = line(DOCID_COLUMN)
-    val chunkId = line(CHUNKID_COLUMN)
+    val text = line(textColumn)
+    val docId = line(docIdColumn)
+    val chunkId = line(chunkIdColumn)
+    val entry = FriesEntry(docId, chunkId, "", "", isTitle = false, text) // we have no section information for this CSV data
 
-    val mentions = reach.extractFrom(text, docId, chunkId)
+    try {
+      val startTime = now
+      val mentions = reach.extractFrom(entry)
+      val endTime = now
+
+      val outputtter = new IndexCardOutput
+      outputtter.writeJSON(
+        docId + "_" + chunkId,
+        mentions,
+        List(entry),
+        startTime, endTime,
+        outputDir + File.separator + docId + "_" + chunkId
+      )
+
+    } catch {
+      case e: Exception =>
+        val report = s"""
+                      |==========
+                      |
+                      | ¡¡¡ extraction error !!!
+                      |
+                      |paper: $docId
+                      |chunk: ${entry.chunkId}
+                      |section: ${entry.sectionId}
+                      |
+                      |error:
+                      |${e.toString}
+                      |
+                      |stack trace:
+                      |${e.getStackTrace.mkString("\n")}
+                      |
+                      |==========
+                      |""".stripMargin
+    }
 
     count += 1
     println(s"Processed $count lines.")
   }
 
-
+  def now = new Date()
 }
