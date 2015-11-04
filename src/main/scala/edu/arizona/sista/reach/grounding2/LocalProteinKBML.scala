8690a0fc10a1eec7f094726bed4e5e7aeaaf1326
Tom Hicks <hickst@tohono.com>
2015-11-03 21:27:18 -0700

    Implement alternative key lookups: untested.

--- src/main/scala/edu/arizona/sista/reach/grounding2/LocalProteinKBML.scala
+++ src/main/scala/edu/arizona/sista/reach/grounding2/LocalProteinKBML.scala
@@ -5,36 +5,69 @@ import edu.arizona.sista.odin._
 /**
   * Trait implementing override logic for knowledge bases which try alternate resolutions.
   *   Written by Tom Hicks. 11/03/2015.
-  *   Last Modified: Initial creation.
+  *   Last Modified: Implement alternative key lookups: untested.
   */
 trait LocalProteinKBML extends LocalKBMentionLookup {
 
-  def tryKeyAndAlternates (key:String, alternates:Seq[String]): Option[KBEntry] = {
-    memoryKB.lookup(key) orElse {
-      alternates.foreach { altKey =>
-        val entry = memoryKB.lookup(altKey)
-        if (entry.isDefined) return entry
-      }
-      return None
+  private def lookups (allKeys:Seq[String]): Option[KBEntry] = {
+    allKeys.foreach { key =>
+      val entry = memoryKB.lookup(key)
+      if (entry.isDefined) return entry
     }
+    return None                             // tried all keys: no success
   }
 
   override def resolve (text:String): Option[KBResolution] = {
-    val key = makeCanonicalKey(text)        // make a lookup key from the given text
-    val altKeys = makeAlternateKeys(key, KBKeyTransforms.proteinKeyTransforms)
-    memoryKB.newResolution(tryKeyAndAlternates(key, altKeys))
+    val key = makeCanonicalKey(text)
+    val allKeys = key +: makeAlternateKeys(key, KBKeyTransforms.proteinKeyTransforms)
+    memoryKB.newResolution(lookups(allKeys))
+  }
+
+
+  private def lookupsByASpecies (allKeys:Seq[String], species:String): Option[KBEntry] = {
+    allKeys.foreach { key =>
+      val entry = memoryKB.lookupByASpecies(key, species)
+      if (entry.isDefined) return entry
+    }
+    return None                             // tried all keys: no success
   }
 
   override def resolveByASpecies (text:String, species:String): Option[KBResolution] = {
-    return None                             // IMPLEMENT LATER
+    val key = makeCanonicalKey(text)
+    val allKeys = key +: makeAlternateKeys(key, KBKeyTransforms.proteinKeyTransforms)
+    memoryKB.newResolution(lookupsByASpecies(allKeys, species))
+  }
+
+
+  private def lookupsBySpecies (allKeys:Seq[String], speciesSet:SpeciesNameSet): Option[Iterable[KBEntry]] = {
+    allKeys.foreach { key =>
+      val entries = memoryKB.lookupBySpecies(key, speciesSet)
+      if (entries.isDefined) return entries
+    }
+    return None                             // tried all keys: no success
   }
 
   override def resolveBySpecies (text:String, speciesSet:SpeciesNameSet): Option[Iterable[KBResolution]] = {
-    return None                             // IMPLEMENT LATER
+    val key = makeCanonicalKey(text)
+    val allKeys = key +: makeAlternateKeys(key, KBKeyTransforms.proteinKeyTransforms)
+    val entries = lookupsBySpecies(allKeys, speciesSet)
+    entries.map(_.map(kbe => memoryKB.newResolution(kbe)))
+  }
+
+
+  private def lookupsHuman (allKeys:Seq[String]): Option[Iterable[KBEntry]] = {
+    allKeys.foreach { key =>
+      val entries = memoryKB.lookupHuman(key)
+      if (entries.isDefined) return entries
+    }
+    return None                             // tried all keys: no success
   }
 
   override def resolveHuman (text:String): Option[KBResolution] = {
-    return None                             // IMPLEMENT LATER
+    val key = makeCanonicalKey(text)
+    val allKeys = key +: makeAlternateKeys(key, KBKeyTransforms.proteinKeyTransforms)
+    val entries = lookupsHuman(allKeys)                   // find matching entries
+    memoryKB.newResolution(entries.flatMap(_.headOption)) // return first entry or None
   }
 
 }
