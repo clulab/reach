b60b8e0270d5b4c9f4b44d2523b7ce2d1a4c060e
Tom Hicks <hickst@tohono.com>
2015-11-04 18:29:08 -0700

    Make alternate keys transform LC text. Add test suites for alt key resolves.

--- src/main/scala/edu/arizona/sista/reach/grounding2/LocalProteinKBML.scala
+++ src/main/scala/edu/arizona/sista/reach/grounding2/LocalProteinKBML.scala
@@ -5,10 +5,46 @@ import edu.arizona.sista.odin._
 /**
   * Trait implementing override logic for knowledge bases which try alternate resolutions.
   *   Written by Tom Hicks. 11/03/2015.
-  *   Last Modified: Update for key transforms class rename.
+  *   Last Modified: Transform lower cased text instead of keys.
   */
 trait LocalProteinKBML extends LocalKBMentionLookup {
 
+  /** Override default lookup to try alternate key lookups. */
+  override def resolve (text:String): Option[KBResolution] = {
+    val key = makeCanonicalKey(text)
+    val lcKey = text.toLowerCase            // transform lower cased keys
+    val allKeys = key +: makeAlternateKeys(lcKey, LocalKBKeyTransforms.proteinKeyTransforms)
+    memoryKB.newResolution(lookups(allKeys))
+  }
+
+  /** Override default lookup to try alternate key lookups. */
+  override def resolveByASpecies (text:String, species:String): Option[KBResolution] = {
+    val key = makeCanonicalKey(text)
+    val lcKey = text.toLowerCase            // transform lower cased keys
+    val allKeys = key +: makeAlternateKeys(lcKey, LocalKBKeyTransforms.proteinKeyTransforms)
+    memoryKB.newResolution(lookupsByASpecies(allKeys, species))
+  }
+
+  /** Override default lookup to try alternate key lookups. */
+  override def resolveBySpecies (text:String, speciesSet:SpeciesNameSet): Option[Iterable[KBResolution]] = {
+    val key = makeCanonicalKey(text)
+    val lcKey = text.toLowerCase            // transform lower cased keys
+    val allKeys = key +: makeAlternateKeys(lcKey, LocalKBKeyTransforms.proteinKeyTransforms)
+    val entries = lookupsBySpecies(allKeys, speciesSet)
+    entries.map(_.map(kbe => memoryKB.newResolution(kbe)))
+  }
+
+  /** Override default lookup to try alternate key lookups. */
+  override def resolveHuman (text:String): Option[KBResolution] = {
+    val key = makeCanonicalKey(text)
+    val lcKey = text.toLowerCase            // transform lower cased keys
+    val allKeys = key +: makeAlternateKeys(lcKey, LocalKBKeyTransforms.proteinKeyTransforms)
+    val entries = lookupsHuman(allKeys)                   // find matching entries
+    memoryKB.newResolution(entries.flatMap(_.headOption)) // return first entry or None
+  }
+
+
+  /** Try lookup for all keys until one succeeds for all fail. */
   private def lookups (allKeys:Seq[String]): Option[KBEntry] = {
     allKeys.foreach { key =>
       val entry = memoryKB.lookup(key)
@@ -17,13 +53,7 @@ trait LocalProteinKBML extends LocalKBMentionLookup {
     return None                             // tried all keys: no success
   }
 
-  override def resolve (text:String): Option[KBResolution] = {
-    val key = makeCanonicalKey(text)
-    val allKeys = key +: makeAlternateKeys(key, LocalKBKeyTransforms.proteinKeyTransforms)
-    memoryKB.newResolution(lookups(allKeys))
-  }
-
-
+  /** Try lookup for all keys until one succeeds for all fail. */
   private def lookupsByASpecies (allKeys:Seq[String], species:String): Option[KBEntry] = {
     allKeys.foreach { key =>
       val entry = memoryKB.lookupByASpecies(key, species)
@@ -32,13 +62,7 @@ trait LocalProteinKBML extends LocalKBMentionLookup {
     return None                             // tried all keys: no success
   }
 
-  override def resolveByASpecies (text:String, species:String): Option[KBResolution] = {
-    val key = makeCanonicalKey(text)
-    val allKeys = key +: makeAlternateKeys(key, LocalKBKeyTransforms.proteinKeyTransforms)
-    memoryKB.newResolution(lookupsByASpecies(allKeys, species))
-  }
-
-
+  /** Try lookup for all keys until one succeeds for all fail. */
   private def lookupsBySpecies (allKeys:Seq[String], speciesSet:SpeciesNameSet): Option[Iterable[KBEntry]] = {
     allKeys.foreach { key =>
       val entries = memoryKB.lookupBySpecies(key, speciesSet)
@@ -47,27 +71,13 @@ trait LocalProteinKBML extends LocalKBMentionLookup {
     return None                             // tried all keys: no success
   }
 
-  override def resolveBySpecies (text:String, speciesSet:SpeciesNameSet): Option[Iterable[KBResolution]] = {
-    val key = makeCanonicalKey(text)
-    val allKeys = key +: makeAlternateKeys(key, LocalKBKeyTransforms.proteinKeyTransforms)
-    val entries = lookupsBySpecies(allKeys, speciesSet)
-    entries.map(_.map(kbe => memoryKB.newResolution(kbe)))
-  }
-
-
+  /** Try lookup for all keys until one succeeds for all fail. */
   private def lookupsHuman (allKeys:Seq[String]): Option[Iterable[KBEntry]] = {
     allKeys.foreach { key =>
       val entries = memoryKB.lookupHuman(key)
       if (entries.isDefined) return entries
     }
     return None                             // tried all keys: no success
-  }
-
-  override def resolveHuman (text:String): Option[KBResolution] = {
-    val key = makeCanonicalKey(text)
-    val allKeys = key +: makeAlternateKeys(key, LocalKBKeyTransforms.proteinKeyTransforms)
-    val entries = lookupsHuman(allKeys)                   // find matching entries
-    memoryKB.newResolution(entries.flatMap(_.headOption)) // return first entry or None
   }
 
 }
--- src/test/scala/edu/arizona/sista/reach/TestAltResolutions.scala
+++ src/test/scala/edu/arizona/sista/reach/TestAltResolutions.scala
@@ -16,45 +16,149 @@ class TestAltResolutions extends FlatSpec with Matchers {
 
   println(imkb4)                            // REMOVE LATER
 
-  "LocalProteinKBML resolve" should "work without using alternate lookups" in {
-    (imkb4.resolve("NOT-IN-KB").isDefined) should be (false) // not in KB
-    (imkb4.resolve("PTHR21244").isDefined) should be (false) // uppercase
-    (imkb4.resolve("not-in-kb").isDefined) should be (false) // not it KB
-    (imkb4.resolve("pthr21244").isDefined) should be (false) // has a species
-    (imkb4.resolve("hk").isDefined) should be (false)        // has a species
+  // this KB includes species, therefore plain resolve should always fail:
+  "LocalProteinKBML resolve" should "fail despite alternate lookups" in {
+    // key not in KB:
+    (imkb4.resolve("NOTINKB").isDefined) should be (false)
+    (imkb4.resolve("notinkb").isDefined) should be (false)
+    (imkb4.resolve("notinkb_human").isDefined) should be (false)
+    (imkb4.resolve("notinkb protein").isDefined) should be (false)
+    (imkb4.resolve("notinkb family").isDefined) should be (false)
+    // entry has a species:
+    (imkb4.resolve("PTHR21244").isDefined) should be (false)
+    (imkb4.resolve("pthr21244").isDefined) should be (false)
+    (imkb4.resolve("pthr21244_human").isDefined) should be (false)
+    (imkb4.resolve("pthr21244 protein").isDefined) should be (false)
+    (imkb4.resolve("pthr21244 family").isDefined) should be (false)
+    (imkb4.resolve("mutant-pthr21244").isDefined) should be (false)
+    (imkb4.resolve("hk").isDefined) should be (false)
+    (imkb4.resolve("hk_human").isDefined) should be (false)
+    (imkb4.resolve("hk protein").isDefined) should be (false)
+    (imkb4.resolve("hk family").isDefined) should be (false)
+    (imkb4.resolve("mutant-hk").isDefined) should be (false)
   }
 
-  "LocalProteinKBML resolveByASpecies" should "work without using alternate lookups" in {
-    (imkb4.resolveByASpecies("NOT-IN-KB", "human").isDefined) should be (false) // not in KB
+  "LocalProteinKBML resolveByASpecies" should "fail despite alternate lookups" in {
+    // key not in KB:
+    (imkb4.resolveByASpecies("NotInKB", "ant").isDefined) should be (false)
+    (imkb4.resolveByASpecies("NotInKB_human", "ant").isDefined) should be (false)
+    (imkb4.resolveByASpecies("NotInKB protein", "ant").isDefined) should be (false)
+    (imkb4.resolveByASpecies("NotInKB family", "ant").isDefined) should be (false)
+    (imkb4.resolveByASpecies("mutant-NotInKB", "ant").isDefined) should be (false)
+    // entry does not have this species:
+    (imkb4.resolveByASpecies("hk", "frog").isDefined) should be (false)
+    (imkb4.resolveByASpecies("hk_human", "frog").isDefined) should be (false)
+    (imkb4.resolveByASpecies("hk protein", "frog").isDefined) should be (false)
+    (imkb4.resolveByASpecies("hk family", "frog").isDefined) should be (false)
+    (imkb4.resolveByASpecies("mutant-hk", "frog").isDefined) should be (false)
+  }
+
+  "LocalProteinKBML resolveByASpecies" should "work with alternate lookups" in {
     (imkb4.resolveByASpecies("pthr21244", "human").isDefined) should be (true)
-    (imkb4.resolveByASpecies("PTHR21244", "human").isDefined) should be (true)
-    (imkb4.resolveByASpecies("pthr21244", "HUMAN").isDefined) should be (true)
-    (imkb4.resolveByASpecies("pthr21244", "mouse").isDefined) should be (true)
-    (imkb4.resolveByASpecies("pthr21244", "aardvark").isDefined) should be (false) // not in KB
+    (imkb4.resolveByASpecies("pthr21244_human", "human").isDefined) should be (true)
+    (imkb4.resolveByASpecies("pthr21244 protein", "human").isDefined) should be (true)
+    (imkb4.resolveByASpecies("PTHR21244 protein", "human").isDefined) should be (true)
+    (imkb4.resolveByASpecies("pthr21244 family", "human").isDefined) should be (true)
+    (imkb4.resolveByASpecies("mutant-pthr21244", "human").isDefined) should be (true)
     (imkb4.resolveByASpecies("hk", "saccharomyces cerevisiae").isDefined) should be (true)
+    (imkb4.resolveByASpecies("hk_human", "saccharomyces cerevisiae").isDefined) should be (true)
+    (imkb4.resolveByASpecies("hk protein", "saccharomyces cerevisiae").isDefined) should be (true)
+    (imkb4.resolveByASpecies("hk family", "saccharomyces cerevisiae").isDefined) should be (true)
+    (imkb4.resolveByASpecies("mutant-hk", "saccharomyces cerevisiae").isDefined) should be (true)
   }
 
-  "LocalProteinKBML resolveBySpecies" should "work without using alternate lookups" in {
-    (imkb4.resolveBySpecies("pthr21244", Set("aardvark")).isDefined) should be (false)
-    (imkb4.resolveBySpecies("pthr21244", Set("human")).isDefined) should be (true)
-    val pt = imkb4.resolveBySpecies("pthr21244", Set("human", "mouse"))
-    (pt.isDefined) should be (true)
-    (pt.get.size == 2) should be (true)
-    (imkb4.resolveBySpecies("pthr21244", Set("human","mouse","gorilla")).isDefined) should be (true)
-    val pt2 = (imkb4.resolveBySpecies("pthr21244", Set("human", "mouse","gorilla")))
-    (pt2.isDefined) should be (true)
-    (pt2.get.size == 2) should be (true)
-    (imkb4.resolveBySpecies("hk", Set("human", "mouse")).isDefined) should be (false)
+  val setA =   Set("aardvark")
+  val setF =   Set("frog")
+  val setH =   Set("human")
+  val setHM =  Set("human", "mouse")
+  val setHMG = Set("human", "mouse", "gorilla")
+  "LocalProteinKBML resolveBySpecies" should "should fail despite alternate lookups" in {
+    // key not in KB:
+    (imkb4.resolveBySpecies("NotInKB", setA).isDefined) should be (false)
+    (imkb4.resolveBySpecies("NotInKB_human", setA).isDefined) should be (false)
+    (imkb4.resolveBySpecies("NotInKB protein", setA).isDefined) should be (false)
+    (imkb4.resolveBySpecies("NotInKB family", setA).isDefined) should be (false)
+    (imkb4.resolveBySpecies("mutant-NotInKB", setA).isDefined) should be (false)
+    (imkb4.resolveBySpecies("pthr21244 mouse", setH).isDefined) should be (false)
+    // entry does not have this species:
+    (imkb4.resolveBySpecies("pthr21244", setF).isDefined) should be (false)
+    (imkb4.resolveBySpecies("pthr21244_human", setF).isDefined) should be (false)
+    (imkb4.resolveBySpecies("pthr21244 protein", setF).isDefined) should be (false)
+    (imkb4.resolveBySpecies("pthr21244 family", setF).isDefined) should be (false)
+    (imkb4.resolveBySpecies("mutant-pthr21244", setF).isDefined) should be (false)
+    // entry does not have these species (yeast only):
+    (imkb4.resolveBySpecies("hk", setHM).isDefined) should be (false)
+    (imkb4.resolveBySpecies("hk_human", setHM).isDefined) should be (false)
+    (imkb4.resolveBySpecies("hk protein", setHM).isDefined) should be (false)
+    (imkb4.resolveBySpecies("hk family", setHM).isDefined) should be (false)
+    (imkb4.resolveBySpecies("mutant-hk", setHM).isDefined) should be (false)
+  }
+
+  "LocalProteinKBML resolveBySpecies" should "work with alternate lookups" in {
+    (imkb4.resolveBySpecies("pthr21244", setH).isDefined) should be (true)
+    (imkb4.resolveBySpecies("PTHR21244", setH).isDefined) should be (true)
+    (imkb4.resolveBySpecies("pthr21244_human", setH).isDefined) should be (true)
+    (imkb4.resolveBySpecies("PTHR21244_human", setH).isDefined) should be (true)
+    (imkb4.resolveBySpecies("pthr21244 protein", setH).isDefined) should be (true)
+    (imkb4.resolveBySpecies("PTHR21244 protein", setH).isDefined) should be (true)
+    (imkb4.resolveBySpecies("pthr21244 family", setH).isDefined) should be (true)
+    (imkb4.resolveBySpecies("PTHR21244 family", setH).isDefined) should be (true)
+    (imkb4.resolveBySpecies("mutant-pthr21244", setH).isDefined) should be (true)
+    (imkb4.resolveBySpecies("MUTANT-PTHR21244", setH).isDefined) should be (true)
+
+    (imkb4.resolveBySpecies("pthr21244", setHM).isDefined) should be (true)
+    (imkb4.resolveBySpecies("pthr21244", setHM).get.size == 2) should be (true)
+    (imkb4.resolveBySpecies("pthr21244_human", setHM).isDefined) should be (true)
+    (imkb4.resolveBySpecies("pthr21244_human", setHM).get.size == 2) should be (true)
+    (imkb4.resolveBySpecies("pthr21244 protein", setHM).isDefined) should be (true)
+    (imkb4.resolveBySpecies("pthr21244 protein", setHM).get.size == 2) should be (true)
+    (imkb4.resolveBySpecies("pthr21244 family", setHM).isDefined) should be (true)
+    (imkb4.resolveBySpecies("pthr21244 family", setHM).get.size == 2) should be (true)
+    (imkb4.resolveBySpecies("mutant-pthr21244", setHM).isDefined) should be (true)
+    (imkb4.resolveBySpecies("mutant-pthr21244", setHM).get.size == 2) should be (true)
+
+    (imkb4.resolveBySpecies("pthr21244", setHMG).isDefined) should be (true)
+    (imkb4.resolveBySpecies("pthr21244", setHMG).get.size == 2) should be (true)
+    (imkb4.resolveBySpecies("pthr21244_human", setHMG).isDefined) should be (true)
+    (imkb4.resolveBySpecies("pthr21244_human", setHMG).get.size == 2) should be (true)
+    (imkb4.resolveBySpecies("pthr21244 protein", setHMG).isDefined) should be (true)
+    (imkb4.resolveBySpecies("pthr21244 protein", setHMG).get.size == 2) should be (true)
+    (imkb4.resolveBySpecies("pthr21244 family", setHMG).isDefined) should be (true)
+    (imkb4.resolveBySpecies("pthr21244 family", setHMG).get.size == 2) should be (true)
+    (imkb4.resolveBySpecies("mutant-pthr21244", setHMG).isDefined) should be (true)
+    (imkb4.resolveBySpecies("mutant-pthr21244", setHMG).get.size == 2) should be (true)
+
     (imkb4.resolveBySpecies("hk", Set("saccharomyces cerevisiae", "ant")).isDefined) should be (true)
     (imkb4.resolveBySpecies("hk", Set("ant", "saccharomyces cerevisiae")).isDefined) should be (true)
-    (imkb4.resolveBySpecies(
-      "hk", Set("ant", "saccharomyces cerevisiae")).get.size == 1) should be (true)
+    (imkb4.resolveBySpecies("hk", Set("ant", "saccharomyces cerevisiae")).get.size == 1) should be (true)
   }
 
-  "LocalProteinKBML resolveHuman" should "work without using alternate lookups" in {
-    (imkb4.resolveHuman("hk").isDefined) should be (false) // yeast only
-    (imkb4.resolveHuman("PTHR21244").isDefined) should be (true)
+  "LocalProteinKBML resolveHuman" should "fail despite alternate lookups" in {
+    // key not in KB:
+    (imkb4.resolveHuman("NotInKB").isDefined) should be (false)
+    (imkb4.resolveHuman("NotInKB_human").isDefined) should be (false)
+    (imkb4.resolveHuman("NotInKB protein").isDefined) should be (false)
+    (imkb4.resolveHuman("NotInKB family").isDefined) should be (false)
+    (imkb4.resolveHuman("mutant-NotInKB").isDefined) should be (false)
+    // entry does not have human species (yeast only):
+    (imkb4.resolveHuman("hk").isDefined) should be (false)
+    (imkb4.resolveHuman("hk_human").isDefined) should be (false)
+    (imkb4.resolveHuman("hk protein").isDefined) should be (false)
+    (imkb4.resolveHuman("hk family").isDefined) should be (false)
+    (imkb4.resolveHuman("mutant-hk").isDefined) should be (false)
+  }
+
+  "LocalProteinKBML resolveHuman" should "work with alternate lookups" in {
     (imkb4.resolveHuman("pthr21244").isDefined) should be (true)
+    (imkb4.resolveHuman("PTHR21244").isDefined) should be (true)
+    (imkb4.resolveHuman("pthr21244_human").isDefined) should be (true)
+    (imkb4.resolveHuman("PTHR21244_human").isDefined) should be (true)
+    (imkb4.resolveHuman("pthr21244 protein").isDefined) should be (true)
+    (imkb4.resolveHuman("PTHR21244 protein").isDefined) should be (true)
+    (imkb4.resolveHuman("pthr21244 family").isDefined) should be (true)
+    (imkb4.resolveHuman("PTHR21244 family").isDefined) should be (true)
+    (imkb4.resolveHuman("mutant-pthr21244").isDefined) should be (true)
+    (imkb4.resolveHuman("mutant-PTHR21244").isDefined) should be (true)
   }
 
 }
