58982ebacf8df2ec4c78c216293a46d94065d631
Tom Hicks <hickst@tohono.com>
2015-11-10 13:38:55 -0700

    Split KB tests into basic and AZ failsafe.

--- src/test/scala/edu/arizona/sista/reach/TestBasicKBs.scala
+++ src/test/scala/edu/arizona/sista/reach/TestBasicKBs.scala
@@ -7,9 +7,9 @@ import edu.arizona.sista.reach.grounding2._
 /**
   * Unit tests to ensure the in-memory KB is working for grounding.
   *   Written by: Tom Hicks. 10/26/2015.
-  *   Last Modified: Update for IMKB has species argument.
+  *   Last Modified: Rename this class. Split off AZ failsafe KB tests.
   */
-class TestIMKB extends FlatSpec with Matchers {
+class TestBasicKBs extends FlatSpec with Matchers {
 
   // Tests of non-speciated (2-column) knowledge base
   val imkb2 = new InMemoryKB(
@@ -67,53 +67,6 @@ class TestIMKB extends FlatSpec with Matchers {
     (imkbPF.lookupHuman("hk").isDefined) should be (false) // yeast only
     (imkbPF.lookupHuman("PTHR21244").isDefined) should be (false)
     (imkbPF.lookupHuman("pthr21244").isDefined) should be (true)
-  }
-
-
-  // Tests of AZ Failsafe KB
-  val kbAZ = new AzFailsafeKBML
-  "AZ Failsafe KB" should "return new UAZ ID for new resolve, then repeat it" in {
-    val x3 = kbAZ.resolve("XXX")
-    (x3.isDefined) should be (true)
-    val xxx = x3.get
-    (kbAZ.resolve("XXX").get.id == xxx.id) should be (true)
-    (kbAZ.resolve("xxx").get.id == xxx.id) should be (true)
-    (kbAZ.resolve("XXXX").get.id == xxx.id) should be (false) // 4 Xs
-    (kbAZ.resolve("xxxx").get.id == xxx.id) should be (false) // 4 xs
-  }
-
-  "AZ Failsafe KB" should "return new UAZ ID for new resolve and resolveHuman" in {
-    val x3 = kbAZ.resolve("XXX")
-    (x3.isDefined) should be (true)
-    val xxx = x3.get
-    (kbAZ.resolveHuman("XXX").isDefined) should be (true)
-    (kbAZ.resolveHuman("XXX").get.id == xxx.id) should be (true)
-    (kbAZ.resolveHuman("xxx").isDefined) should be (true)
-    (kbAZ.resolveHuman("xxx").get.id == xxx.id) should be (true)
-  }
-
-  "AZ Failsafe KB" should "return new UAZ ID for new resolve and resolveByASpecies" in {
-    val x3 = kbAZ.resolve("XXX")
-    (x3.isDefined) should be (true)
-    val xxx = x3.get
-    (kbAZ.resolveByASpecies("XXX", "giraffe").isDefined) should be (true)
-    (kbAZ.resolveByASpecies("XXX", "giraffe").get.id == xxx.id) should be (true)
-    (kbAZ.resolveByASpecies("xxx", "giraffe").isDefined) should be (true)
-    (kbAZ.resolveByASpecies("xxx", "giraffe").get.id == xxx.id) should be (true)
-  }
-
-  "AZ Failsafe KB" should "return new UAZ ID for new resolve and resolveBySpecies" in {
-    val x3 = kbAZ.resolve("XXX")
-    (x3.isDefined) should be (true)
-    val xxx = x3.get
-    (kbAZ.resolveBySpecies("XXX", Set("giraffe")).isDefined) should be (true)
-    (kbAZ.resolveBySpecies("XXX", Set("giraffe")).get.head.id == xxx.id) should be (true)
-    (kbAZ.resolveBySpecies("XXX", Set("human", "mouse")).isDefined) should be (true)
-    (kbAZ.resolveBySpecies("XXX", Set("human", "mouse")).get.head.id == xxx.id) should be (true)
-    (kbAZ.resolveBySpecies("xxx", Set("giraffe")).isDefined) should be (true)
-    (kbAZ.resolveBySpecies("xxx", Set("giraffe")).get.head.id == xxx.id) should be (true)
-    (kbAZ.resolveBySpecies("xxx", Set("human", "mouse")).isDefined) should be (true)
-    (kbAZ.resolveBySpecies("xxx", Set("human", "mouse")).get.head.id == xxx.id) should be (true)
   }
 
 }
