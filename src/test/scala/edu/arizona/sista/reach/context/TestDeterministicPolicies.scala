859c6c6aef8fb6c49f71970c5d1d7f8677cbd48c
Enrique Noriega Atala <enrique.noriega@gmail.com>
2015-12-02 21:24:35 -0700

    Context unit tests progress

--- src/test/scala/edu/arizona/sista/reach/context/TestDeterministicPolicies.scala
+++ src/test/scala/edu/arizona/sista/reach/context/TestDeterministicPolicies.scala
@@ -1,15 +1,27 @@
-package edu.arizona.sista.reach.nxml
+package edu.arizona.sista.reach.context
 
 import io.Source
+import scala.collection.mutable
 import org.scalatest.{Matchers, FlatSpec}
+import edu.arizona.sista.reach.nxml.NxmlReader
+import edu.arizona.sista.reach.mentions._
 import edu.arizona.sista.reach.ReachSystem
 
 trait Fixtures {
   // Set up the fixtures
-  def nxml = Source.fromURL(getClass.getResource("/nxml/ContextTests.nxml")).mkString
+  def nxml = Source.fromURL(getClass.getResource("/inputs/nxml/PMC2597732.nxml")).mkString
   def reader = new NxmlReader
-  def entry = reader.readNxml(nxml, "ContextTests")
+  def entries = reader.readNxml(nxml, "ContextTests")
   def reachSystem = new ReachSystem
+
+  val mentions:Seq[BioEventMention] = reachSystem.extractFrom(entries).filter{
+      case em:BioEventMention => true
+      case _ => false
+    }.map(_.asInstanceOf[BioEventMention])
+
+  val context = mentions.map{
+      _.context.getOrElse(Map[String,Seq[String]]())
+  }
   /////////
 }
 
@@ -18,24 +30,28 @@ class DeterministicPoliciesTests extends FlatSpec with Matchers with Fixtures {
   // Tests
   behavior of "Deterministic policies"
 
-  it should "have two abstract entries with no titles in them" in {
-    info("Running reach over a paragraph")
-
-    val paragraph = entry(2)
-
+  it should "Have some context" in {
     // Remove/Change the filter if context is attached to other BioMentions
-    val mentions = reachSystem.extractFrom(Seq(paragraph)) filter {
-      case em:BioEventMention => true
-      case _ => false
-    }
 
-    info(s"The number of abstract entries is $size")
-    size should equal (2)
+    val size = context.flatMap(_.values.flatten).size
 
-    val titleSize = absEntries.filter(_.isTitle).size
-    info(s"The number of abstract entry titles is $titleSize")
-    titleSize should equal (0)
+    info(s"The number of context resolutions is: $size")
+    size should be > 0
+
   }
+
+  // No more than one context per type
+  it should "have no more than a context of each type simultaneously" in {
+      context foreach {
+            _ foreach {
+                keyVal =>
+                    keyVal._2.size should be <= 1
+            }
+      }
+  }
+  
+  // No more than 5 repetitions of the same context
+  // Mention where context starts
 
   ////////
 }
