3cabf2b8e9cf08feb7768f849b4a72907832e4d9
Tom Hicks <hickst@tohono.com>
2016-01-20 10:59:08 -0700

    Add shell script to run all grounding tests together.

---
+++ src/test/scala/edu/arizona/sista/reach/testGrounding.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#  Run all tests related to Grounding.
+#
+sbt 'test-only edu.arizona.sista.reach.TestKBSupport \
+edu.arizona.sista.reach.TestAzFailsafeKB.scala \
+edu.arizona.sista.reach.TestTsvKBs \
+edu.arizona.sista.reach.TestAdHocKBs \
+edu.arizona.sista.reach.TestProteinResolutions \
+edu.arizona.sista.reach.TestFamilyResolutions \
+edu.arizona.sista.reach.TestOrganCellTypeResolutions \
+edu.arizona.sista.reach.TestGrounding'
