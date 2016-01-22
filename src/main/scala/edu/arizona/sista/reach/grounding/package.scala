f2578d53497c24bb58ace01e0db0bcf94b918255
Tom Hicks <hickst@tohono.com>
2016-01-22 11:40:30 -0700

    Move IMKB types and defs to IMKB.

--- src/main/scala/edu/arizona/sista/reach/grounding/package.scala
+++ src/main/scala/edu/arizona/sista/reach/grounding/package.scala
@@ -3,17 +3,9 @@ package edu.arizona.sista.reach
 /**
   * Package object for grounding.
   *   Written by: Tom Hicks. 1/15/2016.
-  *   Last Modified: Initial creation by refactoring.
+  *   Last Modified: Move IMKB types and defs to IMKB.
   */
 package object grounding {
-
-  /** Type to map species name strings to KB entries for the same key. */
-  type SpeciesEntryMap = scala.collection.mutable.Map[String, KBEntry]
-  def  SpeciesEntryMap() = scala.collection.mutable.Map[String, KBEntry]()
-
-  /** A KB maps key strings to species-differentiated entries. */
-  type KnowledgeBase = scala.collection.mutable.Map[String, SpeciesEntryMap]
-  def  KnowledgeBase() = scala.collection.mutable.Map[String, SpeciesEntryMap]()
 
   /** KB MetaInfo is, at minimum, a map of string keys/values. */
   type KBMetaInfo = scala.collection.mutable.HashMap[String, String]
