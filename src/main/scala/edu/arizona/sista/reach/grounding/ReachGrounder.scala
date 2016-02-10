5b2be7190a06fdf90bc34b29fb1d04d190544657
Tom Hicks <hickst@tohono.com>
2016-02-09 19:53:41 -0700

    Add initial reach grounder2 stub.

---
+++ src/main/scala/edu/arizona/sista/reach/grounding/ReachGrounder.scala
@@ -0,0 +1,27 @@
+package edu.arizona.sista.reach.grounding
+
+import edu.arizona.sista.odin._
+import edu.arizona.sista.reach._
+import edu.arizona.sista.reach.mentions._
+import edu.arizona.sista.reach.grounding._
+
+/**
+  * Class which implements methods to select the final groundings for a mention sequence.
+  *   Written by Tom Hicks. 2/9/2016.
+  *   Last Modified: Initial creation.
+  */
+class ReachGrounder extends DarpaFlow {
+
+  /** Local implementation of darpa flow trait: use KB resolutions and context
+      to select and apply final grounding choice. */
+  def apply (mentions: Seq[Mention], state: State): Seq[Mention] = mentions map {
+    case tm: BioTextBoundMention => selectGrounding(tm, state)
+    case m => m
+  }
+
+  /** Select the final grounding for the given mention. */
+  private def selectGrounding (mention: BioMention, state: State): Mention = {
+    return mention                          // TODO: IMPLEMENT LATER
+  }
+
+}
