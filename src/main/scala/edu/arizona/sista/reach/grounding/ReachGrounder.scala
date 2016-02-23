d9ff3d7e80ef4afe5172d772ae0eb339a027d3c7
Tom Hicks <hickst@tohono.com>
2016-02-22 20:05:31 -0700

    ReachGrounder: WIP: Restrict to bio mentions. Dont extend darpa flow.

--- src/main/scala/edu/arizona/sista/reach/grounding/ReachGrounder.scala
+++ src/main/scala/edu/arizona/sista/reach/grounding/ReachGrounder.scala
@@ -4,32 +4,39 @@ import edu.arizona.sista.odin._
 import edu.arizona.sista.reach._
 import edu.arizona.sista.reach.context._
 import edu.arizona.sista.reach.mentions._
+import edu.arizona.sista.reach.extern.export.MentionManager
 
 /**
   * Class which implements methods to select the final groundings for a mention sequence.
   *   Written by Tom Hicks. 2/9/2016.
-  *   Last Modified: Remove spurious import.
+  *   Last Modified: Restrict to bio mentions. Dont extend darpa flow.
   */
-class ReachGrounder extends DarpaFlow {
+class ReachGrounder {
 
-  /** Local implementation of darpa flow trait: use KB resolutions and context
-      to select and apply final grounding choice. */
-  def apply (mentions: Seq[Mention], state: State): Seq[Mention] = mentions map {
-    case tm: BioTextBoundMention => selectGrounding(tm, state)
+  val mentionMgr = new MentionManager
+
+  /** Use candidate resolutions to select and apply final grounding choice. */
+  def apply (mentions: Seq[BioMention]): Seq[BioMention] = mentions map {
+    case tm: BioTextBoundMention => selectGrounding(tm)
     case m => m
   }
 
   /** Select the final grounding for the given mention. */
-  private def selectGrounding (mention: BioMention, state: State): Mention = {
-    assert(mention.isGrounded,              // SHOULD be grounded before this point
-           s"Mention '${mention}' should be grounded before final grounding selection.")
-
-    if (mention.hasCandidates && hasSpeciesContext(mention)) {
-      println(mention.context.get.get("Species")) // TODO: IMPLEMENT LATER
+  def selectGrounding (mention: BioMention): BioMention = {
+    if (mention.isGrounded) {
+      if (mention.hasCandidates && hasSpeciesContext(mention)) {
+        println(mention.context.get.get("Species")) // TODO: IMPLEMENT LATER
+      }
+      else mention.selectCurrentGrounding
     }
-    else mention.selectCurrentGrounding
 
     return mention
+  }
+
+  def printMention (mention:BioMention, sepStr:String="="): Unit = {
+    System.err.println(sepStr * 40)
+    mentionMgr.mentionToStrings(mention).foreach { System.err.println(_) }
+    System.err.println(sepStr * 40)
   }
 
 }
