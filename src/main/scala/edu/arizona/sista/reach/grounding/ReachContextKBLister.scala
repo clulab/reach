cf354ad13b06ebf937a67e757848f438ff5d4bbb
Tom Hicks <hickst@tohono.com>
2016-02-23 14:02:30 -0700

    Add cell component KBs as context KBs to context lister.

--- src/main/scala/edu/arizona/sista/reach/grounding/ReachContextKBLister.scala
+++ src/main/scala/edu/arizona/sista/reach/grounding/ReachContextKBLister.scala
@@ -5,11 +5,12 @@ import edu.arizona.sista.reach.grounding.ReachIMKBMentionLookups._
 /**
   * Object implementing logic to enumerate context related KB entries.
   *   Written by Tom Hicks. 2/19/2016.
-  *   Last Modified: Initial creation.
+  *   Last Modified: Add cell component KBs as context KBs.
   */
 object ReachContextKBLister {
   /** A sequence of the context related KB instances, whose values are to be listed. */
-  val ContextKBs = Seq( ContextCellLine, ContextCellType, ContextOrgan, ContextSpecies )
+  val ContextKBs = Seq( ContextCellLine, ContextCellType, ContextOrgan, ContextSpecies,
+                        StaticCellLocation, StaticCellLocation2, ModelGendCellLocation )
 
   /** Return a sequence of grounding information objects from the context related KBs. */
   def listContextKBs: Seq[ContextGrounding] = {
