83b0a4a58ad14901c60496c29230a86d33c119dc
Gus Hahn-Powell <gushahnpowell@gmail.com>
2016-03-02 20:59:54 -0700

    Documentation of AssemblyModification derivatives

--- src/main/scala/edu/arizona/sista/assembly/AssemblyModification.scala
+++ src/main/scala/edu/arizona/sista/assembly/AssemblyModification.scala
@@ -1,15 +1,29 @@
 package edu.arizona.sista.assembly
 
-
+/**
+ * Trait for modifications/features associated with an [[edu.arizona.sista.assembly.Entity]].
+ */
 trait AssemblyModification
 
+/**
+ * A representation of a post-translational modification (PTM) ( Phosphorylation, Ubiquitination, etc.) associated with an [[Entity]].
+ * @param label the ptm
+ * @param site an Option[String] representing the Site of the PTM
+ */
 // for keeping track of post-translational modifications and any associated site
 case class PTM(label: String, site: Option[String]) extends AssemblyModification {
   def this(label: String) = this(label, None)
 }
 
-// TODO: how to handle mutations at sites?
+/**
+ * A representation of a mutated form of an [[Entity]].
+ * Mutation sites, if present, are encoded in [[mutantType]].
+ * @param mutantType the type of mutation (see [[edu.arizona.sista.reach.mentions.Mutant.label]])
+ */
 case class MutantEntity(mutantType: String) extends AssemblyModification
 
-// Protein, GGP, etc
+/**
+ * The label associated with an [[Entity]] ( Protein, GGP, etc.)
+ * @param label the label associated with an [[Entity]]
+ */
 case class EntityLabel(label: String) extends AssemblyModification
