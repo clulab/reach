c32b7b2745affcf00e2548cab465a76d63c08164
Gus Hahn-Powell <gushahnpowell@gmail.com>
2016-03-06 22:50:30 -0700

    First pass at exporter for Assembly

---
+++ src/main/scala/edu/arizona/sista/assembly/AssemblyExporter.scala
@@ -0,0 +1,168 @@
+package edu.arizona.sista.assembly
+
+import edu.arizona.sista.odin.Mention
+import java.io.File
+import org.apache.commons.io.FileUtils
+
+case class Row(
+  input: String,
+  output: String,
+  controller: String,
+  precededBy: Seq[IDPointer],
+  evidence: Set[Mention]
+) {
+  // this might serve as a proxy for confidence, though
+  // it would also be good to know how many times this event
+  // was involved in other events (input, controller, etc)
+  val seen = evidence.size
+  // the set of paper ids where mentions of this event were found
+  val papers = evidence.map(_.document.id.getOrElse("UNKNOWN"))
+
+  // gets the most common span
+  def getTextualEvidence: String = {
+    evidence.toSeq.map(_.text).max
+  }
+
+  def toString(sep: String): String = {
+    val precedingEvents = precededBy.distinct.sorted.mkString(", ")
+    val seenIn = papers.toSeq.sorted.mkString(", ")
+    s"$input\t$output\t$controller\t$precedingEvents$seen$getTextualEvidence$seenIn"
+  }
+}
+
+class AssemblyExporter(am: AssemblyManager) {
+
+  import AssemblyExporter._
+
+  val distinctEERs = am.distinctEEReprs
+
+  // LUT for retrieving unique IDs
+  val EERLUT: Map[Int, IDPointer] = distinctEERs.zipWithIndex.map{
+    case pair =>
+      val repr: EntityEventRepresentation = pair._1
+      val id: Int = pair._2
+      (repr.equivalenceHash, id)
+  }.toMap
+
+  val grounding2Text: Map[GroundingID, String] = {
+    val pairs: Set[(GroundingID, String)] = for {
+      pair <- am.distinctSimpleEntitiesWithEvidence
+      entity: SimpleEntity = pair._1
+      evidence: Set[Mention] = pair._2
+      id: GroundingID = entity.id
+      text: String = evidence.map(_.text.toLowerCase).max
+    } yield (id, text)
+
+    pairs.toMap
+  }
+
+  def createInput(repr: EntityEventRepresentation): String = repr match {
+
+    case entity: SimpleEntity =>
+
+        // partition modifications
+        val (muts, other) =
+          entity.modifications.partition(_.isInstanceOf[MutantEntity])
+
+        // mutations
+        val mutations = muts.map(_.asInstanceOf[MutantEntity])
+
+        // construct mutant forms, if present
+        val mutantForms: String = mutations
+          .map(m => s"[${m.mutantType}]")
+          //ex. :[mutant1]:[mutant2]
+          .toSeq.mkString(":", ":", "")
+
+        // find PTMs
+        val ptms: Set[PTM] = other.filter(_.isInstanceOf[PTM])map(_.asInstanceOf[PTM])
+        val features: String = ptms
+          .map(getPTMrepresentation)
+          .mkString(".", ".","")
+
+        val text = grounding2Text.getOrElse(entity.id, entity.id)
+       s"$text$mutantForms$features"
+
+    case complex: Complex =>
+      complex.members.map(createInput).mkString(", ")
+
+    case se: SimpleEvent =>
+      se.input.values.flatten.map(createInput).mkString(", ")
+
+    case reg: Regulation =>
+      // get event IDs for each controlled
+      reg.controlled.map(c => EERLUT(c.equivalenceHash)).mkString(", ")
+  }
+
+  def createOutput(repr: EntityEventRepresentation): String = repr match {
+
+    case complex: Complex =>
+      complex.members.map(createInput).mkString("{", ", ", "}")
+
+    case se: SimpleEvent =>
+      se.output.map(createInput).mkString(", ")
+
+    case reg: Regulation =>
+      reg.controlled.map(createOutput).mkString(", ")
+
+    case _ => ""
+  }
+
+  def createController(repr: EntityEventRepresentation): String = repr match {
+
+    case reg: Regulation =>
+      reg.controller.map{ c =>
+        s"${createInput(c)}.${reg.polarity}"
+      }.mkString(", ")
+
+    case _ => ""
+
+  }
+
+  // TODO: implement a stub in AssemblyManager
+  def precededBy(repr: EntityEventRepresentation): Seq[IDPointer] = {
+    Nil
+  }
+
+  def writeTSV(outfile: String): Unit = {
+    val f = new File(outfile)
+    val text = getRows
+      .map(_.toString)
+      .toSeq
+      .sorted
+      .mkString("\n")
+    FileUtils.writeStringToFile(f, text)
+  }
+
+  // INPUT, OUTPUT, CONTROLLER, PRECEDED BY, EVENT ID, SEEN, EXAMPLE-TEXT
+  def getRows: Set[Row] = {
+
+      val rows: Set[Row] = distinctEERs
+        // ignore SimpleEntities not in other events
+        .filter(r => ! r.isInstanceOf[SimpleEntity])
+        .map { event =>
+          Row(
+            createInput(event),
+            createOutput(event),
+            createController(event),
+            precededBy(event),
+            event.evidence
+         )
+      }
+    rows
+    }
+}
+
+object AssemblyExporter {
+
+  val PTMLUT: Map[String, String] = Map(
+      "Phosphorylation" -> "p",
+      "Ubiquitination" -> "u"
+  )
+
+  def getPTMrepresentation(ptm: PTM): String = {
+    // attempt to retrieve the abberviated form of the label
+    // if the key is missing from the LUT,
+    // return the lowercase form of the first letter of the PTM's label
+    PTMLUT.getOrElse(ptm.label, ptm.label.toLowerCase.head.toString)
+  }
+}
