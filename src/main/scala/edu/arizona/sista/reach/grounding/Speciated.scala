1e63defec7fb23a88e6d2611f093d95e00a4ff7a
Tom Hicks <hickst@tohono.com>
2016-03-08 10:41:44 -0700

    Add species tests to KBResolution and Speciated.

--- src/main/scala/edu/arizona/sista/reach/grounding/Speciated.scala
+++ src/main/scala/edu/arizona/sista/reach/grounding/Speciated.scala
@@ -3,7 +3,7 @@ package edu.arizona.sista.reach.grounding
 /**
   * Trait for species features used in external knowledge bases.
   *   Written by Tom Hicks. 10/23/2015.
-  *   Last Modified: Add human default value string.
+  *   Last Modified: Add contains human species method.
   */
 trait Speciated {
 
@@ -14,16 +14,17 @@ trait Speciated {
   /** A set of label strings for humans, found in KBs. */
   val HumanLabels:SpeciesNameSet = SpeciesNameSet("homo sapiens", "human")
 
+  /** Tell whether the given sequence of species strings contain a human species label. */
+  def containsHumanSpecies (speciesSeq: Seq[String]): Boolean =
+    speciesSeq.exists(isHumanSpecies(_))
 
   /** Tell whether the given species string is label for humans or not. */
-  def isHumanSpecies (species: String): Boolean = {
+  def isHumanSpecies (species: String): Boolean =
     if (HumanLabels.contains(species.toLowerCase)) true else false
-  }
 
   /** Tell whether the given species string is a member of the given set of species. */
-  def isMemberOf (species: String, speciesSet:SpeciesNameSet): Boolean = {
+  def isMemberOf (species: String, speciesSet:SpeciesNameSet): Boolean =
     if (speciesSet.contains(species.toLowerCase)) true else false
-  }
 
 }
 
