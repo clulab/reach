2f7d973c9ddf4e858280dc8477e19318bdfa7e54
Tom Hicks <hickst@tohono.com>
2016-03-09 11:43:28 -0700

    Redo to propagate ambiguity.

--- src/main/scala/edu/arizona/sista/reach/grounding/ReachGrounder.scala
+++ src/main/scala/edu/arizona/sista/reach/grounding/ReachGrounder.scala
@@ -4,52 +4,82 @@ import edu.arizona.sista.odin._
 import edu.arizona.sista.reach._
 import edu.arizona.sista.reach.context._
 import edu.arizona.sista.reach.mentions._
+import edu.arizona.sista.reach.grounding.ReachKBUtils._
 import edu.arizona.sista.reach.extern.export.MentionManager
 
 /**
-  * Class which implements methods to select the final groundings for a mention sequence.
+  * Class which implements methods to select the best groundings for a sequence of mentions.
   *   Written by Tom Hicks. 2/9/2016.
-  *   Last Modified: Cleanups: use bio mention, simplify print mention.
+  *   Last Modified: Redo to propagate ambiguity.
   */
-class ReachGrounder {
+class ReachGrounder extends Speciated {
 
   val mentionMgr = new MentionManager
 
-  /** Use candidate resolutions to select and apply final grounding choice. */
-  def apply (mentions: Seq[BioMention]): Seq[BioMention] = {
-    mentions.flatMap {
-      case bem: BioEventMention => groundArguments(bem) // context can help event argument grounding
-      case bm:  BioMention =>               // other bio mentions can be finalized
-        if (bm.hasCandidates)               // if mention has any grounding candidates
-          bm.selectCurrentGrounding         // finalize the grounding
-        Seq(bm)
-      case m =>
-        Seq(m)                      // else just return the mention unchanged
+  /** Select and apply best grounding choice to a sequence of bio mentions. */
+  def apply (mentions: Seq[BioMention]): Seq[BioMention] = mentions.flatMap {
+    case bm: BioMention => groundMention(bm, Seq.empty[String])
+    case m => Seq(m)                  // else just return the mention unchanged
+  }
+
+  /** Dispatch the given bio mention for grounding, based on candidates and given species context. */
+  def groundMention (mention: BioMention, species: Seq[String]): Seq[BioMention] = {
+    mention match {
+      case bem: BioEventMention => groundArguments(bem)
+      case  bm: BioTextBoundMention =>
+        if (bm.hasMoreCandidates) {         // only redo grounding if more than one choice
+          // System.err.println(s"SPECIES=${species}") // REMOVE LATER
+          if (species.isEmpty || containsHumanSpecies(species))
+            groundAsHuman(bm)               // then prioritize human grounding
+          else                              // else context can influence this grounding
+            groundBySpecies(bm, species)
+        }
+        Seq(bm)                             // return the possibly modified mention
+      case m => Seq(m)                      // else just return the mention unchanged
     }
   }
 
-  def groundArguments (mention: BioEventMention): Seq[BioMention] = {
-    if (hasSpeciesContext(mention)) {       // for now, only using species to help grounding
-      val context = mention.context.get.get("Species").get
-      mention.arguments.values.flatten.toSeq.map(_.toBioMention).map {
-        case arg:BioMention => selectGrounding(context, arg)
-        case m => m
-      }
+  /** Recursively process arguments of given event, possibly setting new context environment. */
+  def groundArguments (event: BioEventMention): Seq[BioMention] = {
+    val args = event.arguments.values.flatten.toSeq.map(_.toBioMention)
+    if (hasSpeciesContext(event)) {       // for now, only using species to help grounding
+      val species = event.context.get.get("Species").get
+      args.flatMap(groundMention(_, species))
     }
-    else Seq(mention)                       // else return mention unchanged
+    else
+      args.flatMap(groundMention(_, Seq.empty[String]))
   }
 
-  /** Select the final grounding for the given mention, if it has grounding candidates. */
-  def selectGrounding (ctx: Seq[String], mention: BioMention): BioMention = {
-    if (mention.hasCandidates) {            // has candidates means it is grounded
-      if (mention.hasMoreCandidates) {
-        System.err.println(s"SEL-GROUNDING: ${ctx}") // REMOVE LATER
-        // TODO: IMPLEMENT selection based on species
-      }
-      else mention.selectCurrentGrounding
+  /** Prioritize the Grounding of the given mention as human.
+    * NB: Mention must be grounded and have more than one candidate. */
+  def groundAsHuman (mention: BioTextBoundMention): BioMention = {
+    if (mention.hasMoreCandidates) {        // sanity check
+      val cands = mention.candidates.get
+      val ordered = selectHuman(cands) ++ selectNoSpecies(cands) ++ selectNotHuman(cands)
+      mention.nominate(Some(ordered))
     }
+    // System.err.println("AS_HUMAN")          // REMOVE LATER
+    // printMention(mention)                   // REMOVE LATER
     return mention
   }
+
+  /** Prioritize the grounding for one of the given species.
+    * NB: Mention must be grounded and have more than one candidate. */
+  def groundBySpecies (mention: BioTextBoundMention, species: Seq[String]): BioMention = {
+    if (mention.hasMoreCandidates) {        // sanity check
+      val cands = mention.candidates.get
+      val candSpecies: Set[String] = cands.map(_.species.toLowerCase).toSet
+      val firstMatch = species.find{ case sp:String => candSpecies.contains(sp.toLowerCase) }
+      val ordered = if (firstMatch.isDefined)
+        selectASpecies(cands, firstMatch.get) ++ selectNotASpecies(cands, firstMatch.get)
+      else cands
+      mention.nominate(Some(ordered))
+    }
+    // System.err.println("BY_SPECIES")       // REMOVE LATER
+    // printMention(mention)                  // REMOVE LATER
+    return mention
+  }
+
 
   def printMention (mention:BioMention): Unit =
     mentionMgr.mentionToStrings(mention).foreach { System.err.println(_) }
