378f0f132ce44d2f99aeee74213f58df4a7b0adf
Tom Hicks <hickst@tohono.com>
2016-03-09 21:27:03 -0700

    Some alterations to try and get this test to work.

--- src/test/scala/edu/arizona/sista/reach/TestMentionSerialization.scala
+++ src/test/scala/edu/arizona/sista/reach/TestMentionSerialization.scala
@@ -1,5 +1,6 @@
 package edu.arizona.sista.reach
 
+import edu.arizona.sista.odin._
 import edu.arizona.sista.reach.mentions._
 import edu.arizona.sista.utils.Serializer
 
@@ -10,24 +11,25 @@ import TestUtils._
 /**
   * Test the serialization of mention sequences.
   *   Written by: Tom Hicks. 3/8/2016
-  *   Last Modified: Initial creation.
+  *   Last Modified: Some alterations to try and get this test to work.
   */
 class TestMentionSerialization extends FlatSpec with Matchers {
 
   val text1 = "Mouse AKT1 phosphorylates PTHR2 in chicken adenoids."
-  val mentions = getBioMentions(text1)
+  val mentions = testReach.extractFrom(text1, "serialization-test", "1")
 
-  val serfile = "/tmp/TestMention.ser"
+  val tempFile = java.io.File.createTempFile("TestMention", ".ser")
+  val serfile = tempFile.getPath
 
   text1 should "produce 7 mentions" in {
     mentions should have size (7)
   }
 
-  "Text" should "write serialized mentions" in {
+  "Serializer" should "write serialized mentions" in {
     Serializer.save[Seq[BioMention]](mentions, serfile)
   }
 
-  "Text" should "load serialized mentions" in {
+  "Serializer" should "load serialized mentions" in {
     val rdMentions = Serializer.load[Seq[BioMention]](serfile)
     rdMentions should have size (7)
   }
