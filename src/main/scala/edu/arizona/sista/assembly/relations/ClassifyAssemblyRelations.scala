364ff2e3d3f073bfd5fe3903e1fd9222a3d72e5b
Gus Hahn-Powell <gushahnpowell@gmail.com>
2016-04-19 15:02:38 -0700

    Added Evaluator

--- src/main/scala/edu/arizona/sista/assembly/relations/ClassifyAssemblyRelations.scala
+++ src/main/scala/edu/arizona/sista/assembly/relations/ClassifyAssemblyRelations.scala
@@ -1,9 +1,39 @@
 package edu.arizona.sista.assembly.relations
 
+import java.io.File
 import com.typesafe.config.ConfigFactory
-import edu.arizona.sista.learning.Datasets._
+import edu.arizona.sista.assembly.relations.CorpusReader._
+import edu.arizona.sista.learning.Datasets
+import edu.arizona.sista.learning.RVFDataset
+import org.apache.commons.io.FileUtils
 
-object ClassifyAssemblyRelations {
+
+object Evaluator {
+
+  def crossValidate(dataset: RVFDataset[String, String]): Seq[(String, String)] = {
+    Datasets.crossValidate[String, String](dataset, () => AssemblyRelationClassifier.getModel("lr")).toSeq
+  }
+
+  def calculateAccuracy(scores: Seq[(String, String)]): Float = {
+    scores.count(pair => pair._1 == pair._2).toFloat / scores.size.toFloat
+  }
+
+  def writeScoresToTSV(scores: Seq[(String, String)], outFile: String): Unit = {
+    val f = new File(outFile)
+    val header = s"Gold\tPredicted"
+
+    val rows = scores.map(pair => s"${pair._1}\t${pair._2}").mkString("\n")
+    val content =
+      s"""$header
+          |$rows
+       """.stripMargin
+
+    FileUtils.writeStringToFile(f, content)
+  }
+
+}
+
+object ClassifyAssemblyRelations extends App {
 
   import CorpusReader._
 
