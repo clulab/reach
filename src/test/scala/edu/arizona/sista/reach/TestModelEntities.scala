40b1e6d1a0ad174f1327dc737475e61a015fb673
Tom Hicks <hickst@tohono.com>
2016-06-09 15:18:30 -0700

    Add some more protein tests to model test suite.

--- src/test/scala/edu/arizona/sista/reach/TestModelEntities.scala
+++ src/test/scala/edu/arizona/sista/reach/TestModelEntities.scala
@@ -11,12 +11,13 @@ import TestUtils._
 /**
   * Test the labeling of entities from the MITRE RAS model.
   *   Written by: Tom Hicks. 6/9/2016.
-  *   Last Modified: Initial creation.
+  *   Last Modified: Add some more protein tests.
   */
 class TestModelEntities extends FlatSpec with Matchers {
 
   val s1 = "BRAF, EGF, EGFR, and GRB2 are proteins."
   val s2 = "HRAS, KRAS, NRAS, and NF1 are proteins."
+  val s2a = "H-RAS, K-RAS, N-RAS, and B-Raf are proteins."
   val s3 = "MAPK1, MAPK3, MEK1, and MEK2 are proteins."
   val s4 = "RASA1, RASA2, RASA3, and SOS1 are proteins."
   val s5 = "p110alpha, p110beta, and p110delta are proteins."
@@ -33,6 +34,14 @@ class TestModelEntities extends FlatSpec with Matchers {
 
   "s2 entities" should "have GPP label" in {
     val mentions = getBioMentions(s2)
+    mentions.isEmpty should be (false)
+    // printMentions(Try(mentions), true)      // DEBUGGING
+    mentions.size should be (4)
+    mentions.count(_ matches "Gene_or_gene_product") should be (4)
+  }
+
+  "s2a entities" should "have GPP label" in {
+    val mentions = getBioMentions(s2a)
     mentions.isEmpty should be (false)
     // printMentions(Try(mentions), true)      // DEBUGGING
     mentions.size should be (4)
