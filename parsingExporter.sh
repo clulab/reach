c69a757718ff53f5414fb426a36c7b30e2e3da00
Enrique Noriega Atala <enrique.noriega@gmail.com>
2016-06-28 09:16:03 -0700

    Added a scala class that exports dependencies, pos tags and discourse trees for exported sentences files

---
+++ parsingExporter.sh
@@ -0,0 +1,14 @@
+ANNDIR="$1"
+
+echo "Processing $ANNDIR"
+
+for F in "$ANNDIR"/PMC*;
+do
+    #echo "$F"
+    NAME=$(basename "$F")
+
+    # echo "$ANNDIR/$ID/event_context.txt"
+    echo "Processing $NAME..."
+    sbt "runMain edu.arizona.sista.reach.context.ParsingExporter $F/sentences.txt $F/sections.txt $F/titles.txt"
+    mv pos.txt deps.txt disc.txt $F
+done
