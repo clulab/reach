11ed00bd5f992ec740d88ccbe0ed9f735333a856
Mihai Surdeanu <surdeanu@gmail.com>
2016-07-05 13:16:23 +0300

    Started the new set of unit tests.

---
+++ src/test/scala/org/clulab/reach/TestFeedback2.scala
@@ -0,0 +1,12 @@
+package org.clulab.reach
+
+import org.scalatest.{Matchers, FlatSpec}
+
+/**
+  * Unit tests based on the second round of feedback from MITRE
+  * User: mihais
+  * Date: 7/5/16
+  */
+class TestFeedback2 extends FlatSpec with Matchers {
+
+}
