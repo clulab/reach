0e96303c0c8806f6980e12ba1e8e8f34abfa80cf
Tom Hicks <hickst@tohono.com>
2016-07-08 22:01:55 -0700

    Update grounding tests shell file.

--- src/test/scala/org/clulab/reach/testGrounding.sh
+++ src/test/scala/org/clulab/reach/testGrounding.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 #  Run all tests related to Grounding.
 #
-sbt 'test-only org.clulab.reach.TestKBSupport \
-org.clulab.reach.TestAzFailsafeKB.scala \
-org.clulab.reach.TestTsvKBs \
+sbt 'test-only \
 org.clulab.reach.TestAdHocIMKBs \
-org.clulab.reach.TestGroundingTrait \
-org.clulab.reach.TestProteinResolutions \
+org.clulab.reach.TestAzFailsafeKB.scala \
 org.clulab.reach.TestFamilyResolutions \
+org.clulab.reach.TestGrounding \
+org.clulab.reach.TestGroundingTrait \
+org.clulab.reach.TestKBSupport \
+org.clulab.reach.TestModelEntities.scala \
+org.clulab.reach.TestNERLabeling.scala \
 org.clulab.reach.TestOrganCellTypeResolutions \
+org.clulab.reach.TestProteinResolutions \
 org.clulab.reach.TestReachContextKBLister.scala \
-org.clulab.reach.TestGrounding'
+org.clulab.reach.TestReachGrounder.scala \
+org.clulab.reach.TestTsvKBs'
