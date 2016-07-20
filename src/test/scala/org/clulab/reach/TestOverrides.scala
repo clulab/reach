b0d36d9255e00b78e1b2ef3f212874283bece183
Tom Hicks <hickst@tohono.com>
2016-07-19 19:42:36 -0700

    Add tests for new KB families. Update for fix of issue #77.

--- src/test/scala/org/clulab/reach/TestOverrides.scala
+++ src/test/scala/org/clulab/reach/TestOverrides.scala
@@ -12,7 +12,7 @@ import TestUtils._
 /**
   * Test that our override KB works properly for NER and grounding.
   *   Written by: Tom Hicks. 7/8/2016.
-  *   Last Modified: Correct tests for fix of issue #77.
+  *   Last Modified: Add tests for new KB families. Update for fix of issue #77.
   */
 class TestOverrides extends FlatSpec with Matchers {
 
@@ -37,15 +37,21 @@ class TestOverrides extends FlatSpec with Matchers {
   val dr2g = "HRAS, H-RAS, KRAS, K-RAS, NRAS, N-RAS are GGPs. "
   val dr2g_ids = Seq("P01112", "P01112", "P01116", "P01116", "P01111", "P01111")
 
+  val fam1 = "ERK1/2, ERK 1/2, Neuregulin, Neuroregulin are Families. "
+  val fam1_ids = Seq("UAZ-PF-244", "UAZ-PF-244", "PF02158", "PF02158")
+
+  val fam2 = "SMAD2/3, SMAD 2/3, and TGFB are Families. "
+  val fam2_ids = Seq("UAZ-PF-243", "UAZ-PF-243", "IPR015615")
+
   val estros = "Estrone E1, estradiol E2, and estriol E3 do not cause cancer."
 
-  val aminos = "Alanine arginine asparagine aspartic aspartate cysteine glutamic glutamate glutamine glycine histidine isoleucine leucine lysine methionine phenylalanine proline serine threonine tryptophan tyrosine valine"
+  val aminos = "Alanine, arginine, asparagine, aspartic acid, aspartate, cysteine, glutamic acid, glutamate, glutamine, glycine, histidine, isoleucine, leucine, lysine, methionine, phenylalanine, proline, serine, threonine, tryptophan, tyrosine, and valine are amino acids"
 
   val aa_short ="Ala, Arg, Asn, Asp, Cys, Gln, Glu, Gly, His, Ile, Leu, Lys, Met, Phe, Pro, Ser, Thr, Trp, Tyr, and Val are now labeled as Sites."
 
   val aa_ids = Seq(
     "UAZ-S-001", "UAZ-S-002", "UAZ-S-003", "UAZ-S-004", "UAZ-S-004",
-    "UAZ-S-005", "UAZ-S-006", "UAZ-S-006", "UAZ-S-007", "UAZ-S-009",
+    "UAZ-S-005", "UAZ-S-006", "UAZ-S-006", "UAZ-S-007", "UAZ-S-008",
     "UAZ-S-009", "UAZ-S-010", "UAZ-S-011", "UAZ-S-012", "UAZ-S-013",
     "UAZ-S-014", "UAZ-S-015", "UAZ-S-016", "UAZ-S-017", "UAZ-S-018",
     "UAZ-S-019", "UAZ-S-020"
@@ -247,6 +253,62 @@ class TestOverrides extends FlatSpec with Matchers {
   }
 
 
+  // Family Tests 1
+  val fam1_mentions = getBioMentions(fam1)
+  fam1 should "have expected number of results" in {
+    fam1_mentions.isEmpty should be (false)
+    // printMentions(Try(fam1_mentions), true)      // DEBUGGING
+    fam1_mentions.size should be (fam1_ids.size)
+  }
+
+  it should "have labeled all mentions as Family" in {
+    fam1_mentions.count(_ matches "Family") should be (fam1_ids.size)
+  }
+
+  it should "have display labeled all mentions as Family" in {
+    fam1_mentions.count(_.displayLabel == "Family") should be (fam1_ids.size)
+  }
+
+  it should "have grounded all mentions as Human" in {
+    fam1_mentions.forall(m => m.grounding.isDefined &&
+                         Speciated.isHumanSpecies(m.grounding.get.species)) should be (true)
+  }
+
+  it should "match expected grounding IDs" in {
+    for ((m, ndx) <- fam1_mentions.zipWithIndex) {
+      m.grounding.isDefined && (m.grounding.get.id == fam1_ids(ndx)) should be (true)
+    }
+  }
+
+
+  // Family Tests 2
+  val fam2_mentions = getBioMentions(fam2)
+  fam2 should "have expected number of results" in {
+    fam2_mentions.isEmpty should be (false)
+    // printMentions(Try(fam2_mentions), true)      // DEBUGGING
+    fam2_mentions.size should be (fam2_ids.size)
+  }
+
+  it should "have labeled all mentions as Family" in {
+    fam2_mentions.count(_ matches "Family") should be (fam2_ids.size)
+  }
+
+  it should "have display labeled all mentions as Family" in {
+    fam2_mentions.count(_.displayLabel == "Family") should be (fam2_ids.size)
+  }
+
+  it should "have grounded all mentions as Human" in {
+    fam2_mentions.forall(m => m.grounding.isDefined &&
+                         Speciated.isHumanSpecies(m.grounding.get.species)) should be (true)
+  }
+
+  it should "match expected grounding IDs" in {
+    for ((m, ndx) <- fam2_mentions.zipWithIndex) {
+      m.grounding.isDefined && (m.grounding.get.id == fam2_ids(ndx)) should be (true)
+    }
+  }
+
+
   estros should "have Simple_chemical labels" in {
     val mentions = getBioMentions(estros)
     mentions.isEmpty should be (false)
@@ -261,7 +323,7 @@ class TestOverrides extends FlatSpec with Matchers {
   val aa_mentions = getBioMentions(aminos)
   aminos should "have expected number of results" in {
     aa_mentions.isEmpty should be (false)
-    // printMentions(Try(aa_mentions), true)      // DEBUGGING
+    printMentions(Try(aa_mentions), true)      // DEBUGGING
     aa_mentions.size should be (aa_ids.size)
   }
 
@@ -273,12 +335,11 @@ class TestOverrides extends FlatSpec with Matchers {
     aa_mentions.count(_.displayLabel == "Site") should be (aa_ids.size)
   }
 
-  // wont pass yet: processors issue #77
-  // it should "match expected grounding IDs" in {
-  //   for ((m, ndx) <- aa_mentions.zipWithIndex) {
-  //     m.grounding.isDefined && (m.grounding.get.id == aa_ids(ndx)) should be (true)
-  //   }
-  // }
+  it should "match expected grounding IDs" in {
+    for ((m, ndx) <- aa_mentions.zipWithIndex) {
+      m.grounding.isDefined && (m.grounding.get.id == aa_ids(ndx)) should be (true)
+    }
+  }
 
 
   // Amino Acid abbreviations relabeled as Sites:
