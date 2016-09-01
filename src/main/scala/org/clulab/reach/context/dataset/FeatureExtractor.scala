ab703232b08c84725a465662e91235b65e3061c6
enoriega <enrique.noriega@gmail.com>
2016-09-01 13:01:42 -0700

    Refinement of feature extraction

--- src/main/scala/org/clulab/reach/context/dataset/FeatureExtractor.scala
+++ src/main/scala/org/clulab/reach/context/dataset/FeatureExtractor.scala
@@ -89,28 +89,51 @@ object FeatureExtractor{
 
   object FeatureProcessing{
     def binSentenceDistance(d:Int):BinnedDistance.Value = {
-      // TODO: Implement this
-      BinnedDistance.SAME
+      if(d == 0)
+        BinnedDistance.SAME
+      else if(d <= 13)
+        BinnedDistance.CLOSE
+      else
+        BinnedDistance.FAR
     }
 
     def binDependencyDistance(d:Int):BinnedDistance.Value = {
-      // TODO: Implement this
-      BinnedDistance.SAME
+      if(d <= 0)
+        BinnedDistance.CLOSE
+      else
+        BinnedDistance.FAR
     }
 
-    def binDiscourseDistance(d:Int):BinnedDistance.Value = {
-      // TODO: Implement this
-      BinnedDistance.SAME
+    // def binDiscourseDistance(d:Int):BinnedDistance.Value = {
+    //   // TODO: Implement this
+    //   BinnedDistance.SAME
+    // }
+
+    def clusterPOSTag(tag:String):String ={
+
+      if(tag.startsWith("NN"))
+        return "NN"
+      else if(tag.startsWith("VB"))
+        return "VB"
+      else if(Set(",", "-RRB-", ".", ":", ";", "LRB").contains(tag))
+        return "BOGUS"
+      else
+        return tag
     }
 
-    def clusterPOSTag(t:String):String ={
-      // TODO: Implement this
-      "NN"
-    }
-
-    def clusterDependency(t:String):String = {
-      // TODO: Implement this
-      "MM"
+    def clusterDependency(d:String):String = {
+      if(d.startsWith("prep"))
+          "prep"
+        else if(d.startsWith("conj"))
+            "conj"
+        else if(d.endsWith("obj"))
+            "obj"
+        else if(d.endsWith("mod"))
+            "mod"
+        else if(d.contains("subj"))
+            "subj"
+        else
+            d
     }
 
 
