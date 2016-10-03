9fa51fb0fb2b780782c649138a89b844b9aff62f
enoriega <enrique.noriega@gmail.com>
2016-10-03 14:32:50 -0700

    Commented out some obsolete tests
    This might need to be substituted

--- src/test/scala/org/clulab/reach/context/TestDeterministicPolicies.scala
+++ src/test/scala/org/clulab/reach/context/TestDeterministicPolicies.scala
@@ -102,93 +102,93 @@ class TestDeterministicPolicies extends FlatSpec with Matchers {
       }
   }
 
-  def boundedPaddingBehavior(paperNum:Int){
-    info(s"Testing bounding padding context")
-
-    val annotation = paperAnnotations(paperNum)
-
-    val friesEntries = annotation.friesEntries
-    val documents = annotation.documents
-    val entitiesPerEntry = annotation.entitiesPerEntry
-
-    val bound = 5
-    val boundedPaddingEngine = new BoundedPaddingContext(bound)
-    boundedPaddingEngine.infer(entitiesPerEntry.flatten)
-
-    // No more than $bound repetitions of the same context
-
-    it should s"not extend an existing context more than $bound times" in {
-
-      val sparseMatrix = boundedPaddingEngine.preprocessedLatentStateMatrix.transpose
-      val matrix = boundedPaddingEngine.latentStateMatrix.transpose
-      // Transpose the matrix
-
-      // Count the number of zeros from before hitting a one from right to left
-
-      val counts = sparseMatrix.map(_.scanRight(0){(step:Boolean, cs:Int) => if(!step) cs+1 else 0}.drop(1))
-
-      // Select the indices that are to be checked
-      val selection = (sparseMatrix zip counts).map{ case (s, c) => (s zip c).zipWithIndex filter {case((value, count), ix) => value && count > bound} map (_._2)}
-
-      // check them
-      selection foreach {
-        _ foreach {
-          x =>
-            val extended = matrix(2).drop(x).take(bound+1).map(if(_) 1 else 0).sum
-            extended should be <= bound
-        }
-      }
-
-    }
-  }
-
-  def bidirectionalPaddingBehavior(paperNum:Int){
-    info(s"Testing bidirectional bounding padding context")
-
-    val annotation = paperAnnotations(paperNum)
-
-    val friesEntries = annotation.friesEntries
-    val documents = annotation.documents
-    val entitiesPerEntry = annotation.entitiesPerEntry
-    val paperMentions = annotation.mentions
-
-    val boundedPaddingEngine = new BidirectionalPaddingContext(bound=5)
-    boundedPaddingEngine.infer(entitiesPerEntry.flatten)
-
-    // No more than $bound repetitions of the same context
-    val bound = 10
-    it should s"not extend an existing context more than $bound times in both directions" in {
-
-      val sparseMatrix = boundedPaddingEngine.preprocessedLatentStateMatrix.transpose
-      val matrix = boundedPaddingEngine.latentStateMatrix.transpose
-      // Transpose the matrix
-
-      // Count the number of zeros from before hitting a one from right to left
-
-      val counts = sparseMatrix.map(_.scanRight(0){(step:Boolean, cs:Int) => if(!step) cs+1 else 0}.drop(1))
-
-      // Select the indices that are to be checked
-      val selection = (sparseMatrix zip counts).map{ case (s, c) => (s zip c).zipWithIndex filter {case((value, count), ix) => value && count > bound} map (_._2)}
-
-      // check them
-      selection foreach {
-        _ foreach {
-          x =>
-            val extended = matrix(2).drop(x).take(bound+1).map(if(_) 1 else 0).sum
-            extended should be <= bound
-        }
-      }
-
-    }
-  }
+  // def boundedPaddingBehavior(paperNum:Int){
+  //   info(s"Testing bounding padding context")
+  //
+  //   val annotation = paperAnnotations(paperNum)
+  //
+  //   val friesEntries = annotation.friesEntries
+  //   val documents = annotation.documents
+  //   val entitiesPerEntry = annotation.entitiesPerEntry
+  //
+  //   val bound = 5
+  //   val boundedPaddingEngine = new BoundedPaddingContext(bound)
+  //   boundedPaddingEngine.infer(entitiesPerEntry.flatten)
+  //
+  //   // No more than $bound repetitions of the same context
+  //
+  //   it should s"not extend an existing context more than $bound times" in {
+  //
+  //     val sparseMatrix = boundedPaddingEngine.preprocessedLatentStateMatrix.transpose
+  //     val matrix = boundedPaddingEngine.latentStateMatrix.transpose
+  //     // Transpose the matrix
+  //
+  //     // Count the number of zeros from before hitting a one from right to left
+  //
+  //     val counts = sparseMatrix.map(_.scanRight(0){(step:Boolean, cs:Int) => if(!step) cs+1 else 0}.drop(1))
+  //
+  //     // Select the indices that are to be checked
+  //     val selection = (sparseMatrix zip counts).map{ case (s, c) => (s zip c).zipWithIndex filter {case((value, count), ix) => value && count > bound} map (_._2)}
+  //
+  //     // check them
+  //     selection foreach {
+  //       _ foreach {
+  //         x =>
+  //           val extended = matrix(2).drop(x).take(bound+1).map(if(_) 1 else 0).sum
+  //           extended should be <= bound
+  //       }
+  //     }
+  //
+  //   }
+  // }
+  //
+  // def bidirectionalPaddingBehavior(paperNum:Int){
+  //   info(s"Testing bidirectional bounding padding context")
+  //
+  //   val annotation = paperAnnotations(paperNum)
+  //
+  //   val friesEntries = annotation.friesEntries
+  //   val documents = annotation.documents
+  //   val entitiesPerEntry = annotation.entitiesPerEntry
+  //   val paperMentions = annotation.mentions
+  //
+  //   val boundedPaddingEngine = new BidirectionalPaddingContext(bound=5)
+  //   boundedPaddingEngine.infer(entitiesPerEntry.flatten)
+  //
+  //   // No more than $bound repetitions of the same context
+  //   val bound = 10
+  //   it should s"not extend an existing context more than $bound times in both directions" in {
+  //
+  //     val sparseMatrix = boundedPaddingEngine.preprocessedLatentStateMatrix.transpose
+  //     val matrix = boundedPaddingEngine.latentStateMatrix.transpose
+  //     // Transpose the matrix
+  //
+  //     // Count the number of zeros from before hitting a one from right to left
+  //
+  //     val counts = sparseMatrix.map(_.scanRight(0){(step:Boolean, cs:Int) => if(!step) cs+1 else 0}.drop(1))
+  //
+  //     // Select the indices that are to be checked
+  //     val selection = (sparseMatrix zip counts).map{ case (s, c) => (s zip c).zipWithIndex filter {case((value, count), ix) => value && count > bound} map (_._2)}
+  //
+  //     // check them
+  //     selection foreach {
+  //       _ foreach {
+  //         x =>
+  //           val extended = matrix(2).drop(x).take(bound+1).map(if(_) 1 else 0).sum
+  //           extended should be <= bound
+  //       }
+  //     }
+  //
+  //   }
+  // }
 
   // Tests
   behavior of "PMC2597732.nxml"
 
   it should behave like contextAssignmentBehavior(1)
   it should behave like differentTypeOfMentionsWithContextBehavior(1)
-  it should behave like boundedPaddingBehavior(1)
-  it should behave like bidirectionalPaddingBehavior(1)
+  // it should behave like boundedPaddingBehavior(1)
+  // it should behave like bidirectionalPaddingBehavior(1)
 
   // behavior of "PMC3189917.nxml"
   //
