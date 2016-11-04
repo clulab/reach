7f08c6c869c37d3b31f18e890860080417faeee1
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2016-11-04 12:42:53 -0700

    deleted test

--- main/src/test/scala/org/clulab/reach/TestTokenPatterns.scala
+++ main/src/test/scala/org/clulab/reach/TestTokenPatterns.scala
@@ -11,7 +11,6 @@ class TestTokenPatterns extends FlatSpec with Matchers {
   val proc = bioproc
   val text1 = "TGFBR2 phosphorylates peri-kappa B and inhibits the ubiquitination of SMAD3."
   val text2 = "TGFBR2 phosphorylates peri-kappa B and peri-kappa C and inhibits the ubiquitination of SMAD3."
-  val text3 = "TGFBR2 phosphorylates peri-kappa B and peri-kappa C by TGFBR3 and inhibits the ubiquitination of SMAD3."
 
   text1 should "contain one match" in {
     val doc = proc annotate text1
@@ -30,16 +29,6 @@ class TestTokenPatterns extends FlatSpec with Matchers {
     val state = State(mentions)
     val results = p.findAllIn(0, doc, state)
     assert(results.size == 1)
-  }
-
-  text3 should "contain two matches" in {
-    val doc = proc annotate text3
-    val p = TokenPattern.compile("@Phosphorylation and inhibits")
-    val mentions = Seq(new TextBoundMention("Phosphorylation", Interval(0, 9), 0, doc, true, "<MANUAL>"),
-                       new TextBoundMention("Phosphorylation", Interval(0, 9), 0, doc, true, "<MANUAL>"))
-    val state = State(mentions)
-    val results = p.findAllIn(0, doc, state)
-    assert(results.size == 2)
   }
 
   val text4 = "a b c d e f g h i c"
