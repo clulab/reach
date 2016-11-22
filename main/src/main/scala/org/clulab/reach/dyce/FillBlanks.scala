3002796539f2d8fe81230e4a7f51b60f7d0e44e0
Enrique Noriega <enrique.noriega@gmail.com>
2016-11-22 12:56:22 -0700

    Finished the execution flow for the fill-the-blanks task. Still need to implement the functions to make it work.

--- main/src/main/scala/org/clulab/reach/dyce/FillBlanks.scala
+++ main/src/main/scala/org/clulab/reach/dyce/FillBlanks.scala
@@ -1,9 +1,9 @@
 package org.clulab.reach.dyce
 
-import java.nio.file.Paths
-
+import collection.mutable
 import org.clulab.reach.grounding.ReachKBUtils
 import org.clulab.reach.mentions.CorefMention
+import org.clulab.struct.DirectedGraph
 
 /**
   * Created by enrique on 21/11/16.
@@ -20,24 +20,76 @@ class FillBlanks {
   val participantA =  Participant("uniprot", "Q13315") // ATM, Grounding ID of the controller
   val participantB = Participant("uniprot", "P42345") // mTOR, Grounding ID of the controller
 
+  val annotationsRecord = mutable.Set[String]()
+
   val indexPath = "/data/nlp/corpora/pmc_openaccess/pmc_aug2016_index" // Lucene index path
+
+  var G:Option[DirectedGraph[Participant]] = None // Directed graph with the model. It is a mutable variable as it will change each step
 
 
   // First step, bootstrap the graph by querying individually the participants
   val docsA:Iterable[String] = FillBlanks.queryIndividualParticipant(participantA)
   val docsB :Iterable[String] = FillBlanks.queryIndividualParticipant(participantB)
 
-  // Annotate them
+  // Join them
   val paperSet:Set[String] = docsA.toSet & docsB.toSet
+
+  // Add them to the annotations record
+  annotationsRecord ++= paperSet
 
   // Extract them
   val activations = FillBlanks.readPapers(paperSet)
 
-  //TODO: Compute overlap with dyce model - Export arizona output and call my python script
+  //TODO: Compute overlap with dyce model - Export arizona output and call my python script or reimplement here for efficiency
 
   // Build a set of connections out of the extractions
   val connections:Iterable[Connection] = FillBlanks.buildEdges(activations)
-  //TODO: Grow the graph
+  //Grow the graph
+  G = Some(FillBlanks.expandGraph(G, connections))
+
+  // Loop of iterative steps of expanding the graph
+  var stop = false
+  while(!stop){ // TODO: Replace the logical statement with something that breaks
+    // Look for a path between participants A and B
+    val path = FillBlanks.findPath(G.get, participantA, participantB)
+
+    path match {
+      case Some(p) =>
+        // TODO: Report the result
+        stop = true
+      case None => Unit
+    }
+
+    if(!stop) {
+      val frontierA = FillBlanks.findFrontier(G.get, participantA)
+      val frontierB = FillBlanks.findFrontier(G.get, participantB)
+
+
+      // TODO: make sure to expand this cross product to include more nodes if necessary (like the original participant)
+      val pairs = for {l <- frontierA; r <- frontierB} yield (l, r) // These are the pairs to expand our search
+
+      // Query the index to find the new papers to annotate
+      val allDocs = pairs flatMap (p => FillBlanks.queryParticipants(p._1, p._2))
+
+      // Filter out those papers that have been already annotated
+      val docs = allDocs filter {
+        d => !annotationsRecord.contains(d)
+      }
+
+      // Add the papers to the record to avoid annotating them later
+      annotationsRecord ++= docs
+
+      // Annotate the new papers
+      val activations = FillBlanks.readPapers(docs)
+
+      //TODO: Compute overlap with dyce model - Export arizona output and call my python script or reimplement here for efficiency
+
+      // Build a set of connections out of the extractions
+      val connections:Iterable[Connection] = FillBlanks.buildEdges(activations)
+      //Grow the graph
+      G = Some(FillBlanks.expandGraph(G, connections))
+    }
+  }
 
 
 }
@@ -45,7 +97,46 @@ class FillBlanks {
 object FillBlanks{
 
   /***
+    * Searches for a path between the participants in the graph
+    * @param G Model graph
+    * @param participantA Source of the path
+    * @param participantB Sink of the path
+    * @return Some sequence if the path exists, otherwise None
+    */
+  def findPath(G: DirectedGraph[Participant], participantA: Participant
+               , participantB: Participant): Option[Seq[Participant]] =
+  {
+    None
+  }
+
+
+  /***
+    * Finds the search frontier in the graph relative to participant
+    *
+    * @param G The model
+    * @param participant The participant of concern
+    * @return Iterable of participants that make the frontier
+    */
+  def findFrontier(G:DirectedGraph[Participant], participant: Participant):Iterable[Participant] = {
+    Nil
+  }
+
+
+  /***
+    * Creates a new graph with the connections of the existing graph and new connections from the second argument
+    *
+    * @param G existing graph, if any, to be expanded
+    * @param connections New information to incorporate to the graph
+    * @return Graph with the new information added
+    */
+  def expandGraph(G: Option[DirectedGraph[Participant]], connections: Iterable[Connection]): DirectedGraph[Participant] = {
+    G.get
+  }
+
+
+  /***
     * Builds edges for the model graph out of raw REACH extractions
+    *
     * @param activations REACH evenbts to use
     * @return Iterable of connection instances
     */
