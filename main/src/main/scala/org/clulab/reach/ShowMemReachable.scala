8cc9acf720bd3a6a7182419446a36703e749f08f
Tom Hicks <hickst@tohono.com>
2017-02-19 07:30:22 -0700

    Add reachability measure of reverse species lookup.

--- main/src/main/scala/org/clulab/reach/ShowMemReachable.scala
+++ main/src/main/scala/org/clulab/reach/ShowMemReachable.scala
@@ -5,16 +5,26 @@ import org.github.jamm._
 import org.clulab.reach.grounding._
 
 /**
- * Test of grounding data structure sizes by reachability.
- */
+  * Test of grounding data structure sizes by reachability.
+  *   Last modified: Add measure of reverse species lookup.
+  */
 object ShowMemReachable extends App {
 
   val meter: MemoryMeter = new MemoryMeter
 
   println(s"Creating ReachIMKBMentionLookups object...")
   val riml = ReachIMKBMentionLookups
+  println(s"Creating ReachMiscLookups object...")
+  val rsl = ReachMiscLookups
 
   println(s"Sizing ReachIMKBMentionLookups object...")
-  println(s"${meter.measureDeep(riml)}")
+  val rimlSize = meter.measureDeep(riml)
+  println(s"RIML=${rimlSize}")
+
+  println(s"Sizing Reverse Species Lookup object...")
+  val rslSize = meter.measureDeep(rsl.ReverseSpeciesLookup)
+  println(s"RSL=${rslSize}")
+
+  println(s"TOTAL=${rimlSize + rslSize}")
 
 }
