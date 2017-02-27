ed88e7774305e92ad44f3abea76b79b04a630be5
Enrique Noriega <enrique.noriega@gmail.com>
2017-02-27 09:40:22 -0700

    Progress

--- main/src/main/scala/org/clulab/reach/dyce/SearchAgentImplementations.scala
+++ main/src/main/scala/org/clulab/reach/dyce/SearchAgentImplementations.scala
@@ -6,6 +6,7 @@ import org.clulab.reach.dyce.QueryStrategy._
 import scalax.collection.edge.LDiEdge
 import scalax.collection.mutable.Graph
 import SearchAgent.Model
+import collection.mutable
 
 /**
   * Created by enrique on 18/02/17.
@@ -47,6 +48,13 @@ class SQLiteSearchAgent(participantA:Participant, participantB:Participant) exte
 
 object CoolSearchAgent extends App with LazyLogging{
 
+  def getParticipants(path:List[Connection]):List[Participant] = {
+    path match {
+      case h::t => h.controller :: getParticipants(t)
+      case Nil => Nil
+    }
+  }
+
   // The first argument is the input file
   val dataSet:Iterable[(String, String, String)] = io.Source.fromFile(args(0)).getLines
     .map{
@@ -55,17 +63,20 @@ object CoolSearchAgent extends App with LazyLogging{
         (t(0), t(1), t(2))
     }.toSeq
 
-  var (successes, failures) = (0, 0)
+  var (successes, failures, hits) = (0, 0, 0)
+  val pathLengths = new mutable.ArrayBuffer[Int]
+  val successIterations = new mutable.ArrayBuffer[Int]
+  val failureIterations = new mutable.ArrayBuffer[Int]
 
   logger.info(s"About to do ${dataSet.size} searches ...")
   logger.info(s"")
-  for(datum <- dataSet){
+  for((datum, ix) <- dataSet.zipWithIndex){
     logger.info(s"Searching for path: ${datum._1}-${datum._2}-${datum._3}")
 
     val participantA =  Participant("", datum._1)
     val participantB = Participant("", datum._3)
 
-    logger.info(s"About to start a focused search with")
+    logger.info(s"About to start a focused search $ix of ${dataSet.size}")
 
     //val agent = new LuceneReachSearchAgent(participantA, participantB)
     val agent = new SQLiteSearchAgent(participantA, participantB)
@@ -74,6 +85,7 @@ object CoolSearchAgent extends App with LazyLogging{
     agent.successStopCondition(participantA, participantB, agent.model) match {
       case Some(path) =>
         successes += 1
+        successIterations += agent.iterationNum
         logger.info("Success!!")
         logger.info(path.mkString(" || "))
       //      for(c <- path){
@@ -83,12 +95,56 @@ object CoolSearchAgent extends App with LazyLogging{
       //        println(sentences.mkString("\n"))
       //        println("----------------------------")
       //      }
+
+       // Analysis
+        val participants = getParticipants(path.toList)
+        val groundTruth = datum.productIterator.toList
+
+        if(participants == groundTruth)
+          hits += 1
+        else{
+          // Get the length of the paths
+          pathLengths += participants.length
+        }
+
       case None =>
         failures += 1
+        failureIterations += agent.iterationNum
         logger.info("Failure")
+
     }
+
+
+
 
     logger.info("")
   }
   logger.info(s"Finished attaining $successes successes and $failures failures")
+  logger.info("")
+  logger.info("Postmortem analysis:")
+  logger.info(s"Exact matches: $hits\t Alternative matches: ${successes-hits}")
+
+  // Length counts
+  val lengths = pathLengths.groupBy(identity).mapValues(_.size)
+  logger.info(s"Alternate path lengths:")
+  val keys = lengths.keys.toSeq.sortBy(k => lengths(k))
+  for(l <- keys){
+    logger.info(s"\tLength $l: ${lengths(l)}")
+  }
+
+  logger.info("")
+  logger.info("Iteration counts for successes")
+  val sIterations = successIterations.groupBy(identity).mapValues(_.size)
+  for(l <- sIterations.keys.toSeq.sorted){
+    logger.info(s"\t$l iterations: ${sIterations(l)}")
+  }
+
+  logger.info("")
+  logger.info("Iteration counts for failures")
+  val fIterations = failureIterations.groupBy(identity).mapValues(_.size)
+  for(l <- fIterations.keys.toSeq.sorted){
+    logger.info(s"\t$l iterations: ${fIterations(l)}")
+  }
+
+
 }
