4c3488ea9e6ed6f8a918489cfb3a3c16f883e8b0
Mihai Surdeanu <surdeanu@gmail.com>
2017-06-07 16:54:45 -0700

    Added script to compute overlap between two IR outputs.

---
+++ main/src/main/scala/org/clulab/reach/indexer/DocListOverlap.scala
@@ -0,0 +1,41 @@
+package org.clulab.reach.indexer
+
+import java.io.{FileWriter, PrintWriter}
+
+import scala.collection.mutable
+
+/**
+  * Created by mihais on 6/7/17.
+  */
+object DocListOverlap {
+  def main(args:Array[String]): Unit = {
+    val oldFile = args(0)
+    val newFile = args(1)
+
+    val oldDocs = readDocNames(oldFile)
+    val newDocs = readDocNames(newFile)
+    println(s"Found ${newDocs.size} new documents.")
+
+    val pw = new PrintWriter(new FileWriter("diff.txt"))
+    for(nn <- newDocs) {
+      if(oldDocs.contains(nn)) pw.print("OLD ")
+      else pw.print("NEW ")
+      pw.println(nn)
+    }
+    pw.close()
+  }
+
+  def readDocNames(fn:String): Set[String] = {
+    val names = new mutable.HashSet[String]()
+    for(l <- io.Source.fromFile(fn).getLines()) {
+      val tokens = l.split("\\s+")
+      if(tokens.length > 0) {
+        val n = tokens(0).replaceAll(".json", "")
+        names += n
+      } else {
+        println("Invalid line: " + l)
+      }
+    }
+    names.toSet
+  }
+}
