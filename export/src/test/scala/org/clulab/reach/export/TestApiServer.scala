083c4fade3865406976c4e1bbe06df746845f8a6
Tom Hicks <hickst@tohono.com>
2017-08-31 20:03:48 -0700

    Update the version test. Add POST text test. Cleanups.

--- export/src/test/scala/org/clulab/reach/export/TestApiServer.scala
+++ export/src/test/scala/org/clulab/reach/export/TestApiServer.scala
@@ -1,15 +1,14 @@
 package org.clulab.reach.export.server
 
-import scala.concurrent.ExecutionContextExecutor
+import scala.concurrent.{ ExecutionContextExecutor, Future }
+import scala.concurrent.duration.FiniteDuration
+import scala.concurrent.duration._
 import scala.io.Source
 
 import com.typesafe.config.{ Config, ConfigFactory }
 
-import de.heikoseeberger.akkahttpjson4s.Json4sSupport
-import org.json4s.{ Formats, DefaultFormats, jackson, native }
-
 import akka.event.Logging
-import akka.http.scaladsl.marshallers.xml.ScalaXmlSupport._
+// import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server._
 import akka.http.scaladsl.server.Directives._
@@ -23,16 +22,12 @@ import org.clulab.reach.export.server.ApiServer._
 /**
   * Unit tests of the API service class.
   *   Written by: Tom Hicks. 8/17/2017.
-  *   Last Modified: Get the GET text test working. Begin POST tests (not working).
+  *   Last Modified: Update the version test. Add POST text test. Cleanups.
   */
 class TestApiServer extends WordSpec
     with Matchers
     with ScalatestRouteTest
-    with Json4sSupport
 {
-
-  implicit val serialization = jackson.Serialization // or native.Serialization
-  implicit val formats = DefaultFormats
 
   implicit val executionContext = system.dispatcher
   val logger = Logging(system, getClass)
@@ -49,15 +44,20 @@ class TestApiServer extends WordSpec
 
   "The class under test" should {
 
+    // GETs
+
     "return correct JSON version string" in {
       Get("/version") ~> route ~> check {
         status should equal(StatusCodes.OK)
-        val resp = responseAs[Map[String,String]]
-        (resp.get("version")) should equal (Some(version))
+        val resp = responseAs[HttpResponse]
+        (resp) should not be (null)
+        (resp.entity) should not be (null)
+        // logger.info(s"resp.entity=${resp.entity}") // DEBUGGING
+        val vers:String = resp.entity.asInstanceOf[HttpEntity.Strict].data.utf8String
+        // logger.info(s"vers=${vers}")        // DEBUGGING
+        (vers) should equal (version)
       }
     }
-
-    // GETs
 
     "GET text" in {
       Get("/api/text?text=ZZZ4%20phosphorylates%20ATM%20") ~> route ~> check {
@@ -107,19 +107,36 @@ class TestApiServer extends WordSpec
 
     // POSTs
 
-    "POST text" in {
-      Post("/api/text",
-        HttpEntity(ContentTypes.`application/json`, """{ "text": "akt1 phosphorylates XXX" }""")) ~> route ~> check
-        {
-          status should equal(StatusCodes.OK)
-          val resp = responseAs[String]
-          // logger.info(s"resp=${resp}")        // DEBUGGING
-          (resp) should not be (empty)
-        }
+    "POST text, default output" in {
+      Post("/api/text?text=akt1%20dephosphorylates%20mek1") ~> route ~> check {
+        status should equal(StatusCodes.OK)
+        val resp = responseAs[HttpResponse]
+        (resp) should not be (null)
+        val entity = resp.entity
+        // logger.info(s"resp.entity=${entity}") // DEBUGGING
+        (resp.entity) should not be (null)
+        val entLen = entity.getContentLengthOption.orElse(-1L)
+        // logger.info(s"entity.length=${entLen}") // DEBUGGING
+        (entLen > 200) should be (true)
+      }
     }
 
-    "POST nxml" in {
-      Post("/api/nxml",
+    "POST upload text, default output" in {
+      Post("/api/uploadText", FormData("text" -> "akt1 phosphorylates MEK1")) ~> route ~> check {
+        status should equal(StatusCodes.OK)
+        val resp = responseAs[HttpResponse]
+        (resp) should not be (null)
+        val entity = resp.entity
+        logger.info(s"resp.entity=${entity}") // DEBUGGING
+        (resp.entity) should not be (null)
+        val entLen = entity.getContentLengthOption.orElse(-1L)
+        logger.info(s"entity.length=${entLen}") // DEBUGGING
+        (entLen > 200) should be (true)
+      }
+    }
+
+    "POST upload nxml" in {
+      Post("/api/uploadNxml",
         HttpEntity(ContentTypes.`application/json`, """{ "nxml": "Xyz1224" }""")) ~> route ~> check
         {
           status should equal(StatusCodes.OK)
