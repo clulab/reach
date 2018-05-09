b5bcaa1e71e53cffa2506fb07ef26f75264fda47
Mihai Surdeanu <surdeanu@gmail.com>
2018-05-09 15:04:54 -0700

    Switched default processor to BioNLPProcessor rather than the server. The client/server config remains available.

--- runAllTests.sh
+++ runAllTests.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
+# uncomment the lines below if processorAnnotator.type = "server"
 # start up the Processors Server for tests to run against:
-echo 'Starting Processor Server...'
-sbt 'run-main org.clulab.processors.server.ProcessorServer' &
-sleep 20
+#echo 'Starting Processor Server...'
+#sbt 'run-main org.clulab.processors.server.ProcessorServer' &
+#sleep 20
 
 sbt test
 sbt main/test
 sbt causalAssembly/test
 sbt export/test
 
-echo 'Stopping Processor Server...'
-sbt 'run-main org.clulab.processors.csshare.ShutdownProcessorServer'
+#echo 'Stopping Processor Server...'
+#sbt 'run-main org.clulab.processors.csshare.ShutdownProcessorServer'
--- runReachShell.sh
+++ runReachShell.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-echo 'Starting Processor Server...'
-sbt 'run-main org.clulab.processors.server.ProcessorServer' >/dev/null 2>&1 &
-sleep 20
+# uncomment the lines below if processorAnnotator.type = "server"
+#echo 'Starting Processor Server...'
+#sbt 'run-main org.clulab.processors.server.ProcessorServer' >/dev/null 2>&1 &
+#sleep 20
 
 echo 'Starting Reach Shell...'
 sbt 'run-main org.clulab.reach.ReachShell'
 
-echo 'Stopping Processor Server...'
-sbt 'run-main org.clulab.processors.csshare.ShutdownProcessorServer'
+#echo 'Stopping Processor Server...'
+#sbt 'run-main org.clulab.processors.csshare.ShutdownProcessorServer'
