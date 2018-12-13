773b53a32b05da066824b6a86ac67aabc9a24def
Enrique Noriega <enrique.noriega@gmail.com>
2018-12-13 16:47:00 -0700

    Implemented the hyphen-role-flipping feature

---
+++ main/src/main/scala/org/clulab/reach/darpa/HyphenHandle.scala
@@ -0,0 +1,46 @@
+package org.clulab.reach.darpa
+
+import org.clulab.odin.{Mention, SynPath, TextBoundMention}
+import org.clulab.processors.Document
+import org.clulab.reach.mentions.{BioEventMention, BioMention}
+
+object HyphenHandle {
+  def handleHyphens(mentions: Seq[BioMention]):Seq[BioMention] = {
+    mentions map {
+      case event:BioEventMention => {
+        (event.controllerArgs(), event.document.text) match {
+            case (Some(controllers), Some(ogText)) if controllers.size == 1 =>
+              val controller = controllers.head
+              val sentence = event.document.sentences(event.sentence)
+
+              val start = controller.tokenInterval.last
+              val end = if(sentence.words.length > start+1) start+1 else start
+
+              val text = ogText.slice(sentence.startOffsets(start), sentence.endOffsets(end))
+
+              if(text.contains('-')){
+                val args = event.arguments
+                // Flip the controller and controlled entities to create a new event
+                val newArgs = args + ("controller" -> args("controlled")) + ("controlled" -> args("controller"))
+
+                new BioEventMention(
+                  event.labels,
+                  event.trigger,
+                  newArgs,
+                  event.paths,
+                  event.sentence,
+                  event.document,
+                  event.keep,
+                  event.foundBy,
+                  event.isDirect
+                )
+              }
+              else
+                event
+            case _ => event
+          }
+      }
+      case m => m
+    }
+  }
+}
