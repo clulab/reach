7e86fa3bb5bc396b0d29b8e8d263719511fde340
ShraddhaThumsi <thumsishraddhasatish@gmail.com>
2019-02-25 18:29:14 -0700

    started ContextEngine subclass

---
+++ main/src/main/scala/org/clulab/reach/context/SVMContextEngine.scala
@@ -0,0 +1,14 @@
+package org.clulab.reach.context
+import org.clulab.reach.mentions.BioMention
+import org.ml4ai.data.classifiers.LinearSVMWrapper
+
+class SVMContextEngine extends ContextEngine {
+  val svmWrapper = new LinearSVMWrapper(null)
+  val trainedSVMInstance = svmWrapper.loadFrom("/Users/shraddha/datascience/ScalaContext/src/main/resources/svmTrainedModel.dat")
+  override def assign(mentions: Seq[BioMention]): Seq[BioMention] = mentions
+
+  override def infer(mentions: Seq[BioMention]): Unit = ()
+
+  override def update(mentions: Seq[BioMention]): Unit = ()
+
+}
