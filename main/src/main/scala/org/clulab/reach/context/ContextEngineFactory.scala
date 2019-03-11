5653eb7111c0f8bf9316c3381aec82c8af785c59
Enrique Noriega <enrique.noriega@gmail.com>
2019-03-11 12:55:26 -0700

    Added support for the SVM engine on the config file. Configured to use it on this branch

--- main/src/main/scala/org/clulab/reach/context/ContextEngineFactory.scala
+++ main/src/main/scala/org/clulab/reach/context/ContextEngineFactory.scala
@@ -11,6 +11,7 @@ object ContextEngineFactory {
       val Policy2 = Value("Policy2")
       val Policy3 = Value("Policy3")
       val Policy4 = Value("Policy4")
+      val SVMPolicy = Value("SVM")
     }
     import Engine._
 
@@ -36,6 +37,7 @@ object ContextEngineFactory {
                 case None => new BidirectionalPaddingContext
             }
             case Dummy => new DummyContextEngine
+            case SVMPolicy => new SVMContextEngine
             case _ => new DummyContextEngine
         }
     }
