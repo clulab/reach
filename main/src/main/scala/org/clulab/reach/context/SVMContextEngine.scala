334708d76d629a729dfbc5411e7b2d618024d015
ShraddhaThumsi <thumsishraddhasatish@gmail.com>
2019-04-03 09:12:32 -0700

    added inhibition papers

--- main/src/main/scala/org/clulab/reach/context/SVMContextEngine.scala
+++ main/src/main/scala/org/clulab/reach/context/SVMContextEngine.scala
@@ -8,6 +8,8 @@ import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.LazyLogging
 
 // This script currently tests papers in the activation version of polarity mismatch.
+// I have added the inhibition papers to a directory called papers2. Please rename the directory to papers when you want to run reach on inhibition
+// Note that Reach MUST have a directory called papers, so whichever papers are in it will be parsed by the system.
 import scala.collection.immutable
 class SVMContextEngine extends ContextEngine with LazyLogging {
 
