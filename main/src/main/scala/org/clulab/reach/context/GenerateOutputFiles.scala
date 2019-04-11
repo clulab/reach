4a17f21de66695832800fb502c2685068c56bc3f
ShraddhaThumsi <thumsishraddhasatish@gmail.com>
2019-04-11 13:06:46 -0700

    running into GC overhead. Will try to run on venti.

--- main/src/main/scala/org/clulab/reach/context/GenerateOutputFiles.scala
+++ main/src/main/scala/org/clulab/reach/context/GenerateOutputFiles.scala
@@ -1,5 +1,17 @@
 package org.clulab.reach.context
-
+import org.clulab.reach.PaperReader
+import com.typesafe.config.ConfigFactory
+import java.io._
+import org.clulab.odin.Mention
 object GenerateOutputFiles extends App {
+  val config = ConfigFactory.load()
+  val currentPaperPath = config.getString("papersDir").concat("/PMC420486.nxml")
+  val file = new File(currentPaperPath)
+  val (paperID, mentions) = PaperReader.readPaper(file)
+  println(mentions.size + " number of mentions captured for current paper")
 
 }
+
+
+
+
