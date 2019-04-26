d9cc1773e8d3caec67e15975337ad6e2a0fe0dd3
ShraddhaThumsi <thumsishraddhasatish@gmail.com>
2019-04-26 10:02:16 -0700

    made major changes to list directory for files and check if output files exist, and if not, create them

--- main/src/main/scala/org/clulab/reach/context/GenerateOutputFiles.scala
+++ main/src/main/scala/org/clulab/reach/context/GenerateOutputFiles.scala
@@ -16,90 +16,106 @@ object GenerateOutputFiles extends App {
     val config = ConfigFactory.load()
     val paper = "PMC1590014"
     val typeOfPaper = "inhibition"
-    val currentPaperPath = config.getString("papersDir").concat(s"/${typeOfPaper}/${paper}.nxml")
-    val pathForSentences = config.getString("contextEngine.params.contextOutputDir").concat(s"${paper}/sentences.txt")
-    val pathForEvents = config.getString("contextEngine.params.contextOutputDir").concat(s"${paper}/event_intervals.txt")
-    val pathForContextMentions = config.getString("contextEngine.params.contextOutputDir").concat(s"${paper}/mention_intervals.txt")
+    val dirForType = config.getString("papersDir").concat(s"/${typeOfPaper}")
+    val outPaperDirPath = config.getString("contextEngine.params.contextOutputDir").concat(s"${paper}")
+    // creating output directory if it doesn't already exist
+    val outputPaperDir = new File(outPaperDirPath)
+    if(!outputPaperDir.exists()) {
+        outputPaperDir.mkdir()
+    }
+    val pathForSentences = outPaperDirPath.concat("/sentences.txt")
+    val pathForEvents = outPaperDirPath.concat("/event_intervals.txt")
+    val pathForContextMentions = outPaperDirPath.concat("/mention_intervals.txt")
     val nxmlReader = new NxmlReader(ignoreSections.toSet, transformText = preproc.preprocessText)
     val contextEngineType = Engine.withName(config.getString("contextEngine.type"))
     lazy val reachSystem = new ReachSystem(processorAnnotator = Some(procAnnotator),
         contextEngineType = contextEngineType,
         contextParams = contextEngineParams)
-    val file = new File(currentPaperPath)
 
+
+
+
+    // ********* Writing to sentences.txt ************
     // reading nxml doc and converting it to Document, to create sentences.txt file
-    val nxmlDoc = nxmlReader.read(file)
-    val document = reachSystem.mkDoc(nxmlDoc)
-    val collectSent = collection.mutable.ListBuffer[String]()
-    for(s <- document.sentences) {
-        val currentSent = s.words.mkString(" ")
-        collectSent += currentSent
-    }
-
-    val pw = new PrintWriter(pathForSentences)
-    for(sen <- collectSent) {
-        pw.write(sen)
-        pw.write("\n")
-    }
-    pw.close()
-
-
-    // extracting mentions to write to evtIntervals.txt
-    val mentions = reachSystem.extractFrom(document)
-    val evtMentionsOnly  = mentions.collect{case evt:EventMention => (evt.sentence, evt.tokenInterval)}
-    val groupBySentIndEvt = evtMentionsOnly.groupBy(_._1)
-    val sorted = ListMap(groupBySentIndEvt.toSeq.sortBy(_._1):_*)
-    val pwevent = new PrintWriter(pathForEvents)
-    for((sentInd, group) <- sorted) {
-        val inter = group.map(_._2)
-        val intervalStringed = inter.map(i => {
-            val str = s"${i.start.toString}-${i.end.toString}"
-            str
-        })
-        val string = intervalStringed.mkString(" ")
-        val toWrite = s"${sentInd.toString} ${string}"
-        pwevent.write(toWrite)
-        pwevent.write("\n")
-    }
-    pwevent.close()
-
-    // extracting context mentions to write to file
-    val contextMentions = mentions filter ContextEngine.isContextMention map (_.asInstanceOf[BioTextBoundMention])
-    val groupsBySentIndexCtx = contextMentions.groupBy(_.sentence)
-    val sortedContextGroups = ListMap(groupsBySentIndexCtx.toSeq.sortBy(_._1):_*)
-    val pwctx = new PrintWriter(pathForContextMentions)
-    for((sentId, ctxGroup) <- sortedContextGroups) {
-        val ctx = ctxGroup.map(bt => (bt.nsId(), bt.tokenInterval, bt.sentenceObj))
-        val ctxMentionStr = ctx.map(c => {
-            var trigger = c._3.words.slice(c._2.start, c._2.end+1)
-            if(trigger(trigger.length-1) == "and" || trigger(trigger.length-1) == ",")
-                trigger = trigger.slice(0,trigger.length-1)
-            val triggerWords = trigger.mkString("_")
-            val s = s"${c._2.start}%${c._2.end}%${triggerWords}%${c._1}"
-            s
-        })
-        val mk = ctxMentionStr.mkString(" ")
-        val str = s"${sentId.toString} ${mk}"
-        pwctx.write(str)
-        pwctx.write("\n")
-    }
-    pwctx.close()
-
-
-
-
-    def mkIntervalStr(seq: Seq[Interval]):Seq[String] = {
-        val unprocStart = seq(0).start
-        val unprocEnd = seq(0).end
-        for(i <- 1 until seq.size) {
-            val currentIntervalStart = seq(i).start
-            val currentIntervalEnd = seq(i).end
-
-
+    val fileListUnfiltered = new File(dirForType)
+    val fileList = fileListUnfiltered.listFiles().filter(x => x.getName.endsWith(".nxml"))
+    for(file <- fileList) {
+        val nxmlDoc = nxmlReader.read(file)
+        val document = reachSystem.mkDoc(nxmlDoc)
+        val collectSent = collection.mutable.ListBuffer[String]()
+        for (s <- document.sentences) {
+            val currentSent = s.words.mkString(" ")
+            collectSent += currentSent
         }
 
-        Seq("")
+        val sentenceFile = new File(pathForSentences)
+        if (!sentenceFile.exists()) {
+            sentenceFile.createNewFile()
+        }
+        val pw = new PrintWriter(sentenceFile)
+        for (sen <- collectSent) {
+            pw.write(sen)
+            pw.write("\n")
+        }
+        pw.close()
+        // ********* Finished writing to sentences.txt ************
+
+
+        // ********* Writing to event_intervals.txt *********
+        val mentions = reachSystem.extractFrom(document)
+        val evtMentionsOnly = mentions.collect { case evt: EventMention => (evt.sentence, evt.tokenInterval) }
+        val groupBySentIndEvt = evtMentionsOnly.groupBy(_._1)
+        val sorted = ListMap(groupBySentIndEvt.toSeq.sortBy(_._1): _*)
+        val eventsFile = new File(pathForEvents)
+        if (!eventsFile.exists()) {
+            eventsFile.createNewFile()
+        }
+        val pwevent = new PrintWriter(eventsFile)
+        for ((sentInd, group) <- sorted) {
+            val inter = group.map(_._2)
+            val intervalStringed = inter.map(i => {
+                val str = s"${i.start.toString}-${i.end.toString}"
+                str
+            })
+            val string = intervalStringed.mkString(" ")
+            val toWrite = s"${sentInd.toString} ${string}"
+            pwevent.write(toWrite)
+            pwevent.write("\n")
+        }
+        pwevent.close()
+        // ********* Finished writing to event_intervals.txt *********
+
+        // ********* Writing to mention_intervals.txt *********
+        val contextMentions = mentions filter ContextEngine.isContextMention map (_.asInstanceOf[BioTextBoundMention])
+        val groupsBySentIndexCtx = contextMentions.groupBy(_.sentence)
+        val sortedContextGroups = ListMap(groupsBySentIndexCtx.toSeq.sortBy(_._1): _*)
+        val mentionsFile = new File(pathForContextMentions)
+        if (!mentionsFile.exists())
+            mentionsFile.createNewFile()
+
+        val pwctx = new PrintWriter(mentionsFile)
+        for ((sentId, ctxGroup) <- sortedContextGroups) {
+            val ctx = ctxGroup.map(bt => (bt.nsId(), bt.tokenInterval, bt.sentenceObj))
+            val ctxMentionStr = ctx.map(c => {
+                var trigger = c._3.words.slice(c._2.start, c._2.end + 1)
+                if (trigger(trigger.length - 1) == "and" || trigger(trigger.length - 1) == ",")
+                    trigger = trigger.slice(0, trigger.length - 1)
+                val triggerWords = trigger.mkString("_")
+                val s = s"${c._2.start}%${c._2.end}%${triggerWords}%${c._1}"
+                s
+            })
+            val mk = ctxMentionStr.mkString(" ")
+            val str = s"${sentId.toString} ${mk}"
+            pwctx.write(str)
+            pwctx.write("\n")
+        }
+        pwctx.close()
+        // ********* Finished writing to mention_intervals.txt *********
     }
+
+
+
+
 
 
 
