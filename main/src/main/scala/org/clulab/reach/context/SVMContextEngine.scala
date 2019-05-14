c1f7dff4c8e1051af6d1c05656d2345172c50ec2
ShraddhaThumsi <thumsishraddhasatish@gmail.com>
2019-05-14 12:39:16 -0400

    need to manually confirm few features

--- main/src/main/scala/org/clulab/reach/context/SVMContextEngine.scala
+++ main/src/main/scala/org/clulab/reach/context/SVMContextEngine.scala
@@ -125,7 +125,7 @@ class SVMContextEngine extends ContextEngine with LazyLogging {
 
                 logger.info(s"For the paper ${aggregatedFeature.PMCID}, event ID: ${k.toString} and context ID: ${ctxId._2}, we have prediction: ${predArrayIntForm(0)}")
 
-                val featureListForDebugging = Seq("sentenceDistance_max", "dependencyDistance_max", "context_frequency_max", "closesCtxOfClass_max", "ctxNegationInTail_max", "evtSentenceFirstPerson_max", "ctxSentencePastTense_max","evtSentencePresentTense_max", "ctxDepTail_prep_min", "ctxDepTail_aux_advcl_min")
+                val featureListForDebugging = Seq("sentenceDistance_max", "dependencyDistance_max", "context_frequency_max", "closesCtxOfClass_max", "ctxNegationInTail_max", "evtSentenceFirstPerson_max", "ctxSentencePastTense_max","evtSentencePresentTense_max")
                 val valueList = featureListForDebugging.map(f => {
                   val index = aggregatedFeature.featureGroupNames.indexOf(f)
                   val featVal = aggregatedFeature.featureGroups(index)
@@ -247,6 +247,14 @@ class SVMContextEngine extends ContextEngine with LazyLogging {
         if(unAggregateFeatureName(featSeq).contains(ctx)) ctxDepFeatures += ctx
       }
     }
+
+    logger.info("Inside Input row function")
+    val hardcodedFeaturesString = hardCodedFeatureNames.mkString(" ")
+    val ctxDepFeaturesString = ctxDepFeatures.mkString(" ")
+    val evtDepFeaturesString = evtDepFeatures.mkString(" ")
+    logger.info(s"Printing hardcoded features present in input row: ${hardcodedFeaturesString}")
+    logger.info(s"Printing ctxDep features present in input row: ${ctxDepFeaturesString}")
+    logger.info(s"Printing evtDep features present in input row: ${evtDepFeaturesString}")
 
 
 
