f769dd53a55e709e821e031ecdfde5e52872ca0f
ShraddhaThumsi <thumsishraddhasatish@gmail.com>
2019-06-05 15:28:51 -0700

    Extracted feature aggregator into a new class. Next important step is to add detailed comments and clean up the code

--- main/src/main/scala/org/clulab/reach/context/SVMContextEngine.scala
+++ main/src/main/scala/org/clulab/reach/context/SVMContextEngine.scala
@@ -5,9 +5,7 @@ import org.clulab.reach.mentions.{BioEventMention, BioMention, BioTextBoundMenti
 import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.LazyLogging
 import org.clulab.context.classifiers.LinearSVMContextClassifier
-import org.clulab.context.utils.{AggregatedContextInstance, CodeUtils, ContextPairInstance}
-import scala.collection.mutable.ListBuffer
-import scala.collection.mutable
+import org.clulab.context.utils.{AggregatedContextInstance, ContextPairInstance}
 import scala.collection.immutable
 
 class SVMContextEngine(sentenceWindow:Option[Int] = None) extends ContextEngine with LazyLogging {
@@ -154,93 +152,6 @@ class SVMContextEngine(sentenceWindow:Option[Int] = None) extends ContextEngine 
     sentIndex+tokenIntervalStart+tokenIntervalEnd
   }
 
-  def aggregateFeatures(instances:Seq[ContextPairInstance], featValLookUp:Map[ContextPairInstance, (Map[String,Double],Map[String,Double],Map[String,Double])]):AggregatedContextInstance = {
-
-    val label = None
-    val featureSetNames = collection.mutable.ListBuffer[String]()
-    val featureSetValues = collection.mutable.ListBuffer[Double]()
-    val inputRows = instances
-    val featNameToVals = collection.mutable.Map[String,mutable.ListBuffer[Double]]()
-    val specfeatureNamesToUse = instances(0).specificFeatureNames
-    val ctxFeatureNamesToUse = instances(0).ctx_dependencyTails
-    val evtFeatureNamesToUse = instances(0).evt_dependencyTails
-    def featureValuePairing(aggr:Map[String,(Double,Double, Double, Int)]): Seq[(String,Double)] = {
-      val pairings = collection.mutable.ListBuffer[(String,Double)]()
-      for((key,value) <- aggr) {
-        val extendedName = CodeUtils.extendFeatureName(key)
-        val minTup = (extendedName._1, value._1)
-        val maxTup = (extendedName._2, value._2)
-        val avgTup = (extendedName._3, value._3/value._4)
-
-        val list = ListBuffer(minTup, maxTup, avgTup)
-        pairings ++= list
-      }
-      pairings
-    }
-
-    def addAggregatedOnce(input: Seq[(String, Double)]):Unit = {
-      for((name,value) <- input) {
-        featureSetNames += name
-        featureSetValues += value
-      }
-    }
-
-
-    // we read through the input row values and add them to a name -> list of features map.
-    // So for a given feature name as key, we will have a list of double as values, where the doubles are the values to the feature in a given input row.
-
-    for(in <- inputRows) {
-      val (specificVals, evtVals, ctxVals) = featValLookUp(in)
-      for((spec,value)<-specificVals) {
-        if(featNameToVals.contains(spec)) {
-          val currentList = featNameToVals(spec)
-          currentList += value
-        }
-        else {
-          val toAddVal = collection.mutable.ListBuffer[Double]()
-          toAddVal += value
-          featNameToVals ++= Map(spec -> toAddVal)
-        }
-      }
-
-      for((spec,value)<-evtVals) {
-        if(featNameToVals.contains(spec)) {
-          val currentList = featNameToVals(spec)
-          currentList += value
-        }
-        else {
-          val toAddVal = collection.mutable.ListBuffer[Double]()
-          toAddVal += value
-          featNameToVals ++= Map(spec -> toAddVal)
-        }
-      }
-
-      for((spec,value)<-ctxVals) {
-        if(featNameToVals.contains(spec)) {
-          val currentList = featNameToVals(spec)
-          currentList += value
-        }
-        else {
-          val toAddVal = collection.mutable.ListBuffer[Double]()
-          toAddVal += value
-          featNameToVals ++= Map(spec -> toAddVal)
-        }
-      }
-    }
-    val aggregatedSpecVals = aggregateInputRowFeatValues(specfeatureNamesToUse, featNameToVals.toMap)
-    val aggregatedctxDepVals = aggregateInputRowFeatValues(ctxFeatureNamesToUse.toSeq, featNameToVals.toMap)
-    val aggregatedevtDepVals = aggregateInputRowFeatValues(evtFeatureNamesToUse.toSeq, featNameToVals.toMap)
-
-    val specFeatVal = featureValuePairing(aggregatedSpecVals)
-    val ctxFeatVal = featureValuePairing(aggregatedctxDepVals)
-    val evtFeatVal = featureValuePairing(aggregatedevtDepVals)
-
-    addAggregatedOnce(specFeatVal)
-    addAggregatedOnce(ctxFeatVal)
-    addAggregatedOnce(evtFeatVal)
-    val newAggRow = AggregatedContextInstance(0, instances(0).PMCID, "", "", label, featureSetValues.toArray,featureSetNames.toArray)
-    newAggRow
-  }
 
   private def writeRowToFile(row:AggregatedContextInstance, evtID: String, ctxID: String):Unit = {
     val typeOfPaper = config.getString("svmContext.paperType")
@@ -257,8 +168,6 @@ class SVMContextEngine(sentenceWindow:Option[Int] = None) extends ContextEngine 
         outputPaperDir.mkdirs()
       }
 
-
-
         if(currentPMCID == fileNamePMCID) {
           val pathForRow = outPaperDirPath.concat(s"/AggregatedRow_${currentPMCID}_${evtID}_${ctxID}.txt")
           val sentenceFile = new File(pathForRow)
@@ -270,32 +179,6 @@ class SVMContextEngine(sentenceWindow:Option[Int] = None) extends ContextEngine 
           os.writeObject(row)
           os.close()
         }
-
-
-
     }
   }
-
-  def aggregateInputRowFeatValues(features:Seq[String], lookUpTable: Map[String,mutable.ListBuffer[Double]]):Map[String,(Double,Double, Double, Int)] = {
-    val resultingMap = collection.mutable.Map[String,(Double,Double, Double, Int)]()
-    for(r <- features) {
-      if(lookUpTable.contains(r)) {
-        val valueList = lookUpTable(r)
-        val min = valueList.foldLeft(Double.MaxValue)(Math.min(_,_))
-        val max = valueList.foldLeft(Double.MinValue)(Math.max(_,_))
-        val sum = valueList.foldLeft(0.0)(_ + _)
-        val tup = (min,max,sum,valueList.size)
-        resultingMap ++= Map(r -> tup)
-      }
-
-      else {
-        val tup = (0.0, 0.0, 0.0, 1)
-        resultingMap ++= Map(r -> tup)
-      }
-    }
-    resultingMap.toMap
-  }
-
-
-
 }
