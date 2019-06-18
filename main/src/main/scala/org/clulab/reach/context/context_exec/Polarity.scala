e71cae6c8d2efe28dfcbd7d059b6c1456a543ec0
ShraddhaThumsi <thumsishraddhasatish@gmail.com>
2019-06-18 14:43:08 -0700

    Removed ctx sentence, also modified separator for the evt sentence, because any commas in the sentence will confuse the csv reader.

--- main/src/main/scala/org/clulab/reach/context/context_exec/Polarity.scala
+++ main/src/main/scala/org/clulab/reach/context/context_exec/Polarity.scala
@@ -8,10 +8,25 @@ object Polarity extends App {
   val polarityDir = config.getString("polarityContext.polarityDir")
   val typeOfPaper = config.getString("polarityContext.typeOfPaper")
   val paperList = List("PMC2958340", "PMC2910130", "PMC4236140", "PMC4142739", "PMC4446607", "PMC4092102", "PMC2587086", "PMC3138418", "PMC3666248", "PMC2636845", "PMC3635065", "PMC3640659", "PMC2686753", "PMC3119364")
+  val header = List("PMCID","evtID","ctxID","polarity_type","evt_sentence")
+  val polFileName = polarityDir.concat("polarity_instances.txt")
+  val polarityFile = new File(polFileName)
+  if (!polarityFile.exists())
+    polarityFile.createNewFile()
+  val printWriter = new PrintWriter(polarityFile)
+  printWriter.write(header.mkString(","))
+  printWriter.write("\n")
+  val linePerRow = collection.mutable.ListBuffer[String]()
   for(pmcid<-paperList) {
     val currentDir = new File(config.getString("svmContext.contextOutputDir").concat(s"${typeOfPaper}/${pmcid}"))
     val currentPolarityFile = currentDir.listFiles().filter(_.getName == "polarity.txt")
+    val lines = Source.fromFile(currentPolarityFile(0)).getLines()
+    linePerRow ++= lines
+  }
 
+  for(l<- linePerRow) {
+    printWriter.write(l)
+    printWriter.write("\n")
   }
 
 }
