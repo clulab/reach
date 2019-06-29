f3892323d2ce3f1d541545f2ad4bfc2e69f3222a
ShraddhaThumsi <thumsishraddhasatish@gmail.com>
2019-06-28 21:51:55 -0700

    filtering pairs to speed up - also running on sentence window 5

--- main/src/main/scala/org/clulab/reach/context/context_exec/Polarity4CrossValidation.scala
+++ main/src/main/scala/org/clulab/reach/context/context_exec/Polarity4CrossValidation.scala
@@ -5,6 +5,7 @@ import java.io.File
 import com.typesafe.config.ConfigFactory
 import org.clulab.context.utils.AggregatedContextInstance
 import org.clulab.reach.context.context_exec.CrossValBySentDist.config
+import org.clulab.reach.context.context_utils.ContextFeatureUtils
 
 object Polarity4CrossValidation extends App {
   val config = ConfigFactory.load()
@@ -17,4 +18,18 @@ object Polarity4CrossValidation extends App {
   val keysForLabels = collection.mutable.HashMap[AggregatedContextInstance, (String, String, String)]()
   val filterForFasterRun = List("0","1","2","3")
   val smallNumOfDirs = allSentDirs.filter(x => filterForFasterRun.contains(x.getName))
+  for(d<- smallNumOfDirs) {
+    val rowFiles = d.listFiles().filter(_.getName.contains("Aggregated"))
+    val rowsForCurrentSent = collection.mutable.ListBuffer[AggregatedContextInstance]()
+    for(r<-rowFiles) {
+      val pathToRow = dirForType.concat(s"/${d.getName}").concat(s"/${r.getName}")
+      val rowSpecs = ContextFeatureUtils.createAggRowSpecsFromFile(r)
+      val row = ContextFeatureUtils.readAggRowFromFile(pathToRow)
+      keysForLabels ++= Map(row -> rowSpecs)
+      rowsForCurrentSent += row
+    }
+    val intName = Integer.parseInt(d.getName)
+    val entry = Map(intName -> rowsForCurrentSent)
+    allRowsBySentDist ++= entry
+  }
 }
