24fd13ef35dbbf9cf3df41a8a613d781c39e2a07
Zhengzhong Liang <zhengzhongliang@Zhengzhongs-MacBook-Pro.local>
2019-06-30 17:30:18 -0700

    fix bug

--- runPolarityTest.sh
+++ runPolarityTest.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-sbt "testOnly org.clulab.polarity.ml.TestRegulationEvents"
-sbt "testOnly org.clulab.polarity.ml.TestActivationEvents"
-sbt "testOnly org.clulab.polarity.ml.TestPolarity"
-sbt "testOnly org.clulab.polarity.ml.TestHyphenedEvents"
-sbt "testOnly org.clulab.polarity.ml.NegationTests"
-sbt "testOnly org.clulab.polarity.ml.HypothesisTests"
-sbt "testOnly org.clulab.polarity.ml.TestTemplaticAutoEvents"
-sbt "testOnly org.clulab.polarity.ml.TestTemplaticSimpleDeEvents"
-sbt "testOnly org.clulab.polarity.ml.TestTemplaticSimpleEvents"
-sbt "testOnly org.clulab.polarity.ml.TestTranscriptionEvents"
-sbt "testOnly org.clulab.polarity.ml.TestTranslocationEvents"
+sbt "testOnly org.clulab.reach.TestRegulationEvents"
+sbt "testOnly org.clulab.reach.TestActivationEvents"
+sbt "testOnly org.clulab.reach.TestPolarity"
+sbt "testOnly org.clulab.reach.TestHyphenedEvents"
+sbt "testOnly org.clulab.reach.NegationTests"
+sbt "testOnly org.clulab.reach.HypothesisTests"
+sbt "testOnly org.clulab.reach.TestTemplaticAutoEvents"
+sbt "testOnly org.clulab.reach.TestTemplaticSimpleDeEvents"
+sbt "testOnly org.clulab.reach.TestTemplaticSimpleEvents"
+sbt "testOnly org.clulab.reach.TestTranscriptionEvents"
+sbt "testOnly org.clulab.reach.TestTranslocationEvents"
 
 
