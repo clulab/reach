67bf53f2f7ee148080d2d0dbc6fea7ccbd0205dd
ShraddhaThumsi <thumsishraddhasatish@gmail.com>
2019-07-02 15:16:50 -0700

    checking unique pairs

--- main/src/main/scala/org/clulab/reach/context/context_utils/EventContextPairGenerator.scala
+++ main/src/main/scala/org/clulab/reach/context/context_utils/EventContextPairGenerator.scala
@@ -12,6 +12,20 @@ class EventContextPairGenerator(mentions:Seq[BioMention], ctxMentions:Seq[BioTex
     case evt:BioEventMention => evt
   }
 
+  val ns = ctxMentions.map(c => c.nsId())
+  val mut = collection.mutable.HashMap[String, Int]()
+  for(s <- ns) {
+    if(mut.contains(s)) {
+      var fr = mut(s)
+      fr += 1
+      mut ++= Map(s -> fr)
+    }
+    else mut ++= Map(s -> 1)
+  }
+
+  for((s,f) <- mut) {
+    println(s"The context label ${s} appears ${f} times")
+  }
 
   // The constructor of this class is supplied with all the mentions and BioTextBoundMentions (i.e. context IDs)
   // Let the given Seq[BioTextBoundMention] be of size m
@@ -19,7 +33,7 @@ class EventContextPairGenerator(mentions:Seq[BioMention], ctxMentions:Seq[BioTex
   // A cross product is then generated for the context-event pairs. The resultant seq(pairs) will be of size m*n
   def yieldContextEventPairs():Seq[Pair] = {
 
-    for(evt <- evtMentions.toSet.toSeq; ctx <- ctxMentions.toSet.toSeq) yield (evt, ctx)
+    for(evt <- evtMentions; ctx <- ctxMentions) yield (evt, ctx)
 
   }
 
