1272236746b6effbc808d17db0a6ac68d72474d5
ShraddhaThumsi <thumsishraddhasatish@gmail.com>
2019-08-14 16:44:22 -0400

    context-event pairs are not getting written and I don't know why. So I'm creating the printwriter with the path to the file rather than the file

--- main/src/main/scala/org/clulab/reach/context/context_utils/ContextFeatureUtils.scala
+++ main/src/main/scala/org/clulab/reach/context/context_utils/ContextFeatureUtils.scala
@@ -35,6 +35,7 @@ object ContextFeatureUtils {
         contextLabelsFile.createNewFile()
       }
       val pw = new PrintWriter(contextLabelsFilePath)
+      println(s"Paper ID: ${p._1.document.id}, Event ID := ${extractEvtId(p._1)}, Context ID := ${p._2.nsId()} \n")
       pw.write(s"Paper ID: ${p._1.document.id}, Event ID := ${extractEvtId(p._1)}, Context ID := ${p._2.nsId()} \n")
       val featureExtractor = new ContextFeatureExtractor(p, ctxMentions)
       featureExtractor.extractFeaturesToCalcByBestFeatSet()
