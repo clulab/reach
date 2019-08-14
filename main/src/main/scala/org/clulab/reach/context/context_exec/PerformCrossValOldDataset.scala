e5fd6e0b8c0420d626215464b3782848c0256a20
ShraddhaThumsi <thumsishraddhasatish@gmail.com>
2019-08-14 18:44:24 -0400

    Writing pairs to file isn't working, so I'll just run individual papers when I do need the pairs to annotate.

--- main/src/main/scala/org/clulab/reach/context/context_exec/PerformCrossValOldDataset.scala
+++ main/src/main/scala/org/clulab/reach/context/context_exec/PerformCrossValOldDataset.scala
@@ -2,7 +2,8 @@ package org.clulab.reach.context.context_exec
 
 import com.typesafe.config.ConfigFactory
 import org.clulab.context.classifiers.LinearSVMContextClassifier
-
+import org.clulab.context.utils.AggregatedContextInstance
+import java.io.File
 object PerformCrossValOldDataset extends App {
   val config = ConfigFactory.load()
   val svmWrapper = new LinearSVMContextClassifier()
@@ -18,4 +19,14 @@ object PerformCrossValOldDataset extends App {
   if(classifierToUse == null) throw new NullPointerException("No classifier found on which I can predict. Please make sure the SVMContextEngine class receives a valid Linear SVM classifier.")
 
   val labelFile = config.getString("svmContext.labelFileOldDataset")
+  val parentDirForRows = config.getString("polarityContext.aggrRowWrittenToFilePerPaper")
+  val allPapersDirs = new File(parentDirForRows).listFiles().filter(_.isDirectory)
+  val idMap = collection.mutable.HashMap[(String,String,String),AggregatedContextInstance]()
+  val keysForLabels = collection.mutable.HashMap[AggregatedContextInstance, (String, String, String)]()
+  val allRowsByPaperID = collection.mutable.HashMap[String, Seq[AggregatedContextInstance]]()
+  for(paperDir <- allPapersDirs) {
+    val rowFiles = paperDir.listFiles().filter(_.getName.contains("Aggregated"))
+    val rowsForCurrentSent = collection.mutable.ListBuffer[AggregatedContextInstance]()
+  }
+
 }
