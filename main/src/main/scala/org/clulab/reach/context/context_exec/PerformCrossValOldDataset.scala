a9ef5c97acc217437fe6023542cafef68bbe9759
ShraddhaThumsi <thumsishraddhasatish@gmail.com>
2019-08-16 12:25:03 -0400

    added code to read rows from file and create a paperID -> set(rows) map

--- main/src/main/scala/org/clulab/reach/context/context_exec/PerformCrossValOldDataset.scala
+++ main/src/main/scala/org/clulab/reach/context/context_exec/PerformCrossValOldDataset.scala
@@ -2,7 +2,7 @@ package org.clulab.reach.context.context_exec
 
 import com.typesafe.config.ConfigFactory
 import org.clulab.context.classifiers.LinearSVMContextClassifier
-import org.clulab.context.utils.AggregatedContextInstance
+import org.clulab.context.utils.{AggregatedContextInstance, CodeUtils}
 import java.io.File
 
 import org.clulab.reach.context.context_utils.ContextFeatureUtils
@@ -20,7 +20,12 @@ object PerformCrossValOldDataset extends App {
 
   if(classifierToUse == null) throw new NullPointerException("No classifier found on which I can predict. Please make sure the SVMContextEngine class receives a valid Linear SVM classifier.")
 
+  // The difference between the old papers parsed in the old reach vs the new Reach involves an extra token in the event. i.e. for a given paper and context label, the old Reach captured the event 0045, but the new Reach may capture it as 0046.
+  // This is the same event, it only includes one extra token. But if we do an exact string match the way we did in the CrossValBySentDist code, we will get an empty set.
+  // To combat this, this code will parse the events ID as an integer, and only consider those events as the same if they math.abs(a-b) <= 1. That means, if the integer values differ by 1, they are just one token away.
+
   val labelFile = config.getString("svmContext.labelFileOldDataset")
+  val labelMapFromOldDataset = CodeUtils.generateLabelMap(labelFile)
   val parentDirForRows = config.getString("polarityContext.aggrRowWrittenToFilePerPaper")
   val allPapersDirs = new File(parentDirForRows).listFiles().filter(_.isDirectory)
   val idMap = collection.mutable.HashMap[(String,String,String),AggregatedContextInstance]()
@@ -49,7 +54,11 @@ object PerformCrossValOldDataset extends App {
 
   println(s"We have a total of ${allRowsByPaperID.size} papers, and the micro-averaged precision will be calculated by treating each paper as a test case in the cross validation loop")
   val giantScoreBoard = collection.mutable.HashMap[String, Double]()
+  // in the cross validation, each paper will be considered as test case once. So when a given paper is a test case, all other papers and their corresponding labels must be the training case.
   for((paperID, rowsPerPaper) <- allRowsByPaperID) {
-
+    val trainingCaseRows = allRowsByPaperID.filter(_._1 != paperID)
+    val trainingLabels = labelMapFromOldDataset.filter(_._1._1 != paperID)
+    println(trainingCaseRows.size)
+    println(trainingLabels.size)
   }
 }
