97bd5166674012401ddc7261bcd2740f4257a3e5
ShraddhaThumsi <thumsishraddhasatish@gmail.com>
2019-09-02 22:57:09 -0700

    fixing precison and recall code

---
+++ main/src/main/scala/org/clulab/reach/context/context_exec/CrossValBySentDist.scala
@@ -0,0 +1,50 @@
+package org.clulab.reach.context.context_exec
+
+import java.io.File
+
+import org.clulab.context.utils.{AggregatedContextInstance, CodeUtils}
+import com.typesafe.config.ConfigFactory
+import org.clulab.context.classifiers.LinearSVMContextClassifier
+import org.clulab.reach.context.context_utils.ContextFeatureUtils
+
+object CrossValBySentDist extends App{
+  val config = ConfigFactory.load()
+  val labelFile = config.getString("svmContext.labelFile")
+  val svmWrapper = new LinearSVMContextClassifier()
+  val config = ConfigFactory.load()
+  val configPath = config.getString("contextEngine.params.trainedSvmPath")
+  val trainedSVMInstance = svmWrapper.loadFrom(configPath)
+  val classifierToUse = trainedSVMInstance.classifier match {
+    case Some(x) => x
+    case None => {
+      null
+    }
+  }
+
+  if(classifierToUse == null) throw new NullPointerException("No classifier found on which I can predict. Please make sure the SVMContextEngine class receives a valid Linear SVM classifier.")
+  val dirForType = config.getString("policy4Params.mentionsOutputFile").concat("sentenceWindows")
+  val allSentDirs = new File(dirForType).listFiles().filter(_.isDirectory)
+  val allRowsBySentDist = collection.mutable.HashMap[Int, Seq[AggregatedContextInstance]]()
+  val keysForLabels = collection.mutable.HashMap[AggregatedContextInstance, (String, String, String)]()
+  val filterForFasterRun = List("15")
+  val smallNumOfDirs = allSentDirs.filter(x => filterForFasterRun.contains(x.getName))
+  for(d<- smallNumOfDirs) {
+    val rowFiles = d.listFiles().filter(_.getName.contains("Aggregated"))
+    val rowsForCurrentSent = collection.mutable.ListBuffer[AggregatedContextInstance]()
+    for(r<-rowFiles) {
+      val pathToRow = dirForType.concat(s"/${d.getName}").concat(s"/${r.getName}")
+      val rowSpecs = ContextFeatureUtils.createAggRowSpecsFromFile(r)
+      val row = ContextFeatureUtils.readAggRowFromFile(pathToRow)
+      if(!rowsForCurrentSent.contains(row))
+      {keysForLabels ++= Map(row -> rowSpecs)
+        rowsForCurrentSent += row}
+
+    }
+    val intName = Integer.parseInt(d.getName)
+    val entry = Map(intName -> rowsForCurrentSent)
+    allRowsBySentDist ++= entry
+  }
+
+  println(allRowsBySentDist.size)
+
+}
