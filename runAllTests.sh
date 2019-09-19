158a86bb175bc8acee621441d5d18a8130530ab2
Mihai Surdeanu <surdeanu@gmail.com>
2019-09-18 20:06:23 -0700

    Added exit code to runAllTests.sh

--- runAllTests.sh
+++ runAllTests.sh
@@ -6,10 +6,40 @@
 #sbt 'run-main org.clulab.processors.server.ProcessorServer' &
 #sleep 20
 
+overallExitCode=0
+
 sbt test
+
+if [ $? -ne 0 ]; then
+    echo "The test suite failed!"
+    overallExitCode=1
+fi
+
 sbt main/test
+
+if [ $? -ne 0 ]; then
+    echo "The main/test suite failed!"
+    overallExitCode=1
+fi
+
 sbt causalAssembly/test
+
+if [ $? -ne 0 ]; then
+    echo "The causalAssembly/test suite failed!"
+    overallExitCode=1
+fi
+
 sbt export/test
+
+if [ $? -ne 0 ]; then
+    echo "The export/test suite failed!"
+    overallExitCode=1
+fi
 
 #echo 'Stopping Processor Server...'
 #sbt 'run-main org.clulab.processors.csshare.ShutdownProcessorServer'
+
+if [ $overallExitCode -ne 0 ]; then
+	echo "At least one test suite failed!"
+	exit 1
+fi
