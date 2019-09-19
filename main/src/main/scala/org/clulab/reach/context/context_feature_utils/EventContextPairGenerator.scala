d67b5d0ffb77861a2ee83cecabdd2920ccbdf529
ShraddhaThumsi <thumsishraddhasatish@gmail.com>
2019-09-19 00:07:10 -0700

    checking if tests pass if ruleBasedEngine is used

--- main/src/main/scala/org/clulab/reach/context/context_feature_utils/EventContextPairGenerator.scala
+++ main/src/main/scala/org/clulab/reach/context/context_feature_utils/EventContextPairGenerator.scala
@@ -11,7 +11,9 @@ class EventContextPairGenerator(mentions:Seq[BioMention], ctxMentions:Seq[BioTex
   val evtMentions = mentions collect  {
     case evt:BioEventMention => evt
   }
-
+  val groundingIDs = ctxMentions.map(x => x.nsId())
+  println(s"Inside pair generator for SVM. Checking if context mentions have rice in them: ${groundingIDs.contains("taxonomy:4530")}")
+  println(s"Inside pair generator for SVM. Checking if context mentions have mice/mouse in them: ${groundingIDs.contains("taxonomy:10090")}")
   // The constructor of this class is supplied with all the mentions and BioTextBoundMentions (i.e. context IDs)
   // Let the given Seq[BioTextBoundMention] be of size m
   // We will filter the event mentions from the mentions (let it be of size n)
