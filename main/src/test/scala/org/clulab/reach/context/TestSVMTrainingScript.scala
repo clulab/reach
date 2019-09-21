c26208cd64ef65de19dab037c8d80caa65f2c36d
ShraddhaThumsi <thumsishraddhasatish@gmail.com>
2019-09-21 15:55:52 -0700

    there seems to be some error in the shell scripting code becasuse of recursive search of directories, so I'm testing for only files and not directories,

--- main/src/test/scala/org/clulab/reach/context/TestSVMTrainingScript.scala
+++ main/src/test/scala/org/clulab/reach/context/TestSVMTrainingScript.scala
@@ -15,7 +15,7 @@ class TestSVMTrainingScript extends FlatSpec with Matchers {
   val commandLineScriptWithoutParams = s"sbt 'run-main org.clulab.reach.context.svm_scripts.TrainSVMInstance'"
 
   "SVM training script" should "create a .dat file to save the trained SVM model to" in {
-    val listOfFilesFromScriptRun = commandLineScriptWithParams #| "ls -al" #| "grep svm_model_from_train_script.dat"!
+    val listOfFilesFromScriptRun = commandLineScriptWithParams #| "ls" #| "grep svm_model_from_train_script.dat"!
     listOfFilesFromScriptRun should be (0)
   }
 
