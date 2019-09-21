2a3c8770a5e03c395e78a25d0f32db790997f341
ShraddhaThumsi <thumsishraddhasatish@gmail.com>
2019-09-21 15:52:55 -0700

    added two tests: 1 to check for illegal argument exception, and 1 to test the creation of the .dat file. I need to figure out how to test the feature size and and that degenerate papers should not be a part of the degenerate set

--- main/src/test/scala/org/clulab/reach/context/TestSVMTrainingScript.scala
+++ main/src/test/scala/org/clulab/reach/context/TestSVMTrainingScript.scala
@@ -15,12 +15,12 @@ class TestSVMTrainingScript extends FlatSpec with Matchers {
   val commandLineScriptWithoutParams = s"sbt 'run-main org.clulab.reach.context.svm_scripts.TrainSVMInstance'"
 
   "SVM training script" should "create a .dat file to save the trained SVM model to" in {
-    val listOfFilesFromScriptRun = commandLineScriptWithParams #| "ls -al" #| "grep svm_model_from_train_script.dat" !
+    val listOfFilesFromScriptRun = commandLineScriptWithParams #| "ls -al" #| "grep svm_model_from_train_script.dat"!
     listOfFilesFromScriptRun should be (0)
   }
 
   "SVM training script" should "throw an exception if no arguments are passed" in {
-    val result = commandLineScriptWithoutParams !!
+    val result = commandLineScriptWithoutParams!!
     val resultThrowsException = result.contains("java.lang.IllegalArgumentException")
     resultThrowsException should be (true)
   }
