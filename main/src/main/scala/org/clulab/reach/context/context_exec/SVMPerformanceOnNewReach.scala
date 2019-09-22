7537681df59a930274610303abca613455055a55
ShraddhaThumsi <thumsishraddhasatish@gmail.com>
2019-09-22 12:55:49 -0700

    added code to check if events and contexts align. Comments to various matchings between events has been provided

--- main/src/main/scala/org/clulab/reach/context/context_exec/SVMPerformanceOnNewReach.scala
+++ main/src/main/scala/org/clulab/reach/context/context_exec/SVMPerformanceOnNewReach.scala
@@ -25,7 +25,6 @@ object SVMPerformanceOnNewReach extends App {
   if(classifierToUse == null) throw new NullPointerException("No classifier found on which I can predict. Please make sure the SVMContextEngine class receives a valid Linear SVM classifier.")
   val labelFile = config.getString("svmContext.labelFileOldDataset")
   val labelMap = CodeUtils.generateLabelMap(labelFile).toSeq
-  println(labelMap)
   val specsByRow = collection.mutable.HashMap[AggregatedContextInstance, (String,String,String)]()
   val pathToParentdirToLoadNewRows = config.getString("polarityContext.aggrRowWrittenToFilePerPaper")
   val parentDirfileInstanceToLoadNewRows = new File(pathToParentdirToLoadNewRows)
@@ -44,6 +43,94 @@ object SVMPerformanceOnNewReach extends App {
       }
     }
     paperIDByNewRowsMap ++= Map(paperID -> listOfRowsInPaper)
+  }
+
+  val giantTruthLabelList = collection.mutable.ListBuffer[Int]()
+  val giantPredictedLabelList = collection.mutable.ListBuffer[Int]()
+  for((paperID, testRows) <- paperIDByNewRowsMap) {
+    val testRowsWithMatchingLabels = collection.mutable.ListBuffer[AggregatedContextInstance]()
+    val predictedLabelsInThisPaper = collection.mutable.ListBuffer[Int]()
+    val trueLabelsInThisPaper = collection.mutable.ListBuffer[Int]()
+    val possibleLabelIDsInThisPaper = labelMap.filter(_._1._1 == paperID)
+    for(tester <- testRows) {
+      for((labelID,label) <- possibleLabelIDsInThisPaper) {
+        val specForTester = specsByRow(tester)
+        if(eventsAlign(specForTester._2,labelID._2) && contextsAlign(specForTester._3,labelID._3)) {
+          testRowsWithMatchingLabels += tester
+          trueLabelsInThisPaper += label
+        }
+      }
+    }
+
+
+    for(validTestRow <- testRowsWithMatchingLabels) {
+      val predictionByTestRow = trainedSVMInstance.predict(Seq(validTestRow))(0)
+      predictedLabelsInThisPaper += predictionByTestRow
+    }
+
+    giantTruthLabelList ++= trueLabelsInThisPaper
+    giantPredictedLabelList ++= predictedLabelsInThisPaper
+
+  }
+
+  println(s"After prediction, ${giantTruthLabelList.size} truth labels were found")
+  println(s"After prediction, ${giantPredictedLabelList.size} predicted labels were found")
+
+
+  def eventsAlign(evtID1: String, evtID2: String):Boolean = {
+    val tupEvt1 = parseEventIDToTup(evtID1)
+    val tupEvt2 = parseEventIDToTup(evtID2)
+    // the purpose of this function is to align events.
+    // Since we need to consider possible overlap and containment of one event by another, we need to test if both ways are possible
+    eventsAlign(tupEvt1, tupEvt2) || eventsAlign(tupEvt2, tupEvt1)
+  }
+
+  def contextsAlign(ctxID1: String, ctxID2: String):Boolean = {
+    ctxID1 == ctxID2
+  }
+
+
+
+  def parseEventIDToTup(eventID: String):(Int,Int,Int) = {
+    val sentenceIndexString = eventID.split("from")(0).replace("in","")
+    val eventTokenString = eventID.split("from")(1)
+    val sentenceIndex = Integer.parseInt(sentenceIndexString)
+    val eventTokenStart = Integer.parseInt(eventTokenString.split("to")(0))
+    val eventTokenEnd = Integer.parseInt(eventTokenString.split("to")(1))
+    (sentenceIndex,eventTokenStart,eventTokenEnd)
+  }
+
+
+  def eventsAlign(eventSpec1:(Int,Int,Int), eventSpec2:(Int,Int,Int)):Boolean = {
+    val sameSentenceIndex = eventSpec1._1 == eventSpec2._1
+    val someMatchExists = isThereSomeMatch(eventSpec1._2, eventSpec1._3, eventSpec2._2, eventSpec2._3)
+    sameSentenceIndex && someMatchExists
+  }
+
+  def isThereSomeMatch(evt1Start:Int, evt1End:Int, evt2Start:Int, evt2End: Int):Boolean = {
+    // exact match is when both events have the same start and end token values
+    val exactMatch = ((evt1Start == evt2Start) && (evt1End == evt2End))
+
+
+    // same start is when the tokens start at the same point, but one event must end before the other
+    // please note that one event has to end before the other, because if they were the same, they would have already
+    // been counted as an exactMatch
+    val sameStart = ((evt1Start == evt2Start) && (evt1End < evt2End))
+
+
+    // same end is when one event may start after the other has already started, but they end at the same token
+    // again, they must start at different points, else they would have been counted as an exact match
+    val sameEnd = ((evt1End == evt2End) && (evt1Start < evt2Start))
+
+    // containment is when one event is completely inside the other event
+    val containment = ((evt1Start < evt2Start) && (evt1End > evt2End))
+
+    //overlap is when one event starts before the other, but also ends before the other.
+    // the end of the first event has to be before the second event finishes.
+    val overlap = ((evt1Start < evt2Start) && (evt1End < evt2End) && (evt1End > evt2Start))
+
+
+    exactMatch || sameStart || sameEnd || containment || overlap
   }
 
 
