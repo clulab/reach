4d0f3e755bb94db277b54c2775ea8b3c3e0819da
ShraddhaThumsi <thumsishraddhasatish@gmail.com>
2019-09-22 11:12:49 -0700

    starting code to test performance of SVM on old dataset

--- main/src/main/scala/org/clulab/reach/context/context_exec/SVMPerformanceOnNewReach.scala
+++ main/src/main/scala/org/clulab/reach/context/context_exec/SVMPerformanceOnNewReach.scala
@@ -2,6 +2,9 @@ package org.clulab.reach.context.context_exec
 
 import com.typesafe.config.ConfigFactory
 import org.clulab.context.classifiers.LinearSVMContextClassifier
+import java.io.File
+
+import org.clulab.context.utils.AggregatedContextInstance
 
 object SVMPerformanceOnNewReach extends App {
   val svmWrapper = new LinearSVMContextClassifier()
@@ -9,7 +12,6 @@ object SVMPerformanceOnNewReach extends App {
 
 
   val configPath = config.getString("contextEngine.params.trainedSvmPath")
-  println(configPath)
   val trainedSVMInstance = svmWrapper.loadFrom(configPath)
   val classifierToUse = trainedSVMInstance.classifier match {
     case Some(x) => x
@@ -18,8 +20,14 @@ object SVMPerformanceOnNewReach extends App {
     }
   }
 
-  println(classifierToUse == null)
 
   if(classifierToUse == null) throw new NullPointerException("No classifier found on which I can predict. Please make sure the SVMContextEngine class receives a valid Linear SVM classifier.")
   val labelFile = config.getString("svmContext.labelFileOldDataset")
+
+  val pathTodirToLoadNewRows = config.getString("polarityContext.aggrRowWrittenToFilePerPaper")
+  val fileInstanceToLoadNewRows = new File(pathTodirToLoadNewRows)
+  val paperDirs = fileInstanceToLoadNewRows.listFiles().filter(x => x.isDirectory && x.getName.startsWith("PMC"))
+  val paperIDByNewRowsMap = collection.mutable.HashMap[String, Seq[AggregatedContextInstance]]()
+
+
 }
