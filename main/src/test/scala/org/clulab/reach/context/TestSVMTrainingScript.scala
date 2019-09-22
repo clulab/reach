b9c79d8054756014d20885db0fd089ef349decb6
ShraddhaThumsi <thumsishraddhasatish@gmail.com>
2019-09-21 18:04:20 -0700

    trying with linestream instead of .!

--- main/src/test/scala/org/clulab/reach/context/TestSVMTrainingScript.scala
+++ main/src/test/scala/org/clulab/reach/context/TestSVMTrainingScript.scala
@@ -18,9 +18,9 @@ class TestSVMTrainingScript extends FlatSpec with Matchers {
   val commandLineScriptWithoutParams = s"sbt 'run-main org.clulab.reach.context.svm_scripts.TrainSVMInstance'"
 
   "SVM training script" should "create a .dat file to save the trained SVM model to" in {
-    val resultOfBasicEcho = Seq("ls", "-al").!
-    println(resultOfBasicEcho)
-    val listOfFilesFromScriptRun = Seq(commandLineScriptWithParams,"ls","grep svm_model_from_train_script.dat").!
+    val listOfFilesFromScriptRun = Seq(commandLineScriptWithParams).!
+    println(listOfFilesFromScriptRun)
+    //val listOfFilesFromScriptRun = Seq(commandLineScriptWithParams,"ls","grep svm_model_from_train_script.dat").!
     listOfFilesFromScriptRun should be (0)
 
   }
