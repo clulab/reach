1f78e82a3ea3b6921b97e03497bc97442499e324
ShraddhaThumsi <thumsishraddhasatish@gmail.com>
2019-09-21 17:39:09 -0700

    resolving runtime error on test 2 by similar Seq(commands).!

--- main/src/test/scala/org/clulab/reach/context/TestSVMTrainingScript.scala
+++ main/src/test/scala/org/clulab/reach/context/TestSVMTrainingScript.scala
@@ -24,7 +24,7 @@ class TestSVMTrainingScript extends FlatSpec with Matchers {
   }
 
   "SVM training script" should "throw an exception if no arguments are passed" in {
-    val result = commandLineScriptWithoutParams.!!
+    val result = Seq(commandLineScriptWithoutParams).!!
     val resultThrowsException = result.contains("java.lang.IllegalArgumentException")
     resultThrowsException should be (true)
 
