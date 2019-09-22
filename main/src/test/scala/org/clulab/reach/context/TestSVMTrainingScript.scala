0c343f358471fbc47921e91982f80739f5191a60
ShraddhaThumsi <thumsishraddhasatish@gmail.com>
2019-09-21 17:17:33 -0700

    trying to run shell script using .! and Seq()

--- main/src/test/scala/org/clulab/reach/context/TestSVMTrainingScript.scala
+++ main/src/test/scala/org/clulab/reach/context/TestSVMTrainingScript.scala
@@ -15,15 +15,16 @@ class TestSVMTrainingScript extends FlatSpec with Matchers {
   val commandLineScriptWithoutParams = s"sbt 'run-main org.clulab.reach.context.svm_scripts.TrainSVMInstance'"
 
   "SVM training script" should "create a .dat file to save the trained SVM model to" in {
-//    val listOfFilesFromScriptRun = commandLineScriptWithParams #| "ls" #| "grep svm_model_from_train_script.dat"!
-//    listOfFilesFromScriptRun should be (0)
-    //TODO
+    val listOfFilesFromScriptRun = Seq(commandLineScriptWithParams,"ls","grep svm_model_from_train_script.dat").!
+    listOfFilesFromScriptRun should be (0)
+
   }
 
   "SVM training script" should "throw an exception if no arguments are passed" in {
-    val result = commandLineScriptWithoutParams!!
-    val resultThrowsException = result.contains("java.lang.IllegalArgumentException")
-    resultThrowsException should be (true)
+//    val result = commandLineScriptWithoutParams!!
+//    val resultThrowsException = result.contains("java.lang.IllegalArgumentException")
+//    resultThrowsException should be (true)
+    //TODO
   }
 
   "Default training dataset" should "not contain degenerate papers" in {
