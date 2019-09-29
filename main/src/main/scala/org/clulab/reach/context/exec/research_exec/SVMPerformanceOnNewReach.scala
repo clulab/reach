bf0e2a818cbd92f3089a6d39b59910602d00caee
ShraddhaThumsi <thumsishraddhasatish@gmail.com>
2019-09-29 11:39:59 -0700

    the sentences in the binary files are sorted, but there seems to be a mismatch with paper IDs.

--- main/src/main/scala/org/clulab/reach/context/exec/research_exec/SVMPerformanceOnNewReach.scala
+++ main/src/main/scala/org/clulab/reach/context/exec/research_exec/SVMPerformanceOnNewReach.scala
@@ -274,12 +274,15 @@ object SVMPerformanceOnNewReach extends App {
       val sentencesWithIndices = sentences.zipWithIndex
       val sentencesToWriteToNewReach = collection.mutable.ListBuffer[String]()
       val sentencesToWriteToOldReach = collection.mutable.ListBuffer[String]()
+      println(s"We are currently viewing sentences from ${paperID}")
 
       val sortedEventSpansInPaperNewReach = sortedEventsInNewReachByPaper.filter(_._1 == paperID)(0)
+      println(s"Checking for unique events in: ${sortedEventSpansInPaperNewReach._1} (new reach)")
       println(s"In task 5, The current paper is: ${paperID}")
       println(s"The following are the unique event spans as found in new Reach")
       println(sortedEventSpansInPaperNewReach)
       val sortedEventSpansInPaperOldReach = sortedEventsInOldReachByPaper.filter(_._1 == paperID)(0)
+      println(s"Checking for unique events in: ${sortedEventSpansInPaperOldReach._1} (old reach)")
       for((sentence,sentenceIndex) <- sentencesWithIndices) {
         val sentenceToAddToNewReach = EventAlignmentUtils.makeBinarySentenceFromWords(sentence,sentenceIndex,sortedEventSpansInPaperNewReach._2)
         val sentenceToAddToOldReach = EventAlignmentUtils.makeBinarySentenceFromWords(sentence,sentenceIndex,sortedEventSpansInPaperOldReach._2)
