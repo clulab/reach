ba2d4d294c26d9c230f4b994ac0e210c078ff6e5
ShraddhaThumsi <thumsi.s@husky.neu.edu>
2019-11-09 11:29:14 -0700

    not clear which files are actually getting read

--- main/src/main/scala/org/clulab/reach/context/classifiers/SVMContextEngine.scala
+++ main/src/main/scala/org/clulab/reach/context/classifiers/SVMContextEngine.scala
@@ -93,13 +93,8 @@ class SVMContextEngine(sentenceWindow:Option[Int] = None) extends ContextEngine 
                 println("current event ID from Reach 2019")
                 println(eventID)
                 println("Pairs read from annotation file")
-                println(manualAnnotations.mkString("\n"))
-//                val printableMatchingSpans = manualAnnotations.filter(x => shouldIPrint(x._2,eventID)).map(_._2).toSet
-//                for(m <- printableMatchingSpans) {
-//                  println(s"The current paper is: ${paperID}")
-//                  println(s"The current event span from Reach parser is: ${eventID}")
-//                  println(s"The current event span from the annotated dataset is: ${m}")
-//                }
+                val annotationsInPaper = manualAnnotations.filter(_._1 == paperID)
+                println(annotationsInPaper.mkString("\n"))
 
                 if(manualAnnotations.contains(rowID))
 
