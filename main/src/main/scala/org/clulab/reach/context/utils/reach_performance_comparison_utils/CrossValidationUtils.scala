d7cd29f278116b94c1986a3e3c1a91a370b6f5b2
ShraddhaThumsi <thumsi.s@husky.neu.edu>
2019-11-21 13:07:20 -0700

    changed the code to remove balancing before writing the dataframe from the python side. I will now balance the rows in the cross validation loop itself.

--- main/src/main/scala/org/clulab/reach/context/utils/reach_performance_comparison_utils/CrossValidationUtils.scala
+++ main/src/main/scala/org/clulab/reach/context/utils/reach_performance_comparison_utils/CrossValidationUtils.scala
@@ -39,11 +39,7 @@ object CrossValidationUtils {
 
       val testingRowsFromCurrentPaper = rowsOfAggrRows.filter(x=>x.PMCID == paperID)
       val trainingRows = rowsOfAggrRows.filter(x=>x.PMCID!=paperID)
-      val balancedTrainingData = reachVersion.contains("2016") match {
-        //case true => Balancer.balanceByPaperAgg(trainingRows, 1)
-        case true => trainingRows
-        case false => trainingRows
-      }
+      val balancedTrainingData = Balancer.balanceByPaperAgg(trainingRows, 1)
 
       println(s"Size of training data set for ${reachVersion} when ${paperID} is the test case: ${balancedTrainingData.size}")
       val trainingfeatureValues = untrainedInstanceForCV.constructTupsForRVF(balancedTrainingData)
@@ -122,11 +118,8 @@ object CrossValidationUtils {
     }
     for(p<-papersToTestOn){
       val testingRows = rowsOfAggrContInst.filter(_.PMCID == p)
-      val balancedTestingData = reachVersion.contains("2016") match {
-        //case true => Balancer.balanceByPaperAgg(testingRows, 1)
-        case true => testingRows
-        case false => testingRows
-      }
+      val balancedTestingData = Balancer.balanceByPaperAgg(testingRows, 1)
+      println(s"The reach version ${reachVersion} has ${balancedTestingData.size} rows in the paper ${p}")
       val trueLabels = DummyClassifier.getLabelsFromDataset(balancedTestingData)
       val sentenceDistMinIndices = testingRows.map(x=>x.featureGroupNames.indexOf("sentenceDistance_min"))
       val sentenceDistMinValues = testingRows.map(x=>{
