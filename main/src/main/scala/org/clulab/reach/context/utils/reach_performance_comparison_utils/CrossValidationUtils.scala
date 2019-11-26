fd887ec379ddeac2b249f81d17e9cf9d4cc3bf4b
ShraddhaThumsi <thumsi.s@husky.neu.edu>
2019-11-26 11:53:00 -0700

    checking with smaller baseline window

--- main/src/main/scala/org/clulab/reach/context/utils/reach_performance_comparison_utils/CrossValidationUtils.scala
+++ main/src/main/scala/org/clulab/reach/context/utils/reach_performance_comparison_utils/CrossValidationUtils.scala
@@ -138,7 +138,7 @@ object CrossValidationUtils {
         valueAtSentMinIndex
       })
 
-      val predictedLabels = sentenceDistMinValues.map(predictPerRowDeterministic(_,14.0))
+      val predictedLabels = sentenceDistMinValues.map(predictPerRowDeterministic(_,6.0))
       microAveragedTruthLabels ++= trueLabels
       microAveragedPredictedLabels ++= predictedLabels
     }
