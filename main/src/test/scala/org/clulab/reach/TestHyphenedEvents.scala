36bbea8d22368d588744811d37ed8170e61b92ef
Mihai Surdeanu <surdeanu@gmail.com>
2020-10-13 20:39:40 -0500

    "fixed" failing test

--- main/src/test/scala/org/clulab/reach/TestHyphenedEvents.scala
+++ main/src/test/scala/org/clulab/reach/TestHyphenedEvents.scala
@@ -6,20 +6,21 @@ import TestUtils._
 
 class TestHyphenedEvents  extends FlatSpec with Matchers {
 
-  val sen1 = "The EM-inducing TFs (TWIST1, SNAIL1, SLUG, ZEB1, and FOXC2) in the CD45 - cells were determined using qRT-PCR."
+  val sen1 = "The KRas-inducing TFs (TWIST1, SNAIL1, SLUG, ZEB1, and FOXC2) in the CD45 - cells were determined using qRT-PCR."
 
   sen1 should "have a positive activation of levels of EM by TFs, TWIST1, SNAIL1, SLUG, ZEB1, FOXC2 and CD45" in {
 
     val mentions = getBioMentions(sen1)
+    val controlled = "KRas"
 
     // "TFs" is no longer picked up as a protein in the latest KBs?
     // hasPositiveActivation("TFs", "EM", mentions) should be (true)
-    hasPositiveActivation("TWIST1", "EM", mentions) should be (true)
-    hasPositiveActivation("SNAIL1", "EM", mentions) should be (true)
-    hasPositiveActivation("SLUG", "EM", mentions) should be (true)
-    hasPositiveActivation("ZEB1", "EM", mentions) should be (true)
-    hasPositiveActivation("FOXC2", "EM", mentions) should be (true)
-    hasPositiveActivation("CD45", "EM", mentions) should be (true)
+    hasPositiveActivation("TWIST1", controlled, mentions) should be (true)
+    hasPositiveActivation("SNAIL1", controlled, mentions) should be (true)
+    hasPositiveActivation("SLUG", controlled, mentions) should be (true)
+    hasPositiveActivation("ZEB1", controlled, mentions) should be (true)
+    hasPositiveActivation("FOXC2", controlled, mentions) should be (true)
+    hasPositiveActivation("CD45", controlled, mentions) should be (true)
 
   }
 
