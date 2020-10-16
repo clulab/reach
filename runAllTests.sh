6538b8e516375262f8ed54dfd33d8498b42b05b9
Keith Alcock <github@keithalcock.com>
2020-10-16 08:53:51 -0700

    Revert testing to multiple sbt runs
    It will take longer, but garbage collection works better

--- runAllTests.sh
+++ runAllTests.sh
@@ -1,15 +1,34 @@
 #!/usr/bin/env bash
 
+# One reason that not all tests are run in one calling of sbt
+# is that we get out of memory errors when doing that.
+
 # uncomment the lines below if processorAnnotator.type = "server"
 # start up the Processors Server for tests to run against:
 #echo 'Starting Processor Server...'
-#sbt 'runMain org.clulab.processors.server.ProcessorServer' &
+#sbt 'run-main org.clulab.processors.server.ProcessorServer' &
 #sleep 20
 
-# Don't start sbt multiple times if it is not necessary.
-# This will only fail once, but we want a yes/no answer anyway.
+overallExitCode=0
 
-sbt 'root/test; processors/test; main/test; export/test'
+function runTest {
+    echo "Testing '$1' suite..."
+    sbt $1
+    if [ $? -ne 0 ]; then
+        echo "The '$1' suite failed!"
+        overallExitCode=1
+    fi
+}
+
+runTest "test"
+runTest "processors/test"
+runTest "main/test"
+runTest "export/test"
 
 #echo 'Stopping Processor Server...'
-#sbt 'runMain org.clulab.processors.csshare.ShutdownProcessorServer'
+#sbt 'run-main org.clulab.processors.csshare.ShutdownProcessorServer'
+
+if [ $overallExitCode -ne 0 ]; then
+	echo "At least one test suite failed!"
+	exit 1
+fi
