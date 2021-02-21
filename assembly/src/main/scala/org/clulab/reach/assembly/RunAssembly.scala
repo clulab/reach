f132488d2c38bd831a2ed67bf7668b32abba8dab
Zhengzhong Liang <zhengzhongliang@Zhengzhongs-MacBook-Pro.local>
2021-02-21 16:18:47 -0700

    debug

--- assembly/src/main/scala/org/clulab/reach/assembly/RunAssembly.scala
+++ assembly/src/main/scala/org/clulab/reach/assembly/RunAssembly.scala
@@ -335,6 +335,8 @@ object EvalUnlabeledEventPairsFeatureClassifier extends App with LazyLogging {
   val results = config.getString("assembly.classifier.results")
   val eps: Seq[EventPair] = CorpusReader.readCorpus(config.getString("assembly.corpus.corpusDirNewTrain")).instances
 
+  logger(s"number of loaded event pairs: ${eps.length}")
+
   // gather precedence relations corpus
   val precedenceAnnotations = CorpusReader.filterRelations(eps, precedenceRelations)
   // train
