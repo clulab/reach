ba04b44c07007d8400378c8967591f4bfd403862
Zhengzhong Liang <zhengzhongliang93@gmail.com>
2022-05-14 11:01:48 -0700

    add code to support pytorch comm

--- main/src/main/scala/org/clulab/reach/context/NeuralEnginePython.py
+++ main/src/main/scala/org/clulab/reach/context/NeuralEnginePython.py
@@ -477,9 +477,9 @@ class NeuralContextEnginePythonInterface:
     print("*" * 40)
     print("Start loading the config file ...")
     python_file_dir = os.path.abspath("NeuralEnginePython.py")
-    resource_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(python_file_dir))))))
-    config_dir = os.path.join(resource_dir, 'NeuralContextEnginePython.conf')
-    print(resource_dir, config_dir,)
+    root_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(python_file_dir))))))
+    config_dir = os.path.join(root_dir, 'resources/NeuralContextEnginePython.conf')
+    print(root_dir, config_dir,)
     config = RunPythonModel.read_json(config_dir)
 
     print(config["device"])
